6bb52112de36cf189a7578a1d11679cde3fa385a
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-08-04 19:53:51 -0400

    fix(tz): Add get delegate to tz interface

--- packages/core/src/tz/interface.ts
+++ packages/core/src/tz/interface.ts
@@ -4,4 +4,10 @@ export interface TzProvider {
    * @param address Tezos address you want to get the balance for (eg tz1...)
    */
   getBalance(address: string): Promise<number>;
+
+  /**
+   *
+   * @param address Tezos address you want to get the delegate for (eg tz1...)
+   */
+  getDelegate(address: string): Promise<string | null>;
 }
