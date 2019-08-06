661407ae34d1675d621efc32115a215545ebc98c
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-08-06 12:42:11 -0400

    refactor(tsconfig): Use tsconfig paths during development instead of build assets for dependencies

---
+++ packages/tezos-ts-rpc/src/index.ts
@@ -0,0 +1,1 @@
+export * from "./tezos-ts-rpc";
--- packages/tezos-ts/rollup.config.ts
+++ packages/tezos-ts/rollup.config.ts
@@ -24,7 +24,7 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({ tsconfig: './tsconfig.prod.json', useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
