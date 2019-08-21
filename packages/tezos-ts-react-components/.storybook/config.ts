7f3f7feb9fc23529c0dd955952ee18dcc95ccaac
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-08-21 14:31:11 -0400

    feat(react-components): Add formatting options to balance component

--- packages/tezos-ts-react-components/.storybook/config.ts
+++ packages/tezos-ts-react-components/.storybook/config.ts
@@ -1,9 +1,17 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../stories', true, /\.stories\.tsx$/);
 
 function loadStories() {
   req.keys().forEach(req);
 }
+
+import yourTheme from './theme';
+
+addParameters({
+  options: {
+    theme: yourTheme
+  }
+});
 
 configure(loadStories, module);
---
+++ packages/tezos-ts-react-components/.storybook/theme.ts
@@ -0,0 +1,6 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Tezos ts react components demo'
+});
