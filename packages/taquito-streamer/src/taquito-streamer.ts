7c2cf5c9d3f483cf1e6f488d9aafaa6d1281cc0e
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-08-29 09:04:27 -0400

    feat(tezos-streamer): Introduce tezos-streamer implementation of the subscribe api

--- packages/taquito-streamer/src/taquito-streamer.ts
+++ packages/taquito-streamer/src/taquito-streamer.ts
@@ -85,7 +85,7 @@ export class Subscription {
 }
 
 export class StreamerProvider implements SubscribeProvider {
-  constructor(private url: string = DEFAULT_STREAMER_URL) {}
+  constructor(private url: string = DEFAULT_STREAMER_URL) { }
 
   subscribe(_filter: 'head'): Subscription {
     const ws = new WS(this.url);
