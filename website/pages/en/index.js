60dee127aaab3f40c049b35b92e53055b2063308
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-09-13 16:15:04 +0000

    Remove copyright message

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -1,15 +1,7 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
 
 const CompLibrary = require('../../core/CompLibrary.js');
 
-const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
