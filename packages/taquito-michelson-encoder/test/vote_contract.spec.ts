492e37cd8ae3da110d1a56bea7eb52cefdc38198
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-09-26 00:28:16 +0000

    test(michelson-encoder) Add a test case for name collision

--- packages/taquito-michelson-encoder/test/vote_contract.spec.ts
+++ packages/taquito-michelson-encoder/test/vote_contract.spec.ts
@@ -1,9 +1,32 @@
-import { params, storage } from '../data/vote_contract';
+import { params, storage, rpcContractResponse } from '../data/vote_contract';
 import { Schema } from '../src/schema/storage';
 
 describe('Schema test', () => {
+  it('Should parse storage schema properly', () => {
+    const schema = new Schema(storage);
+    expect(schema.ExtractSchema()).toEqual({
+      "mgr1": {
+        "addr": "address",
+        "key": "key_hash",
+      },
+      "mgr2": {
+        "addr": "address",
+        "key": "key_hash",
+      },
+    });
+  });
+
   it('Should parse storage properly', () => {
     const schema = new Schema(storage);
-    expect(schema.ExtractSchema()).toEqual({});
+    expect(schema.Execute(rpcContractResponse.script.storage)).toEqual({
+      "mgr1": {
+        "addr": "tz1QZ6KY7d3BuZDT1d19dUxoQrtFPN2QJ3hn",
+        "key": null,
+      },
+      "mgr2": {
+        "addr": "tz1QZ6KY7d3BuZDT1d19dUxoQrtFPN2QJ3hn",
+        "key": null,
+      },
+    });
   });
 });
