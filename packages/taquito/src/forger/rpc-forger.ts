ecc66a552a1a23e2183ef45ad8c57c0dbbd1a0e8
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-12-02 14:24:42 +0000

    Only use required properties

--- packages/taquito/src/forger/rpc-forger.ts
+++ packages/taquito/src/forger/rpc-forger.ts
@@ -2,9 +2,9 @@ import { Forger, ForgeParams, ForgeResponse } from './interface';
 import { Context } from '../context';
 
 export class RpcForger implements Forger {
-  constructor(private context: Context) {}
+  constructor(private context: Context) { }
 
-  forge(params: ForgeParams): Promise<ForgeResponse> {
-    return this.context.rpc.forgeOperations(params);
+  forge({ branch, contents }: ForgeParams): Promise<ForgeResponse> {
+    return this.context.rpc.forgeOperations({ branch, contents });
   }
 }
