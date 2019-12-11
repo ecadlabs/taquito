f47387a7aaef7fb431adbf341a06994645de0fad
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-12-11 20:24:43 +0000

    refactor(taquito): Do not use async iterator for counter provider

--- packages/taquito/src/preparer/preparers/counter.ts
+++ packages/taquito/src/preparer/preparers/counter.ts
@@ -1,23 +1,41 @@
-import { Preparer, PreparerContext } from "../types";
+import { Preparer, PreparerContext } from '../types';
 
-import { RPCOperation, RPCActivateOperation, RPCDelegateOperation, RPCTransferOperation, RPCRevealOperation, RPCOriginationOperation } from "../../operations/types";
-import { Context } from "../../context";
+import {
+  RPCOperation,
+  RPCActivateOperation,
+  RPCDelegateOperation,
+  RPCTransferOperation,
+  RPCRevealOperation,
+  RPCOriginationOperation,
+} from '../../operations/types';
+import { Context } from '../../context';
+
+class Counter {
+  private lastCounter?: number;
+
+  constructor(private pkh: string, private context: Context) {}
+
+  public async next(): Promise<{ value: number }> {
+    let counter = this.lastCounter;
+    if (typeof counter === 'undefined') {
+      const contract = await this.context.rpc.getContract(this.pkh);
+
+      let c = contract ? parseInt(contract.counter || '0', 10) : 0;
+
+      counter = c;
+    }
+
+    this.lastCounter = ++counter;
+    return { value: this.lastCounter };
+  }
+}
 
 export class CounterProvider {
-  private m: Map<string, AsyncIterator<number, number>> = new Map();
+  private m: Map<string, Counter> = new Map();
 
   for(pkh: string, context: Context) {
     if (!this.m.has(pkh)) {
-      this.m.set(pkh, (async function* () {
-        const contract = await context.rpc.getContract(pkh)
-
-        let c = contract ? parseInt(contract.counter || '0', 10) : 0;
-        yield ++c;
-
-        while (true) {
-          yield ++c;
-        }
-      })())
+      this.m.set(pkh, new Counter(pkh, context));
     }
 
     return this.m.get(pkh)!;
@@ -32,19 +50,31 @@ export class CounterProvider {
   }
 }
 
-type RPCOperationWithCounter = RPCActivateOperation | ((RPCOriginationOperation
-  | RPCTransferOperation
-  | RPCDelegateOperation
-  | RPCRevealOperation) & { counter: string });
+type RPCOperationWithCounter =
+  | RPCActivateOperation
+  | ((
+      | RPCOriginationOperation
+      | RPCTransferOperation
+      | RPCDelegateOperation
+      | RPCRevealOperation
+    ) & { counter: string });
 
 export class CounterPreparer implements Preparer {
-  async prepare(unPreparedOps: RPCOperation[], context: PreparerContext): Promise<RPCOperationWithCounter[]> {
+  async prepare(
+    unPreparedOps: RPCOperation[],
+    context: PreparerContext
+  ): Promise<RPCOperationWithCounter[]> {
     const counterProvider = new CounterProvider();
-    let results: RPCOperationWithCounter[] = []
+    let results: RPCOperationWithCounter[] = [];
     for (const op of unPreparedOps) {
-      if (op.kind === 'reveal' || op.kind === 'transaction' || op.kind === 'origination' || op.kind === 'delegation') {
-        const it = counterProvider.for(context.source, context.context)
-        results.push(Object.assign(op, { counter: String((await it.next()).value) }))
+      if (
+        op.kind === 'reveal' ||
+        op.kind === 'transaction' ||
+        op.kind === 'origination' ||
+        op.kind === 'delegation'
+      ) {
+        const it = counterProvider.for(context.source, context.context);
+        results.push(Object.assign(op, { counter: String((await it.next()).value) }));
       } else {
         results.push(op);
       }
