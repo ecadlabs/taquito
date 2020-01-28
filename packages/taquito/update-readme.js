d834b0f2831ef4b2d8fed4100b30a5c1832fe92f
Simon Boissonneault-Robert <simrob7000@gmail.com>
2020-01-28 16:13:40 +0000

    build(taquito): Compute checksum and update cdn script example on build

---
+++ packages/taquito/update-readme.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const readmePath = './README.md';
+
+const manifest = require('./manifest.json')
+const package = require('./package.json')
+
+const integrityRegex = /integrity=\"(.*)\"/;
+const versionRegex = /@taquito\/taquito@(.+)\/dist/
+
+if (fs.existsSync(readmePath)) {
+  let readme = fs.readFileSync(readmePath).toString('utf8');
+
+  readme = readme.replace(integrityRegex, `integrity="${manifest['main.js'].integrity}"`)
+  readme = readme.replace(versionRegex, `@taquito/taquito@${package.version}/dist`)
+
+  fs.writeFileSync(readmePath, readme);
+}
