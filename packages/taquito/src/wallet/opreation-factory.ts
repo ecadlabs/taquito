0d6d2d77cb3adcff43214e4b89321a0ce1e7252d
Simon Boissonneault-Robert <simrob7000@gmail.com>
2020-03-11 15:27:14 +0000

    test(taquito/wallet): Add tests for operation and operation-factory

--- packages/taquito/src/wallet/opreation-factory.ts
+++ packages/taquito/src/wallet/opreation-factory.ts
@@ -1,51 +1,77 @@
-import { BlockResponse } from "@taquito/rpc";
-import { AsyncSubject, concat, defer, from, MonoTypeOperatorFunction, Observable, of, range, throwError, timer } from "rxjs";
-import { concatMap, distinctUntilKeyChanged, first, shareReplay, startWith, switchMap, timeoutWith } from "rxjs/operators";
-import { Context } from "../context";
-import { WalletOperation } from "./operation";
-import { TransactionWalletOperation } from "./transaction-operation";
-import { OriginationWalletOperation } from "./origination-operation";
-import { DelegationWalletOperation } from "./delegation-operation";
+import { BlockResponse } from '@taquito/rpc';
+import {
+  concat,
+  defer,
+  from,
+  MonoTypeOperatorFunction,
+  Observable,
+  of,
+  range,
+  ReplaySubject,
+  SchedulerLike,
+  throwError,
+  timer,
+} from 'rxjs';
+import {
+  concatMap,
+  distinctUntilKeyChanged,
+  first,
+  shareReplay,
+  startWith,
+  switchMap,
+  timeoutWith,
+} from 'rxjs/operators';
+import { Context } from '../context';
+import { DelegationWalletOperation } from './delegation-operation';
+import { WalletOperation } from './operation';
+import { OriginationWalletOperation } from './origination-operation';
+import { TransactionWalletOperation } from './transaction-operation';
 
-const cacheUntil = <T>(cacheUntilObs: Observable<any>): MonoTypeOperatorFunction<T> => (source) => {
-  let subject: AsyncSubject<T> | null = null;
+export const cacheUntil = <T>(
+  cacheUntilObs: Observable<any>
+): MonoTypeOperatorFunction<T> => source => {
+  let subject: ReplaySubject<T> | null = null;
 
   return defer(() => {
     if (!subject) {
-      subject = new AsyncSubject<T>();
-      source.subscribe(subject);
+      subject = new ReplaySubject<T>();
+      source.pipe(first()).subscribe(subject);
       cacheUntilObs.pipe(first()).subscribe(() => {
         subject = null;
       });
     }
 
     return subject;
-  })
-}
+  });
+};
 
-const createNewPollingBasedHeadObservable = (
+export const createNewPollingBasedHeadObservable = (
   pollingTimer: Observable<number>,
   sharedHeadOb: Observable<BlockResponse>,
-  context: Context
+  context: Context,
+  scheduler?: SchedulerLike
 ): Observable<BlockResponse> => {
   return pollingTimer.pipe(
     switchMap(() => sharedHeadOb),
     distinctUntilKeyChanged('hash'),
-    timeoutWith(context.config.confirmationPollingTimeoutSecond * 1000, throwError(new Error('Confirmation polling timed out'))),
-    shareReplay(),
-  )
-}
+    timeoutWith(
+      context.config.confirmationPollingTimeoutSecond * 1000,
+      throwError(new Error('Confirmation polling timed out')),
+      scheduler
+    ),
+    shareReplay({
+      refCount: true,
+      scheduler,
+    })
+  );
+};
 
 export interface OperationFactoryConfig {
-  blockIdentifier?: string
+  blockIdentifier?: string;
 }
 
 export class OperationFactory {
-
-
-  constructor(private context: Context) {
-
-  }
+  constructor(private context: Context) {}
 
   // Cache the last block for one second across all operations
   private sharedHeadObs = defer(() => from(this.context.rpc.getBlock())).pipe(
@@ -57,51 +83,76 @@ export class OperationFactory {
       timer(0, this.context.config.confirmationPollingIntervalSecond * 1000),
       this.sharedHeadObs,
       this.context
-    )
+    );
   }
 
   private createPastBlockWalker(startBlock: string, count: number = 1) {
-    return from(this.context.rpc.getBlock({ block: startBlock }))
-      .pipe(
-        switchMap((block) => {
-          if (count === 1) {
-            return of(block);
-          }
+    return from(this.context.rpc.getBlock({ block: startBlock })).pipe(
+      switchMap(block => {
+        if (count === 1) {
+          return of(block);
+        }
 
-          return range(block.header.level, count - 1).pipe(
-            startWith(block),
-            concatMap(async (level) => {
-              return this.context.rpc.getBlock({ block: String(level) })
-            }))
-        }))
-
+        return range(block.header.level, count - 1).pipe(
+          startWith(block),
+          concatMap(async level => {
+            return this.context.rpc.getBlock({ block: String(level) });
+          })
+        );
+      })
+    );
   }
 
   private createHeadObservableFromConfig({ blockIdentifier }: OperationFactoryConfig) {
-    const observableSequence: Observable<BlockResponse>[] = []
+    const observableSequence: Observable<BlockResponse>[] = [];
 
     if (blockIdentifier) {
-      observableSequence.push(this.createPastBlockWalker(blockIdentifier))
+      observableSequence.push(this.createPastBlockWalker(blockIdentifier));
     }
 
-    observableSequence.push(this.createNewHeadObservable())
+    observableSequence.push(this.createNewHeadObservable());
 
-    return concat(...observableSequence)
+    return concat(...observableSequence);
   }
 
   createOperation(hash: string, config: OperationFactoryConfig = {}): WalletOperation {
-    return new WalletOperation(hash, this.context.clone(), this.createHeadObservableFromConfig(config))
+    return new WalletOperation(
+      hash,
+      this.context.clone(),
+      this.createHeadObservableFromConfig(config)
+    );
   }
 
-  createTransactionOperation(hash: string, config: OperationFactoryConfig = {}): TransactionWalletOperation {
-    return new TransactionWalletOperation(hash, this.context.clone(), this.createHeadObservableFromConfig(config))
+  createTransactionOperation(
+    hash: string,
+    config: OperationFactoryConfig = {}
+  ): TransactionWalletOperation {
+    return new TransactionWalletOperation(
+      hash,
+      this.context.clone(),
+      this.createHeadObservableFromConfig(config)
+    );
   }
 
-  createDelegationOperation(hash: string, config: OperationFactoryConfig = {}): DelegationWalletOperation {
-    return new DelegationWalletOperation(hash, this.context.clone(), this.createHeadObservableFromConfig(config))
+  createDelegationOperation(
+    hash: string,
+    config: OperationFactoryConfig = {}
+  ): DelegationWalletOperation {
+    return new DelegationWalletOperation(
+      hash,
+      this.context.clone(),
+      this.createHeadObservableFromConfig(config)
+    );
   }
 
-  createOriginationOperation(hash: string, config: OperationFactoryConfig = {}): OriginationWalletOperation {
-    return new OriginationWalletOperation(hash, this.context.clone(), this.createHeadObservableFromConfig(config))
+  createOriginationOperation(
+    hash: string,
+    config: OperationFactoryConfig = {}
+  ): OriginationWalletOperation {
+    return new OriginationWalletOperation(
+      hash,
+      this.context.clone(),
+      this.createHeadObservableFromConfig(config)
+    );
   }
 }
---
+++ packages/taquito/test/wallet/operation-factory.spec.ts
@@ -0,0 +1,123 @@
+import { rxSandbox } from 'rx-sandbox';
+import { defer, of } from 'rxjs';
+import { Context } from '../../src/context';
+import {
+  createNewPollingBasedHeadObservable,
+  cacheUntil,
+} from '../../src/wallet/opreation-factory';
+import { switchMap } from 'rxjs/operators';
+describe('createNewPollingBasedHeadObservable', () => {
+  const createFakeBlock = (level: number) => ({ hash: `test_${level}` });
+
+  it('Should resolve give a new head each time it polls', async done => {
+    const { cold, flush, scheduler, getMessages, e, advanceTo, s } = rxSandbox.create();
+    const timer = cold<number>('a-b');
+
+    const blocks = [
+      cold('(a|)', { a: createFakeBlock(0) }),
+      cold('(a|)', { a: createFakeBlock(1) }),
+    ];
+
+    let currentBlock = 0;
+
+    const blockObs = defer(() => {
+      return blocks[currentBlock++];
+    });
+
+    const obs = createNewPollingBasedHeadObservable(
+      timer,
+      blockObs as any,
+      new Context(),
+      scheduler
+    );
+
+    const messages = getMessages(obs);
+
+    advanceTo(0);
+
+    expect(messages).toEqual(e('a', { a: createFakeBlock(0) }));
+    expect(blocks[0].subscriptions).toEqual([s('(^!)--')]);
+    expect(blocks[1].subscriptions).toEqual([]);
+
+    flush();
+
+    expect(messages).toEqual(e('a-b', { a: createFakeBlock(0), b: createFakeBlock(1) }));
+    expect(blocks[0].subscriptions).toEqual([s('(^!)--')]);
+    expect(blocks[1].subscriptions).toEqual([s('--(^!)')]);
+
+    done();
+  });
+
+  it('Should not emit new head if the hash did not changed', async done => {
+    const { cold, flush, scheduler, getMessages, e, advanceTo, s } = rxSandbox.create();
+    const timer = cold<number>('a-b');
+
+    const blocks = [
+      cold('(a|)', { a: createFakeBlock(0) }),
+      cold('(a|)', { a: createFakeBlock(0) }),
+    ];
+
+    let currentBlock = 0;
+
+    const blockObs = defer(() => {
+      return blocks[currentBlock++];
+    });
+
+    const obs = createNewPollingBasedHeadObservable(
+      timer,
+      blockObs as any,
+      new Context(),
+      scheduler
+    );
+
+    const messages = getMessages(obs);
+
+    advanceTo(0);
+
+    expect(messages).toEqual(e('a', { a: createFakeBlock(0) }));
+    expect(blocks[0].subscriptions).toEqual([s('(^!)--')]);
+
+    flush();
+
+    // Should still subscribe to the observable but do not emit anything
+    expect(messages).toEqual(e('a', { a: createFakeBlock(0) }));
+    expect(blocks[1].subscriptions).toEqual([s('--(^!)')]);
+
+    done();
+  });
+});
+
+describe('Cache until operator', () => {
+  it('should subscribe to source and replay the last value until cacheInvalidator emits', async done => {
+    const { cold, flush, hot, getMessages, e, s } = rxSandbox.create();
+    const vals = [cold('a'), cold('b'), cold('c'), cold('d')];
+
+    let currentVals = 0;
+
+    const source = defer(() => {
+      return vals[currentVals++];
+    });
+
+    const cacheInvalidator = hot('^--a---------');
+    const poller = cold('ab-c------d--');
+
+    const cachedSource = source.pipe(cacheUntil(cacheInvalidator));
+
+    const obs = poller.pipe(
+      switchMap(() => {
+        return cachedSource;
+      })
+    );
+
+    const messages = getMessages(obs);
+    flush();
+
+    expect(messages).toEqual(e('aa-b------b--'));
+    expect(vals[0].subscriptions).toEqual([s('(^!)--')]);
+    expect(vals[1].subscriptions).toEqual([s('---(^!)')]);
+    expect(vals[2].subscriptions).toEqual([]);
+    expect(vals[3].subscriptions).toEqual([]);
+
+    done();
+  });
+});
