bb02bac3aa6c69d8a2c192084ed6580f86a4adf7
Simon Boissonneault-Robert <simrob7000@gmail.com>
2020-03-11 22:45:45 +0000

    refactor(taquito/wallet): Introduce higher level wallet class which contains shared logic between di
    Wallet provider now need to define how to map parameter to their respective format.
    Wallet API now
    supports batch operation
    Wallet API now delays the sending of operation until send method is called

--- packages/taquito/src/wallet/index.ts
+++ packages/taquito/src/wallet/index.ts
@@ -1,6 +1,7 @@
-export * from './interface';
-export { LegacyWallet } from './legacy';
+export * from './wallet';
 export * from './operation';
 export * from './transaction-operation';
 export * from './origination-operation';
 export * from './delegation-operation';
+export * from './interface';
+export * from './legacy';
