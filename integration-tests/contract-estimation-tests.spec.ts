e021a7b71813a3ae1503afc6bf2c3f911773bc14
Jev Björsell <jev@ecadlabs.com>
2020-04-11 15:35:46 -0700

    fix(tests) Update integration test for behaviour...
    that was corrected in PR #332

--- integration-tests/contract-estimation-tests.spec.ts
+++ integration-tests/contract-estimation-tests.spec.ts
@@ -163,14 +163,8 @@ CONFIGS().forEach(({ lib, setup, knownBaker, createAddress }) => {
     it('Estimate transfer to regular address with a fixed fee', async (done) => {
       // fee, gasLimit and storage limit are not taken into account
       const params = { fee: 2000, to: await Tezos.signer.publicKeyHash(), mutez: true, amount: amt - (1382 + DEFAULT_FEE.REVEAL) }
-      let estimate = await LowAmountTez.estimate.transfer(params);
-      expect(estimate).toMatchObject({
-        gasLimit: 10307,
-        storageLimit: 0,
-        suggestedFeeMutez: 1382
-      });
 
-      await expect(LowAmountTez.contract.transfer(params)).rejects.toEqual(
+      await expect(LowAmountTez.estimate.transfer(params)).rejects.toEqual(
         expect.objectContaining({
           // Not sure if it is expected according to (https://tezos.gitlab.io/api/errors.html)
           message: expect.stringContaining('storage_error'),
