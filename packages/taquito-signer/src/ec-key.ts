1cd1cfc7be7a52fde371e6f343901e4a959360fa
Andrew Kishino <AndrewKishino@gmail.com>
2020-05-30 10:02:50 -0700

    fix(taquito-signer): ensure public keys are padded correctly
    When deriving the public key and ultimately the public key hash from a secret key, some keys need to
    be padded in order to produce a valid key length of 32 bytes

--- packages/taquito-signer/src/ec-key.ts
+++ packages/taquito-signer/src/ec-key.ts
@@ -45,22 +45,10 @@ export class ECKey {
 
     this._key = decrypt(b58cdecode(this.key, prefix[keyPrefix]));
     const keyPair = new elliptic.ec(this.curve).keyFromPrivate(this._key);
-    const pref =
-      keyPair
-        .getPublic()
-        .getY()
-        .toArray()[31] % 2
-        ? 3
-        : 2;
+    const pref = keyPair.getPublic().getY().toArray()[31] % 2 ? 3 : 2;
+    const pad = new Array(32).fill(0);
     this._publicKey = toBuffer(
-      new Uint8Array(
-        [pref].concat(
-          keyPair
-            .getPublic()
-            .getX()
-            .toArray()
-        )
-      )
+      new Uint8Array([pref].concat(pad.concat(keyPair.getPublic().getX().toArray()).slice(-32)))
     );
   }
 
