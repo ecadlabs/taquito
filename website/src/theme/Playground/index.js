7a57dfc1b674f5e02b6a155fcb4df620a3554aee
Roxane Letourneau <roxane@ecadlabs.com>
2020-07-03 11:22:32 -0700

    Working with contracts having complexe storage/parameters

--- website/src/theme/Playground/index.js
+++ website/src/theme/Playground/index.js
@@ -98,6 +98,13 @@ const contractMap8pairs =
 //contract for map and bigmap combined example
 const contractMapBigMap = 
 [ { "prim": "parameter", "args": [ { "prim": "unit" } ] },{ "prim": "storage","args":[ { "prim": "pair","args":[ { "prim": "big_map","args":[ { "prim": "pair","args": [ { "prim": "nat" }, { "prim": "address" } ] },{ "prim": "int" } ], "annots": [ "%thebigmap" ] },{ "prim": "map","args":[ { "prim": "pair","args": [ { "prim": "nat" }, { "prim": "address" } ] },{ "prim": "int" } ], "annots": [ "%themap" ] } ] } ] },{ "prim": "code","args":[ [ { "prim": "DUP" }, { "prim": "CDR" },{ "prim": "NIL", "args": [ { "prim": "operation" } ] },{ "prim": "PAIR" },{ "prim": "DIP", "args": [ [ { "prim": "DROP" } ] ] } ] ] } ]
+
+//signer for example with complex storage/parameters
+const emailExample = "zsjpcmui.oysiavbv@tezos.example.org"
+const passwordExample = "4rW0D22yXt"
+const mnemonicExample = "arrange ceiling whisper churn congress double step carpet empty rice prevent swallow silk casual champion"
+const secretExample = "af552679c4943509bd77643b8ef3f8dcf42e61b3"
+
 `;
 
     this.transpile({ code, scope, transformCode, noInline });
