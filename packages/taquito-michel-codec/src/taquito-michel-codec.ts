72e5af797e7436e403c53dc6cc4788308ed683b4
Eugene Zagidullin <e.asphyx@gmail.com>
2020-07-18 14:16:05 +0300

    Consolidate all validators in one module

--- packages/taquito-michel-codec/src/taquito-michel-codec.ts
+++ packages/taquito-michel-codec/src/taquito-michel-codec.ts
@@ -3,7 +3,5 @@ export * from "./micheline-parser";
 export * from "./micheline-emitter";
 export * from "./michelson-validator";
 export * from "./michelson-types";
-export * from "./michelson-typecheck-type";
-export * from "./michelson-typecheck-data";
-export * from "./michelson-typecheck-code";
+export * from "./michelson-typecheck";
 export { MacroError } from "./macros";
