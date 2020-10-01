90521b95c7a1f23123e02dddb398db1235a7dfe4
Roxane Letourneau <roxane@ecadlabs.com>
2020-10-01 14:18:17 -0700

    Fixed contract-simple-delegation.spec.ts when account already delegated

--- integration-tests/contract-simple-delegation.spec.ts
+++ integration-tests/contract-simple-delegation.spec.ts
@@ -11,18 +11,23 @@ CONFIGS().forEach(({ lib, rpc, setup, knownBaker }) => {
     })
     it('succeeds in delegating its account to a known baker', async (done) => {
       const delegate = knownBaker
-      const op = await Tezos.contract.setDelegate({
-        delegate,
-        source: await Tezos.signer.publicKeyHash(),
-        fee: DEFAULT_FEE.DELEGATION,
-        gasLimit: DEFAULT_GAS_LIMIT.DELEGATION
-      })
-      await op.confirmation()
-      expect(op.hash).toBeDefined();
-      expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY)
-
-      const account = await Tezos.rpc.getDelegate(await Tezos.signer.publicKeyHash())
-      expect(account).toEqual(delegate)
+      try {
+        const op = await Tezos.contract.setDelegate({
+          delegate,
+          source: await Tezos.signer.publicKeyHash(),
+          fee: DEFAULT_FEE.DELEGATION,
+          gasLimit: DEFAULT_GAS_LIMIT.DELEGATION
+        })
+        await op.confirmation()
+        expect(op.hash).toBeDefined();
+        expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY)
+  
+        const account = await Tezos.rpc.getDelegate(await Tezos.signer.publicKeyHash())
+        expect(account).toEqual(delegate)
+      } catch (ex) {
+        //When running tests more than one time with the same faucet key, the account is already delegated to the given delegate
+        expect(ex.message).toMatch('delegate.unchanged')
+      }
       done();
     });
   });
