fe32c1b050e07276e4af7b48386d4527a045885f
sukhi-singh <sukhwinder@ecadlabs.com>
2020-11-29 16:46:09 -0800

    add integration tests to get constants for proto7

--- integration-tests/rpc-get-protocol-constants.spec.ts
+++ integration-tests/rpc-get-protocol-constants.spec.ts
@@ -214,7 +214,7 @@ describe('Fetch constants for all protocols on Mainnet', () => {
             } else {
                 expect(constants.test_chain_duration).toBeUndefined();
             }
-            
+
         } catch (ex) {
             fail(ex);
         }
@@ -329,6 +329,80 @@ describe('Fetch constants for all protocols on Mainnet', () => {
             }
             expect(constants.endorsement_reward.toString()).toEqual("1250000,833333");
             expect(constants.cost_per_byte.toString()).toEqual("1000");
+            expect(constants.hard_storage_limit_per_operation.toString()).toEqual("60000");
+
+            if (constants.test_chain_duration) {
+                expect(constants.test_chain_duration?.toString()).toEqual("1966080");
+            } else {
+                expect(constants.test_chain_duration).toBeUndefined();
+            }
+
+            if (constants.quorum_min) {
+                expect(constants).toHaveProperty("quorum_min", 2000);
+            } else {
+                expect(constants.quorum_min).toBeUndefined();
+            }
+
+            if (constants.quorum_max) {
+                expect(constants).toHaveProperty("quorum_max", 7000);
+            } else {
+                expect(constants.quorum_max).toBeUndefined();
+            }
+
+            if (constants.min_proposal_quorum) {
+                expect(constants).toHaveProperty("min_proposal_quorum", 500);
+            } else {
+                expect(constants.min_proposal_quorum).toBeUndefined();
+            }
+
+            if (constants.initial_endorsers) {
+                expect(constants).toHaveProperty("initial_endorsers", 24);
+            } else {
+                expect(constants.initial_endorsers).toBeUndefined();
+            }
+
+            if (constants.delay_per_missing_endorsement) {
+                expect(constants.delay_per_missing_endorsement.toString()).toEqual("8");
+            } else {
+                expect(constants.delay_per_missing_endorsement).toBeUndefined();
+            }
+
+        } catch (ex) {
+            fail(ex);
+        }
+        done();
+    })
+
+    it('successfully fetches Proto7 constants at level 1212416', async (done) => {
+        try {
+            const constants = await Tezos.rpc.getConstants({ block: "1212416" });
+            expect(constants).toHaveProperty("proof_of_work_nonce_size", 8);
+            expect(constants).toHaveProperty("nonce_length", 32);
+            expect(constants).toHaveProperty("max_operation_data_length", 16384);
+            expect(constants).toHaveProperty("max_proposals_per_delegate", 20);
+            expect(constants).toHaveProperty("preserved_cycles", 5);
+            expect(constants).toHaveProperty("blocks_per_cycle", 4096);
+            expect(constants).toHaveProperty("blocks_per_commitment", 32);
+            expect(constants).toHaveProperty("blocks_per_roll_snapshot", 256);
+            expect(constants).toHaveProperty("blocks_per_voting_period", 32768);
+            expect(constants.time_between_blocks.toString()).toEqual("60,40");
+            expect(constants).toHaveProperty("endorsers_per_block", 32);
+            expect(constants.hard_gas_limit_per_operation.toString()).toEqual("1040000");
+            expect(constants.hard_gas_limit_per_block.toString()).toEqual("10400000");
+            expect(constants.proof_of_work_threshold.toString()).toEqual("70368744177663");
+            expect(constants.tokens_per_roll.toString()).toEqual("8000000000");
+            expect(constants).toHaveProperty("michelson_maximum_type_size", 1000);
+            expect(constants.seed_nonce_revelation_tip.toString()).toEqual("125000");
+            expect(constants).toHaveProperty("origination_size", 257);
+            expect(constants.block_security_deposit.toString()).toEqual("512000000");
+            expect(constants.endorsement_security_deposit.toString()).toEqual("64000000");
+            if (constants.baking_reward_per_endorsement) {
+                expect(constants.baking_reward_per_endorsement.toString()).toEqual("1250000,187500");
+            } else {
+                expect(constants).toBeUndefined();
+            }
+            expect(constants.endorsement_reward.toString()).toEqual("1250000,833333");
+            expect(constants.cost_per_byte.toString()).toEqual("250");
             expect(constants.hard_storage_limit_per_operation.toString()).toEqual("60000");
 
             if (constants.test_chain_duration) {
