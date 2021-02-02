ac87fc7305475d005b4538ed768db865b8ec243d
Eugene Zagidullin <e.asphyx@gmail.com>
2021-02-02 16:11:06 +0200

    Pre edo tests ok

--- packages/taquito-michel-codec/test/typecheck.spec.ts
+++ packages/taquito-michel-codec/test/typecheck.spec.ts
@@ -1,118 +1,118 @@
-import { MichelsonType, MichelsonData, MichelsonPassableType } from "../src/michelson-types";
+import { MichelsonType, MichelsonData } from "../src/michelson-types";
 import { assertDataValid, assertTypesEqual, contractEntryPoint } from "../src/michelson-typecheck";
 
 describe('Typecheck', () => {
     it('assertDataValid: string', () => {
         const typedef: MichelsonType = { "prim": "string" };
-        const data: MichelsonData<typeof typedef> = { string: "test" };
+        const data: MichelsonData = { string: "test" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: int', () => {
         const typedef: MichelsonType = { "prim": "int" };
-        const data: MichelsonData<typeof typedef> = { int: "0" };
+        const data: MichelsonData = { int: "0" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: nat', () => {
         const typedef: MichelsonType = { "prim": "nat" };
-        const data: MichelsonData<typeof typedef> = { int: "0" };
+        const data: MichelsonData = { int: "0" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: bytes', () => {
         const typedef: MichelsonType = { "prim": "bytes" };
-        const data: MichelsonData<typeof typedef> = { "bytes": "0xABCDEF42" };
+        const data: MichelsonData = { "bytes": "ABCDEF42" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: bool', () => {
         const typedef: MichelsonType = { "prim": "bool" };
-        const data: MichelsonData<typeof typedef> = { "prim": "True" };
+        const data: MichelsonData = { "prim": "True" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: unit', () => {
         const typedef: MichelsonType = { "prim": "unit" };
-        const data: MichelsonData<typeof typedef> = { "prim": "Unit" };
+        const data: MichelsonData = { "prim": "Unit" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: list', () => {
         const typedef: MichelsonType = { "prim": "list", args: [{ prim: "string" }] };
-        const data: MichelsonData<typeof typedef> = [{ string: "aaa" }, { string: "bbb" }];
+        const data: MichelsonData = [{ string: "aaa" }, { string: "bbb" }];
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: pair', () => {
         const typedef: MichelsonType = { "prim": "pair", args: [{ prim: "string" }, { prim: "nat" }] };
-        const data: MichelsonData<typeof typedef> = { prim: "Pair", args: [{ string: "aaa" }, { int: "0" }] };
+        const data: MichelsonData = { prim: "Pair", args: [{ string: "aaa" }, { int: "0" }] };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: option nat', () => {
         const typedef: MichelsonType = { "prim": "option", args: [{ prim: "nat" }] };
-        const data: MichelsonData<typeof typedef> = { prim: "Some", args: [{ int: "0" }] };
+        const data: MichelsonData = { prim: "Some", args: [{ int: "0" }] };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: option none', () => {
         const typedef: MichelsonType = { "prim": "option", args: [{ prim: "nat" }] };
-        const data: MichelsonData<typeof typedef> = { prim: "None" };
+        const data: MichelsonData = { prim: "None" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: or left', () => {
         const typedef: MichelsonType = { "prim": "or", args: [{ prim: "string" }, { prim: "nat" }] };
-        const data: MichelsonData<typeof typedef> = { prim: "Left", args: [{ string: "aaa" }] };
+        const data: MichelsonData = { prim: "Left", args: [{ string: "aaa" }] };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: or right', () => {
         const typedef: MichelsonType = { "prim": "or", args: [{ prim: "string" }, { prim: "nat" }] };
-        const data: MichelsonData<typeof typedef> = { prim: "Right", args: [{ int: "0" }] };
+        const data: MichelsonData = { prim: "Right", args: [{ int: "0" }] };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: set', () => {
         const typedef: MichelsonType = { "prim": "set", args: [{ "prim": "pair", args: [{ prim: "string" }, { prim: "nat" }] }] };
-        const data: MichelsonData<typeof typedef> = [{ prim: "Pair", args: [{ string: "aaa" }, { int: "0" }] }, { prim: "Pair", args: [{ string: "bbb" }, { int: "1" }] }];
+        const data: MichelsonData = [{ prim: "Pair", args: [{ string: "aaa" }, { int: "0" }] }, { prim: "Pair", args: [{ string: "bbb" }, { int: "1" }] }];
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: unsorted set', () => {
         const typedef: MichelsonType = { "prim": "set", args: [{ "prim": "pair", args: [{ prim: "string" }, { prim: "nat" }] }] };
-        const data: MichelsonData<typeof typedef> = [{ prim: "Pair", args: [{ string: "bbb" }, { int: "0" }] }, { prim: "Pair", args: [{ string: "aaa" }, { int: "1" }] }];
+        const data: MichelsonData = [{ prim: "Pair", args: [{ string: "bbb" }, { int: "0" }] }, { prim: "Pair", args: [{ string: "aaa" }, { int: "1" }] }];
         expect(() => assertDataValid(data, typedef)).toThrow();
     });
 
     it('assertDataValid: map', () => {
         const typedef: MichelsonType = { "prim": "map", args: [{ prim: "string" }, { prim: "nat" }] };
-        const data: MichelsonData<typeof typedef> = [{ prim: "Elt", args: [{ string: "aaa" }, { int: "0" }] }, { prim: "Elt", args: [{ string: "bbb" }, { int: "1" }] }];
+        const data: MichelsonData = [{ prim: "Elt", args: [{ string: "aaa" }, { int: "0" }] }, { prim: "Elt", args: [{ string: "bbb" }, { int: "1" }] }];
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: unsorted map', () => {
         const typedef: MichelsonType = { "prim": "map", args: [{ prim: "string" }, { prim: "nat" }] };
-        const data: MichelsonData<typeof typedef> = [{ prim: "Elt", args: [{ string: "bbb" }, { int: "0" }] }, { prim: "Elt", args: [{ string: "aaa" }, { int: "1" }] }];
+        const data: MichelsonData = [{ prim: "Elt", args: [{ string: "bbb" }, { int: "0" }] }, { prim: "Elt", args: [{ string: "aaa" }, { int: "1" }] }];
         expect(() => assertDataValid(data, typedef)).toThrow();
     });
 
     it('assertDataValid: timestamp', () => {
         const typedef: MichelsonType = { "prim": "timestamp" };
-        const data: MichelsonData<typeof typedef> = { string: "2020-06-21T00:39:07Z" };
+        const data: MichelsonData = { string: "2020-06-21T00:39:07Z" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: address', () => {
         const typedef: MichelsonType = { "prim": "address" };
-        const data: MichelsonData<typeof typedef> = { string: "tz1VmUWL8DxseZnPTdhHQkkuk6nK55NVdKCG" };
+        const data: MichelsonData = { string: "tz1VmUWL8DxseZnPTdhHQkkuk6nK55NVdKCG" };
         assertDataValid(data, typedef);
     });
 
     it('assertDataValid: mutez', () => {
         const typedef: MichelsonType = { "prim": "mutez" };
-        const data: MichelsonData<typeof typedef> = { int: "0" };
+        const data: MichelsonData = { int: "0" };
         assertDataValid(data, typedef);
     });
 
@@ -146,7 +146,7 @@ describe('Typecheck', () => {
     });
 
     it('contractEntrypoint', () => {
-        const param: MichelsonPassableType = {
+        const param: MichelsonType = {
             prim: 'or',
             args: [
                 {
