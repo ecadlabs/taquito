dfcf9582b0f6dde7faa765fe2cd12b9f69297154
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-02-23 08:19:27 -0800

    add more wallet tests

---
+++ integration-tests/wallet-ligo-increment-contract.spec.ts
@@ -0,0 +1,33 @@
+import { CONFIGS } from "./config";
+import { ligoSample } from "./data/ligo-simple-contract";
+
+CONFIGS().forEach(({ lib, rpc, setup }) => {
+  const Tezos = lib;
+  describe(`Simple ligo origination scenario using: ${rpc}`, () => {
+
+    beforeEach(async (done) => {
+      await setup()
+      done()
+    })
+    it('for wallet api, originates michelson produced by ligo, and increments a counter in storage', async (done) => {
+      const op = await Tezos.wallet.originate({
+        balance: "1",
+        code: ligoSample,
+        storage: 0,
+      }).send();
+      await op.confirmation()
+      expect(op.opHash).toBeDefined();
+      const contract = await op.contract();
+
+      const storage: any = await contract.storage()
+      expect(storage.toString()).toEqual("0")
+      const opMethod = await contract.methods.default("2").send();
+
+      await opMethod.confirmation();
+      expect(op.opHash).toBeDefined();
+       const storage2: any = await contract.storage()
+      expect(storage2.toString()).toEqual("2")
+      done();
+    });
+  });
+})
---
+++ integration-tests/wallet-no-annotations-call-by-index.spec.ts
@@ -0,0 +1,57 @@
+import { CONFIGS } from "./config";
+import { noAnnotCode, noAnnotInit } from "./data/token_without_annotation";
+
+CONFIGS().forEach(({ lib, rpc, setup }) => {
+  const Tezos = lib;
+  describe(`Test contract made with wallet API with no annotations calling methods by index using: ${rpc}`, () => {
+
+    beforeEach(async (done) => {
+      await setup()
+      done()
+    })
+    it('Test contract madew ith wallet API with no annotations for methods', async (done) => {
+      // Constants to replace annotations
+      const ACCOUNTS = '0';
+      const BALANCE = '0';
+      const ALLOWANCES = '1';
+      const TRANSFER = '0';
+      const APPROVE = '2';
+
+      // Actual tests
+
+      const ACCOUNT1_ADDRESS = await Tezos.signer.publicKeyHash()
+      const ACCOUNT2_ADDRESS = 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu'
+
+      // Originate a contract with a known state
+      const op = await Tezos.wallet.originate({
+        balance: "1",
+        code: noAnnotCode,
+        init: noAnnotInit(await Tezos.signer.publicKeyHash())
+      }).send();
+      const contract = await op.contract()
+
+      // Make a transfer
+      const operation = await contract.methods[TRANSFER](ACCOUNT1_ADDRESS, ACCOUNT2_ADDRESS, "1").send();
+      await operation.confirmation();
+      expect(operation.status).toEqual('applied')
+
+      // Verify that the transfer was done as expected
+      const storage = await contract.storage<any>()
+      let account1 = await storage[ACCOUNTS].get(ACCOUNT1_ADDRESS)
+      expect(account1[BALANCE].toString()).toEqual('16')
+
+      const account2 = await storage[ACCOUNTS].get(ACCOUNT2_ADDRESS)
+      expect(account2[BALANCE].toString()).toEqual('1')
+
+      // Approve
+      const operation2 = await contract.methods[APPROVE](ACCOUNT2_ADDRESS, "1").send();
+      await operation2.confirmation();
+      expect(operation2.status).toEqual('applied')
+
+      // Verify that the allowance was done as expected
+      account1 = await storage[ACCOUNTS].get(ACCOUNT1_ADDRESS)
+      expect(account1[ALLOWANCES].get(ACCOUNT2_ADDRESS).toString()).toEqual('1')
+      done();
+    })
+  });
+})
