0073c69401fe9c6a657470301d289701eee5db21
Roxane Letourneau <roxane@ecadlabs.com>
2021-03-01 08:59:37 -0800

    Added deprecated comment for bytes2Char, char2Bytes in tzip16 package

--- packages/taquito-tzip16/src/taquito-tzip16.ts
+++ packages/taquito-tzip16/src/taquito-tzip16.ts
@@ -16,4 +16,9 @@ export * from './viewKind/interface';
 export * from './viewKind/michelson-storage-view';
 export * from './viewKind/viewFactory';
 
+/**
+ * @deprecated `import { bytes2Char, char2Bytes } from "@taquito/tzip16"` is deprecated in favor of 
+ * `import { bytes2Char, char2Bytes } from "@taquito/utils"`
+ *
+ */
 export { bytes2Char, char2Bytes } from "@taquito/utils";
