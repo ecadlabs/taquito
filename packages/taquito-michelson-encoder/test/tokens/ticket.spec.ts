00402c6d190a74fef247fc5ebc563aae8a69bebf
Roxane Letourneau <roxane@ecadlabs.com>
2021-03-15 10:05:14 -0700

    Support other types rather than just nat for the value property of ticket

--- packages/taquito-michelson-encoder/test/tokens/ticket.spec.ts
+++ packages/taquito-michelson-encoder/test/tokens/ticket.spec.ts
@@ -4,8 +4,10 @@ import { TicketToken, EncodeTicketError } from '../../src/tokens/ticket';
 
 describe('Ticket token', () => {
   let tokenTicketNat: TicketToken;
+  let tokenTicketTimestamp: TicketToken;
   beforeEach(() => {
     tokenTicketNat = createToken({"prim":"ticket","args":[{"prim":"nat"}],"annots":["%receive"]}, 0) as TicketToken;
+    tokenTicketTimestamp = createToken({"prim":"ticket","args":[{"prim":"timestamp"}]}, 0) as TicketToken;
   });
 
   describe('EncodeObject', () => {
@@ -14,6 +16,7 @@ describe('Ticket token', () => {
       expect(() => tokenTicketNat.EncodeObject(2)).toThrowError(EncodeTicketError);
       expect(() => tokenTicketNat.EncodeObject({})).toThrowError(EncodeTicketError);
       expect(() => tokenTicketNat.EncodeObject('11')).toThrowError(EncodeTicketError);
+      expect(() => tokenTicketTimestamp.EncodeObject(new Date())).toThrowError(EncodeTicketError);
     });
   });
 
@@ -24,15 +27,24 @@ describe('Ticket token', () => {
       expect(() => tokenTicketNat.Encode([11])).toThrowError(EncodeTicketError);
       expect(() => tokenTicketNat.Encode([])).toThrowError(EncodeTicketError);
       expect(() => tokenTicketNat.Encode([{}])).toThrowError(EncodeTicketError);
+      expect(() => tokenTicketTimestamp.EncodeObject([new Date()])).toThrowError(EncodeTicketError);
     });
   });
 
   describe('Execute', () => {
-    it('Should execute on readTicketType', () => {
+    it('Should execute on readTicketType with ticket of type nat', () => {
       expect(tokenTicketNat.Execute({"prim":"Pair","args":[{"string":"KT1EAMUQC1yJ2sRPNPpLHVMGCzroYGe1C1ea"},{"int":"0"},{"int":"1"}]})).toEqual({
         ticketer: 'KT1EAMUQC1yJ2sRPNPpLHVMGCzroYGe1C1ea',
         value: new BigNumber('0'),
         amount: new BigNumber('1')
+      });
+    });
+
+    it('Should execute on readTicketType with ticket of type timestamp', () => {
+      expect(tokenTicketTimestamp.Execute({"prim":"Pair","args":[{"string":"KT1PVuv7af4VkPsZVZ8oZz9GSSdGnGBCbFWw"},{"string":"2021-03-09T16:32:15Z"},{"int":"2"}]})).toEqual({
+        ticketer: 'KT1PVuv7af4VkPsZVZ8oZz9GSSdGnGBCbFWw',
+        value: "2021-03-09T16:32:15.000Z",
+        amount: new BigNumber('2')
       });
     });
 
