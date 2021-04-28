6be8667dca4d3a974529f6f530374cd66a482683
Eugene Zagidullin <eugene@ecadlabs.com>
2021-04-28 20:57:15 +0300

    one more test

--- packages/taquito-michel-bridge/test/typeinfo.spec.ts
+++ packages/taquito-michel-bridge/test/typeinfo.spec.ts
@@ -2,7 +2,7 @@ import { MichelsonType } from "@taquito/michel-codec";
 import { getTypeInfo, TypeInfo, ObjectID } from "../src/typeinfo";
 
 describe("Type info", () => {
-    it("object", () => {
+    it("annotated comb", () => {
         const src: MichelsonType = [
             { prim: "int", annots: ["%one"] },
             { prim: "nat", annots: ["%two"] },
@@ -191,6 +191,76 @@ describe("Type info", () => {
                         left: { type: 'signature', expr: { prim: 'signature' } },
                         right: { type: 'bool', expr: { prim: 'bool' } },
                         name: 'tuple'
+                    }
+                }
+            }
+        };
+
+        const ti = getTypeInfo(src);
+        expect(ti).toEqual(expected);
+    });
+
+    it("partially annotated", () => {
+        const src: MichelsonType = {
+            prim: "pair",
+            args: [
+                { prim: "int" },
+                {
+                    prim: "pair", args: [
+                        { prim: "nat", annots: ["%one"] },
+                        { prim: "string", annots: ["%two"] }
+                    ]
+                },
+            ]
+        };
+
+        const expected: TypeInfo = {
+            type: 'pair',
+            expr: {
+                prim: 'pair',
+                args: [
+                    { prim: 'int' },
+                    {
+                        prim: 'pair',
+                        args: [
+                            { prim: 'nat', annots: ['%one'] },
+                            { prim: 'string', annots: ['%two'] }
+                        ]
+                    }
+                ]
+            },
+            left: { type: 'int', expr: { prim: 'int' } },
+            right: {
+                type: ObjectID,
+                expr: {
+                    prim: 'pair',
+                    args: [
+                        { prim: 'nat', annots: ['%one'] },
+                        { prim: 'string', annots: ['%two'] }
+                    ]
+                },
+                fields: [
+                    {
+                        type: 'nat',
+                        expr: { prim: 'nat', annots: ['%one'] },
+                        field: 'one'
+                    },
+                    {
+                        type: 'string',
+                        expr: { prim: 'string', annots: ['%two'] },
+                        field: 'two'
+                    }
+                ],
+                fieldsIndex: {
+                    one: {
+                        type: 'nat',
+                        expr: { prim: 'nat', annots: ['%one'] },
+                        field: 'one'
+                    },
+                    two: {
+                        type: 'string',
+                        expr: { prim: 'string', annots: ['%two'] },
+                        field: 'two'
                     }
                 }
             }
