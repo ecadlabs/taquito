c95dc924140a0119104acaf66af5ff70a43d32c1
Kunal Jaiswal <kunal@ecadlabs.com>
2021-05-10 13:58:49 -0600

    Issue-765 Fixed versions for rxjs and typescript in all package json files

--- packages/taquito-tezbridge-wallet/src/version.ts
+++ packages/taquito-tezbridge-wallet/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "334454ed1f3717affb11a943e271fe2a6330a0c5",
+    "commitHash": "e8e9332535423269b904d04eeaec154661ae0422",
     "version": "9.0.1"
 };
 /* tslint:enable */
