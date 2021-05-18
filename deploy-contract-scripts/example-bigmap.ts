c8bf5b2311fb47f462d3d7cd059eb26f43c243a7
Robert Evans <robertevans@Roberts-MacBook-Pro.local>
2021-05-18 15:50:04 -0400

    issue-873: added contract script for the sequential and parallel bigmap code examples

---
+++ deploy-contract-scripts/example-bigmap.ts
@@ -0,0 +1,49 @@
+import {TezosToolkit, MichelsonMap} from '@taquito/taquito';
+import {importKey} from '@taquito/signer';
+const tezos = new TezosToolkit("https://api.tez.ie/rpc/florencenet");
+const contractJSON = require('./code-json/example-bigmap.json');
+
+async function deploy() {
+  await importKey(
+    tezos, 
+    "ahzahfuy.nymukxzz@tezos.example.org",
+    "zCiXvkVJDB",
+    [
+      "blood",
+      "predict",
+      "enact",
+      "change",
+      "fresh",
+      "clever",
+      "test",
+      "segment",
+      "health",
+      "famous",
+      "cash",
+      "wet",
+      "slow",
+      "snack",
+      "reduce"
+    ].join(" "),
+    "0e4fb76262187b84cb60e307dd5aa7c4562adb2e"
+    );
+
+  tezos.contract
+    .originate({
+      code: contractJSON,
+      storage: {
+        0: 10,
+        1: new MichelsonMap(),
+      },
+    })
+    .then((originationOp) => {
+      console.log(`Waiting for confirmation of origination for ${originationOp.contractAddress}...`);
+      return originationOp.contract();
+    })
+    .then((contract) => {
+      console.log(`Origination completed.`);
+    })
+    .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
+}
+
+deploy();
