15e5c9174fd615826248ae04838d9ce104fb2724
Roxane Létourneau <65877347+roxaneletourneau@users.noreply.github.com>
2021-05-20 14:07:04 -0700

    Added a test cases with 150 operations in a batch (#880)
    * Added a test cases with 150 operations in a batch
    
    * Removed console.log

---
+++ integration-tests/contract-batch-high-number-of-operations.spec.ts
@@ -0,0 +1,41 @@
+import { CONFIGS } from './config';
+import { b58cencode, Prefix, prefix } from '@taquito/utils';
+import { InMemorySigner } from '@taquito/signer';
+const crypto = require('crypto');
+
+CONFIGS().forEach(({ lib, rpc, setup }) => {
+    const Tezos = lib;
+
+    describe(`Test contract.batch containing a high number of operations using: ${rpc}`, () => {
+        beforeEach(async (done) => {
+            await setup(true);
+            done();
+        });
+
+        it('Test batch with 150 operations', async (done) => {
+            const dests: { key: string, pkh: string }[] = [];
+            const batchSize = 150;
+
+            for (let i = 0; i < batchSize; i++) {
+                const keyBytes = Buffer.alloc(32);
+                crypto.randomFillSync(keyBytes)
+
+                const key = b58cencode(new Uint8Array(keyBytes), prefix[Prefix.SPSK]);
+                const pkh = await new InMemorySigner(key).publicKeyHash();
+                dests.push({ key, pkh });
+            }
+
+            const batch = Tezos.contract.batch()
+            dests.forEach(({ pkh }) => {
+                // We currently need to bypass the estimate for gapha2net as the hard_gas_limit_per_block is exceeded
+                batch.withTransfer({ to: pkh, amount: 0.001, gasLimit: 1527});
+            })
+
+            const op = await batch.send();
+            await op.confirmation();
+
+            expect(op.status).toEqual('applied');
+            done();
+        });
+    });
+});
