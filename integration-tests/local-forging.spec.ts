594a04541c1711d3900059df23ab89f79eae4d0b
Roxane Letourneau <roxane@ecadlabs.com>
2021-05-21 10:08:41 -0700

    Run edo forging tests for all tesnets

--- integration-tests/local-forging.spec.ts
+++ integration-tests/local-forging.spec.ts
@@ -1,11 +1,10 @@
 import { CONFIGS } from "./config";
 import { commonCases, edoCases } from '../packages/taquito-local-forging/test/allTestsCases';
 import { localForger } from '@taquito/local-forging'
-import { Protocols, TezosToolkit } from "@taquito/taquito";
+import { TezosToolkit } from "@taquito/taquito";
 
-CONFIGS().forEach(({ rpc, protocol }) => {
+CONFIGS().forEach(({ rpc }) => {
     const Tezos = new TezosToolkit(rpc);
-    const edonet = (protocol === Protocols.PtEdo2Zk) ? test : test.skip;
 
     describe(`Test local forger: ${rpc}`, () => {
 
@@ -22,7 +21,7 @@ CONFIGS().forEach(({ rpc, protocol }) => {
 
          edoCases.forEach(({ name, operation, expected }) => {
 
-            edonet(`Should give the same result as when forging with the rpc: ${name} (${rpc})`, async done => {
+            it(`Should give the same result as when forging with the rpc: ${name} (${rpc})`, async done => {
                 const result = await localForger.forge(operation);
                 const rpcResult = await Tezos.rpc.forgeOperations(operation);
                 expect(result).toEqual(rpcResult);
