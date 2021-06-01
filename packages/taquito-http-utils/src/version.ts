4e32e7674a7a271c1e3542dbf5e47d38bc59b020
Roxane Letourneau <roxane@ecadlabs.com>
2021-06-01 13:41:59 -0700

    chore(releng) bump version to 9.1.0-beta-RC.0

--- packages/taquito-http-utils/src/version.ts
+++ packages/taquito-http-utils/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "b87492033d9a1eba4ca2ec655548ef0007a89248",
+    "commitHash": "b6ec709bfb98d93e03810e3fb90d2273a8670332",
     "version": "9.1.0-beta-RC.0"
 };
 /* tslint:enable */
