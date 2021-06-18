7daacfa715dfc0c6bc1bb60164d715c168be30e0
Roxane Letourneau <roxane@ecadlabs.com>
2021-06-17 17:07:13 -0700

    Added sapling endpoints to the RpcClient and proper rendering of the sapling state when fetching storage

--- integration-tests/contract-originate-contract-with-sapling.spec.ts
+++ integration-tests/contract-originate-contract-with-sapling.spec.ts
@@ -1,16 +1,18 @@
 import { CONFIGS } from './config';
 import { SaplingStateValue } from '../packages/taquito-michelson-encoder/src/taquito-michelson-encoder';
 import { saplingContract } from './data/sapling_contracts';
+import BigNumber from 'bignumber.js';
+import { SaplingStateAbstraction } from 'taquito/src/contract/sapling-state-abstraction';
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
-	const Tezos = lib;
-	const test = require('jest-retries');
+  const Tezos = lib;
+  const test = require('jest-retries');
 
-	describe(`Test origination of contracts with sapling using: ${rpc}`, () => {
-		beforeEach(async (done) => {
-			await setup();
-			done();
-		});
+  describe(`Test origination of contracts with sapling using: ${rpc}`, () => {
+    beforeEach(async (done) => {
+      await setup();
+      done();
+    });
 
     test('Originates a contract with sapling states in its storage', 2, async (done: () => void) => {
       const op = await Tezos.contract.originate({
@@ -24,7 +26,22 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
       await op.confirmation();
       expect(op.hash).toBeDefined();
       expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
+
+      const contract = await op.contract()
+      interface StorageType {
+        balance: BigNumber,
+        ledger1: SaplingStateAbstraction,
+        ledger2: SaplingStateAbstraction
+      }
+
+      const storage: StorageType = await contract.storage();
+      const saplingStateLedger1 = await storage.ledger1.getSaplingDiff();
+
+      expect(saplingStateLedger1.root).toBeDefined();
+      expect(saplingStateLedger1.nullifiers.length).toEqual(0);
+      expect(saplingStateLedger1.commitments_and_ciphertexts.length).toEqual(0);
+
       done();
     });
-	});
+  });
 });
