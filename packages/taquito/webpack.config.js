3bb24ac299842b3ec3cab20d4fcbccc716c476bd
Roxane Létourneau <65877347+roxaneletourneau@users.noreply.github.com>
2021-06-22 14:32:18 -0700

    765 package (#945)
    * Issue-765 Initial commit for dependency work
    
    * Issue-765 Regenerated newer lock files
    
    * Updated package files
    
    * Fix webpack
    
    * Downgraded jest
    
    Co-authored-by: Kunal Jaiswal <kunal@ecadlabs.com>

--- packages/taquito/webpack.config.js
+++ packages/taquito/webpack.config.js
@@ -13,8 +13,17 @@ module.exports = {
     filename: pkg.unpkg,
     crossOriginLoading: 'anonymous'
   },
-  node: {
-    fs: 'empty'
+  resolve: {
+    fallback: {
+      fs: false,
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      url: require.resolve("url/"),
+      stream: require.resolve("stream-browserify"),
+      events: require.resolve("events/"),
+      buffer: require.resolve("buffer/")
+    }
   },
   optimization: {
     minimize: true,
