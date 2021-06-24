d0d38b3871c6ef8f43d30435eb56d1814f56b6fe
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-06-24 09:52:30 -0700

    modified packages

--- packages/taquito-http-utils/src/version.ts
+++ packages/taquito-http-utils/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "02966de9d4addd327339de91a452803a0c709844",
+    "commitHash": "023ae4f4f0b8e763335b9e85be53a654cef6e5c0",
     "version": "9.1.1"
 };
 /* tslint:enable */
