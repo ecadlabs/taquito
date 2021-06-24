ff9065f061357f731b23e0f12af67bdba40d06b3
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-06-24 13:54:36 -0700

    more conflict resolutions

--- packages/taquito-rpc/src/version.ts
+++ packages/taquito-rpc/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "46ce21d6d08a6b1525118956e043f20200700ae8",
+    "commitHash": "1e4a03b126cdefaf970f298b5d3db43df228e2f9",
     "version": "9.1.1"
 };
 /* tslint:enable */
