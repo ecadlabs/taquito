eab1c7b7d1b227b744fa23eed13ed1c1865db0ff
Roxane Letourneau <roxane@ecadlabs.com>
2021-07-02 11:21:24 -0700

    downgraded sodium

--- packages/taquito-tezbridge-wallet/src/version.ts
+++ packages/taquito-tezbridge-wallet/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "098e324434b6f9cdab4dc4ee96d2ae49d08b390e",
+    "commitHash": "b064414d33104f1e7a817d6fd4cb7cef2f97b6df",
     "version": "9.2.0-beta-RC.1"
 };
 /* tslint:enable */
