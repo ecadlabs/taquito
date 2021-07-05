cd51010ce97451f7b6d5fa573c171162697fe7c1
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-07-05 11:26:44 -0700

    merge permit work through rebase

--- packages/taquito-beacon-wallet/src/version.ts
+++ packages/taquito-beacon-wallet/src/version.ts
@@ -2,7 +2,11 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
+<<<<<<< HEAD
     "commitHash": "46ce21d6d08a6b1525118956e043f20200700ae8",
+=======
+    "commitHash": "60ced224797ae0a15c5212ce379f4ceb181effed",
+>>>>>>> 46ce21d6d... deal with conflicts
     "version": "9.1.1"
 };
 /* tslint:enable */
--- packages/taquito/src/version.ts
+++ packages/taquito/src/version.ts
@@ -4,6 +4,7 @@
 export const VERSION = {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     "commitHash": "a8e9548e650215186f661033b59299ace72c9ee6",
     "version": "9.2.0"
 =======
@@ -11,6 +12,9 @@ export const VERSION = {
 =======
     "commitHash": "46ce21d6d08a6b1525118956e043f20200700ae8",
 >>>>>>> c44b8add6... deal with conflicts
+=======
+    "commitHash": "60ced224797ae0a15c5212ce379f4ceb181effed",
+>>>>>>> 46ce21d6d... deal with conflicts
     "version": "9.1.1"
 >>>>>>> fb9ac31d3... deal with conflicts
 };
