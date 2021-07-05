8e0b41b882813071840b325aaca19e2a7a77a419
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-07-05 11:07:33 -0700

    merge permit work through rebase

--- packages/taquito-local-forging/src/version.ts
+++ packages/taquito-local-forging/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "a8e9548e650215186f661033b59299ace72c9ee6",
-    "version": "9.2.0"
+    "commitHash": "60ced224797ae0a15c5212ce379f4ceb181effed",
+    "version": "9.1.1"
 };
 /* tslint:enable */
--- packages/taquito-tezbridge-signer/src/version.ts
+++ packages/taquito-tezbridge-signer/src/version.ts
@@ -2,7 +2,12 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
+<<<<<<< HEAD
     "commitHash": "a8e9548e650215186f661033b59299ace72c9ee6",
     "version": "9.2.0"
+=======
+    "commitHash": "60ced224797ae0a15c5212ce379f4ceb181effed",
+    "version": "9.1.1"
+>>>>>>> fb9ac31d3... deal with conflicts
 };
 /* tslint:enable */
