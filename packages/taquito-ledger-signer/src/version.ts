3316c1bbc40f2f7151ff35908438fdd7db6da669
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-07-05 11:27:52 -0700

    merge permit work through rebase

--- packages/taquito-ledger-signer/src/version.ts
+++ packages/taquito-ledger-signer/src/version.ts
@@ -4,6 +4,7 @@
 export const VERSION = {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     "commitHash": "46ce21d6d08a6b1525118956e043f20200700ae8",
 =======
     "commitHash": "60ced224797ae0a15c5212ce379f4ceb181effed",
@@ -11,6 +12,9 @@ export const VERSION = {
 =======
     "commitHash": "46ce21d6d08a6b1525118956e043f20200700ae8",
 >>>>>>> 17caf8144... deal with conflicts
+=======
+    "commitHash": "1e4a03b126cdefaf970f298b5d3db43df228e2f9",
+>>>>>>> ff9065f06... more conflict resolutions
     "version": "9.1.1"
 };
 /* tslint:enable */
--- packages/taquito-tezbridge-wallet/src/version.ts
+++ packages/taquito-tezbridge-wallet/src/version.ts
@@ -6,6 +6,7 @@ export const VERSION = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     "commitHash": "a8e9548e650215186f661033b59299ace72c9ee6",
     "version": "9.2.0"
 =======
@@ -19,6 +20,9 @@ export const VERSION = {
 =======
     "commitHash": "46ce21d6d08a6b1525118956e043f20200700ae8",
 >>>>>>> 17caf8144... deal with conflicts
+=======
+    "commitHash": "1e4a03b126cdefaf970f298b5d3db43df228e2f9",
+>>>>>>> ff9065f06... more conflict resolutions
     "version": "9.1.1"
 >>>>>>> fb9ac31d3... deal with conflicts
 };
