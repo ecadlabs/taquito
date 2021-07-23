56904d7503c7fb2b2e7b22039f89281f2b62f476
Roxane Letourneau <roxane@ecadlabs.com>
2021-07-23 15:15:21 -0700

    chore(releng) bump version to 9.2.0-stablelib.0

--- packages/taquito-remote-signer/src/version.ts
+++ packages/taquito-remote-signer/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "38740772b5f1ae1f599256e0c1b9d4eb35b28497",
-    "version": "9.2.0"
+    "commitHash": "88089857fb6e5f9a7ebcec072cd4dbea9b3e4572",
+    "version": "9.2.0-stablelib.0"
 };
 /* tslint:enable */
