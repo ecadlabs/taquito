e51e2e62d1aba8f8ad5077ba7ec973f5628f69ce
Kunal Jaiswal <kunal@ecadlabs.com>
2021-08-03 12:51:01 -0600

    Issue-467 Flextesa support initial commit

--- integration-tests/config.ts
+++ integration-tests/config.ts
@@ -98,6 +98,21 @@ const florencenetEphemeral = {
   }
 }
 
+const sandboxProtocolEphemeral = florencenetEphemeral;
+
+const sandboxEphemeral = {
+  rpc: process.env['TEZOS_RPC_SANDBOX'] || 'http://localhost:8732',
+  knownBaker: sandboxProtocolEphemeral.knownBaker,
+  knownContract: 'KT1WtxvaDHanaLJQUej6GMFFNZ2bV9NFSjde',
+  knownBigMapContract: 'KT1QPThjY15EaJSngX5YiJHL1QBRKEgHxNcW',
+  protocol: sandboxProtocolEphemeral.protocol,
+  signerConfig: {
+    type: SignerType.EPHEMERAL_KEY as SignerType.EPHEMERAL_KEY,
+    keyUrl: 'https://api.tez.ie/keys/florencenet',
+    requestHeaders: { 'Authorization': 'Bearer taquito-example' },
+  }
+}
+
 // Well known faucet key. Can be overridden by setting the `TEZOS_FAUCET_KEY_FILE` environment variable
 const key = {
   email: "mfbzlhsv.owpfexem@tezos.example.org",
@@ -204,6 +219,9 @@ else if (process.env['FLORENCENET']) {
 }
 else if (process.env['HANGZHOUNET']) {
   providers.push(hangzhounetEphemeral)
+}
+else if (process.env['SANDBOX']) {
+  providers.push(sandboxEphemeral)
 } else {
   providers.push(florencenetEphemeral, hangzhounetEphemeral, granadanetEphemeral)
 }
