7917f71688ffbaff105db8d151122ed58685bf9c
Roxane Letourneau <roxane@ecadlabs.com>
2021-09-21 08:31:58 -0700

    Removed comments

--- packages/taquito-indexer/src/types-indexers.ts
+++ packages/taquito-indexer/src/types-indexers.ts
@@ -19,14 +19,12 @@ export interface OperationContentsCommon {
     block: string;
     timestamp: string;
     level: number;
-    sender: string; // sender always point to the caller, source is the implicit account that initiates tx
+    sender: string;
 }
 
 export interface OperationContentsEndorsement extends OperationContentsCommon {
-    // reward: BigNumber; // Missing in Tezgraph
-    // deposit: BigNumber; // Missing in Tezgraph
     delegate?: string;
-    slots: number // Number of assigned endorsement slots (out of 32) to the baker (delegate) who sent the operation
+    slots: number
 }
 
 export interface OperationContentsTransaction extends OperationContentsCommon {
@@ -37,7 +35,6 @@ export interface OperationContentsTransaction extends OperationContentsCommon {
     storage_limit?: BigNumber;
     amount?: BigNumber;
     parameters?: TransactionOperationParameter;
-    // storage_size?: BigNumber; // Missing in Tezgraph
     destination: string;
     consumed_gas: BigNumber;
 }
@@ -48,7 +45,6 @@ export interface OperationContentsReveal extends OperationContentsCommon {
     gas_limit?: BigNumber;
     consumed_gas?: BigNumber;
     fee: BigNumber;
-    // public_key: string; Missing in Tzkt
 }
 
 export interface OperationContentsOrigination extends OperationContentsCommon {
@@ -58,8 +54,6 @@ export interface OperationContentsOrigination extends OperationContentsCommon {
     gas_limit?: BigNumber;
     consumed_gas: BigNumber;
     storage_limit?: BigNumber; 
-    // storage_size?: BigNumber; // Missing in Tezgraph
-    // balance: BigNumber; // Missing in Tezgraph
     contract_address: string;
 }
 
@@ -69,40 +63,39 @@ export interface OperationContentsDelegation extends OperationContentsCommon {
     counter?: BigNumber;
     gas_limit?: BigNumber;
     consumed_gas?: BigNumber;
-    // amount: BigNumber; // Missing in Tezgraph
     delegate?: string;
 }
 
-export interface OperationContentsActivateAccount extends OperationContentsCommon { // Not supported by the current Tezgraph version
+export interface OperationContentsActivateAccount extends OperationContentsCommon { 
     kind: OpKind.ACTIVATION;
     balance: BigNumber; 
 }
 
-export interface OperationContentsSeedNonceRevelation extends OperationContentsCommon { // Not supported by the current Tezgraph version
+export interface OperationContentsSeedNonceRevelation extends OperationContentsCommon { 
     kind: OpKind.SEED_NONCE_REVELATION;
     reward: BigNumber;
     receiver: string;
     revealed_level: number;
 }
 
-export interface OperationContentsDoubleEndorsement extends OperationContentsCommon { // Not supported by the current Tezgraph version
+export interface OperationContentsDoubleEndorsement extends OperationContentsCommon { 
     kind: OpKind.DOUBLE_ENDORSEMENT_EVIDENCE;
     receiver: string;
     accused_level: number;
 }
  
-export interface OperationContentsDoubleBaking extends OperationContentsCommon { // Not supported by the current Tezgraph version
+export interface OperationContentsDoubleBaking extends OperationContentsCommon { 
     kind: OpKind.DOUBLE_BAKING_EVIDENCE;
     receiver: string; 
     accused_level: number;
 }
 
-export interface OperationContentsProposals extends OperationContentsCommon { // Not supported by the current Tezgraph version
+export interface OperationContentsProposals extends OperationContentsCommon { 
     kind: OpKind.PROPOSALS;
     proposal_hash: string;
 }
 
-export interface OperationContentsBallot extends OperationContentsCommon { // Not supported by the current Tezgraph version
+export interface OperationContentsBallot extends OperationContentsCommon { 
     kind: OpKind.BALLOT;
     proposal_hash: string;
     vote: OperationContentsBallotEnum;
