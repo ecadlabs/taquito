c28ecbedd8b55c54fc41f8b1fd6d34c9bed4a437
Kunal Jaiswal <kunal@ecadlabs.com>
2021-09-22 22:44:33 -0600

    Issue-457 Added dynamic runner name

--- integration-tests/config.ts
+++ integration-tests/config.ts
@@ -110,7 +110,7 @@ const sandboxEphemeral = {
   protocol: sandboxProtocolEphemeral.protocol,
   signerConfig: {
     type: SignerType.EPHEMERAL_KEY as SignerType.EPHEMERAL_KEY,
-    keyUrl: 'http://flexteza-runner-1-1:3000/flextesanet',
+    keyUrl: 'http://runner_name:3000/flextesanet',
     requestHeaders: { 'Authorization': 'Bearer taquito-example' },
   },
   isSandbox: true
