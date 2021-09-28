09da77b8945c524c704b22c1d5cc50b2d9a35135
Kunal Jaiswal <kunal@ecadlabs.com>
2021-09-28 15:23:57 -0600

    Issue-457 Fixed issue with integration tests config - added 3 retries

--- integration-tests/config.ts
+++ integration-tests/config.ts
@@ -221,7 +221,7 @@ const setupSignerWithFreshKey = async (
   let count = 0;
   const maxTries = 3;
   
-  while(true) {
+  while(count != maxTries) {
     try {
       const key = await httpClient.createRequest<string>({
         url: keyUrl,
