e6c76a0465d905c25ac267d3b9b392c1bf255c04
Kunal Jaiswal <kunal@ecadlabs.com>
2021-09-28 12:05:42 -0600

    Rebase

--- packages/taquito-indexer/src/version.ts
+++ packages/taquito-indexer/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "a3a152f48cd0ecccbc934b0466fa908b7ec464ce",
-    "version": "10.1.0"
+    "commitHash": "5f3d9f126aae791fefb2262ffeee5938560b1288",
+    "version": "10.1.2"
 };
 /* tslint:enable */
--- packages/taquito/src/version.ts
+++ packages/taquito/src/version.ts
@@ -2,16 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    "commitHash": "eb5b2c4973cd11098b68ce62eb6702dea376e569",
+    "commitHash": "5f3d9f126aae791fefb2262ffeee5938560b1288",
     "version": "10.1.2"
-=======
-    "commitHash": "388f8ef6919651d13b1a5b4e48c14e4fdf16fa28",
-=======
-    "commitHash": "a2ea15cb3b4dc548c202d0d62161979cca2f011e",
->>>>>>> 1254fc9f9 (Issue-457 More CI job changes)
-    "version": "10.0.0"
->>>>>>> a2ea15cb3 (Issue-457 All tests passing)
 };
 /* tslint:enable */
