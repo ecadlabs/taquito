7d918f90e477455c36ca742fc102a8510878ade2
Kunal Jaiswal <kunal@ecadlabs.com>
2021-10-04 08:47:43 -0600

    Issue-457 Replaced packaged tezos client with binary

--- packages/taquito-tezbridge-signer/src/version.ts
+++ packages/taquito-tezbridge-signer/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "b4f66211d84bc2e04ef3aa4206b3611cb91c188f",
+    "commitHash": "beb8cd4fec9941e8ab08b13f935a065f9b2fe9c1",
     "version": "10.1.2"
 };
 /* tslint:enable */
