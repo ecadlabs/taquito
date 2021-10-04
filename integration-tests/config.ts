0ab58f3a9d507b6c12cb3e7edc2808ff02f9a25e
Kunal Jaiswal <kunal@ecadlabs.com>
2021-10-04 12:25:11 -0600

    Issue-457 Replaced packaged tezos client with binary

--- integration-tests/config.ts
+++ integration-tests/config.ts
@@ -232,7 +232,7 @@ const setupSignerWithFreshKey = async (
       const signer = new InMemorySigner(key!);
       Tezos.setSignerProvider(signer);
     } catch (e) {
-      jest.setTimeout(3000);
+      //jest.setTimeout(3000);
       if(++count == retries) console.log("An error occurs when trying to fetch a fresh key:", e)
     }
   }
@@ -260,7 +260,7 @@ const setupSignerWithEphemeralKey = async (
       Tezos.setSignerProvider(signer);
 
     } catch (e) {
-      jest.setTimeout(3000);
+      //jest.setTimeout(3000);
       if(++count == retries) console.log("An error occurs when trying to fetch an ephemeral key:", e)
     }
   }
