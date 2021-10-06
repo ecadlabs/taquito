09b5fe06913e5445e4e131d81c054a20e8f42f4f
Kunal Jaiswal <kunal@ecadlabs.com>
2021-10-06 16:25:24 -0600

    Issue-457 Removed delay

--- integration-tests/config.ts
+++ integration-tests/config.ts
@@ -248,10 +248,10 @@ const setupSignerWithFreshKey = async (
   { keyUrl, requestHeaders }: EphemeralConfig
 ) => {
   const httpClient = new HttpBackend();
-  let count=0;
-  const retries=3;
+  // let count=0;
+  // const retries=3;
   
-  while(count != retries) {
+  // while(count != retries) {
     try {
         const key = await httpClient.createRequest<string>({
           url: keyUrl,
@@ -262,10 +262,10 @@ const setupSignerWithFreshKey = async (
         const signer = new InMemorySigner(key!);
         Tezos.setSignerProvider(signer);
       } catch (e) {
-        await new Promise((r) => setTimeout(r, 3000));
-        if(++count == retries) console.log("An error occurs when trying to fetch a fresh key:", e)
+        // await new Promise((r) => setTimeout(r, 3000));
+        console.log("An error occurs when trying to fetch a fresh key:", e)
     }
-  }
+  // }
 };
 
 const setupSignerWithEphemeralKey = async (
@@ -274,10 +274,10 @@ const setupSignerWithEphemeralKey = async (
 ) => {
   const ephemeralUrl = `${keyUrl}/ephemeral`;
   const httpClient = new HttpBackend();
-  let count=0;
-  const retries=3;
+  // let count=0;
+  // const retries=3;
   
-  while(count != retries) {
+  // while(count != retries) {
     try {
       const { id, pkh } = await httpClient.createRequest({
         url: ephemeralUrl,
@@ -289,10 +289,10 @@ const setupSignerWithEphemeralKey = async (
       Tezos.setSignerProvider(signer);
 
     } catch (e) {
-      await new Promise((r) => setTimeout(r, 3000));
-      if(++count == retries) console.log("An error occurs when trying to fetch an ephemeral key:", e)
+      // await new Promise((r) => setTimeout(r, 3000));
+      console.log("An error occurs when trying to fetch an ephemeral key:", e)
     }
-  }
+  // }
 };
 
 const setupWithFaucetKey = async (Tezos: TezosToolkit, signerConfig: FaucetConfig) => {
