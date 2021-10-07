883ee7d5be14dbfce021a589918807784d2622c3
Georgiy Bukharov <souljorje@gmail.com>
2021-10-07 19:03:51 +0400

    refactor: replace singleton lodash with separate packages
    save bundle and fix vulnerability https://github.com/advisories/GHSA-8p5q-j9m2-g8wr

--- packages/taquito-http-utils/src/version.ts
+++ packages/taquito-http-utils/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "68be71fd14ffd87bd0a0f91dab60d69c8665e88c",
+    "commitHash": "8a736ba2e8c83a3f998a8511c27d7609a9bc38d5",
     "version": "10.2.0"
 };
 /* tslint:enable */
