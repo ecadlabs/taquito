0d7d48d3dd589b0e115f5d427a4c231ad730e069
Roxane Letourneau <roxane@ecadlabs.com>
2021-10-08 14:08:03 -0700

    New tokens for chest and chest_key in Michelson-encoder package

---
+++ packages/taquito-michelson-encoder/src/tokens/chest-key.ts
@@ -0,0 +1,58 @@
+import { Token, TokenFactory, TokenValidationError } from './token';
+
+export class ChestKeyValidationError extends TokenValidationError {
+  name: string = 'ChestKeyValidationError';
+  constructor(public value: any, public token: ChestKeyToken, message: string) {
+    super(value, token, message);
+  }
+}
+export class ChestKeyToken extends Token {
+  static prim = 'chest_key';
+
+  constructor(
+    protected val: { prim: string; args: any[]; annots: any[] },
+    protected idx: number,
+    protected fac: TokenFactory
+  ) {
+    super(val, idx, fac);
+  }
+
+  private isValid(val: any): ChestKeyValidationError | null {
+    if (/^[0-9a-fA-F]*$/.test(val) && val.length % 2 === 0) {
+      return null;
+    } else {
+      return new ChestKeyValidationError(val, this, `Invalid bytes: ${val}`);
+    }
+  }
+
+  private convertUint8ArrayToHexString(val: any) {
+    return val.constructor === Uint8Array ? Buffer.from(val).toString('hex') : val;
+  }
+
+  Encode(args: any[]) {
+    let val = args.pop();
+    val = this.convertUint8ArrayToHexString(val);
+    const err = this.isValid(val);
+    if (err) {
+      throw err;
+    }
+    return { bytes: val };
+  }
+
+  EncodeObject(val: string | Uint8Array) {
+    val = this.convertUint8ArrayToHexString(val);
+    const err = this.isValid(val);
+    if (err) {
+      throw err;
+    }
+    return { bytes: val };
+  }
+
+  Execute(val: any): string {
+    return val.bytes;
+  }
+
+  public ExtractSchema() {
+    return ChestKeyToken.prim;
+  }
+}
