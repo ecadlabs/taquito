a5b5f9116e1bc178d623aed32dfaf2e64a282a28
Kunal Jaiswal <kunal@ecadlabs.com>
2021-10-13 10:13:03 -0600

    Issue-457 Added Tzip12BigMapsOffchain generation

--- example/deploy-flextesa-contracts.ts
+++ example/deploy-flextesa-contracts.ts
@@ -3,9 +3,24 @@ import { importKey } from '@taquito/signer';
 import { knownContract } from './data/knownContract';
 import { knownBigMapContract } from './data/knownBigMapContract';
 import { VIEW_LAMBDA } from '../packages/taquito/src/contract/view_lambda';
+import { fa2ForTokenMetadataView } from '../integration-tests/data/fa2-for-token-metadata-view';
+import { b58cencode, char2Bytes, Prefix, prefix } from '@taquito/utils';
+
 const { exec } = require("child_process");
 
 const provider = 'http://macmini:20000';
+
+async function createAddress() {
+  const tezos = new TezosToolkit(provider)
+
+  const keyBytes = Buffer.alloc(32);
+  nodeCrypto.randomFillSync(keyBytes)
+
+  const key = b58cencode(new Uint8Array(keyBytes), prefix[Prefix.P2SK]);
+  await importKey(tezos, key);
+
+  return tezos;
+}
 
 async function example() {
   const tezos = new TezosToolkit(provider);
@@ -85,6 +100,85 @@ async function example() {
     const lambdaContractAddress = lambdaContract.address
     console.log(lambdaContractAddress);
 
+    console.log('Deploying Tzip12BigMapsOffChain contract...');
+    
+    const LocalTez1 = await createAddress();
+    const localTez1Pkh = await LocalTez1.signer.publicKeyHash();
+    const LocalTez2 = await createAddress();
+    const localTez2Pkh = await LocalTez2.signer.publicKeyHash();
+
+    const ledger = new MichelsonMap();
+    ledger.set(
+      {
+        0: localTez1Pkh,
+        1: 0
+      },
+      '20000'
+    );
+    ledger.set(
+      {
+        0: localTez2Pkh,
+        1: 1
+      },
+      '20000'
+    );
+
+    const url = 'https://storage.googleapis.com/tzip-16/fa2-views.json';
+    const bytesUrl = char2Bytes(url);
+    const metadata = new MichelsonMap();
+    metadata.set('', bytesUrl);
+
+    const operators = new MichelsonMap();
+
+    const tokens = new MichelsonMap();
+    const metadataMap0 = new MichelsonMap();
+    metadataMap0.set('', char2Bytes('https://storage.googleapis.com/tzip-16/token-metadata.json'));
+    metadataMap0.set('name', char2Bytes('Name from URI is prioritized!'));
+    const metadataMap1 = new MichelsonMap();
+    metadataMap1.set('name', char2Bytes('AliceToken'));
+    metadataMap1.set('symbol', char2Bytes('ALC'));
+    metadataMap1.set('decimals', '30');
+    metadataMap1.set('extra', char2Bytes('Add more data'));
+    const metadataMap2 = new MichelsonMap();
+    metadataMap2.set('name', char2Bytes('Invalid token metadata'));
+    tokens.set('0', {
+      metadata_map: metadataMap0,
+      total_supply: '20000'
+    });
+    tokens.set('1', {
+      metadata_map: metadataMap1,
+      total_supply: '20000'
+    });
+    tokens.set('2', {
+      metadata_map: metadataMap2,
+      total_supply: '20000'
+    });
+
+
+    const op12 = await tezos.contract.originate({
+      code: fa2ForTokenMetadataView,
+      storage: {
+        administrator: await tezos.signer.publicKeyHash(),
+        all_tokens: '2',
+        ledger,
+        metadata,
+        operators,
+        paused: false,
+        tokens
+      }
+    });
+    await op12.confirmation();
+
+  console.log('Awaiting confirmation...');
+  const contract12 = await op12.contract();
+  console.log('Tzip12BigMapsOffChain Contract address',contract12.address)
+  console.log('Gas Used', op12.consumedGas);
+  console.log('Storage Paid', op12.storageDiff);
+  console.log('Storage Size', op12.storageSize);
+  console.log('Storage', await contract12.storage());
+  console.log('Operation hash:', op.hash, 'Included in block level:', op12.includedInBlock);
+
+
     exec("cd ../integration-tests && sed -i 's/sandbox_known_contract/"+contractknownContract.address+"/g' config.ts", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
@@ -98,15 +192,27 @@ async function example() {
     });
 
     exec("cd ../integration-tests && sed -i 's/sandbox_known_bigmap_contract/"+contractknownBigMapContract.address+"/g' config.ts", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log("Integration tests sandbox config updated with know bigmap contract: "+contractknownBigMapContract.address);
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+      }
+      console.log("Integration tests sandbox config updated with know bigmap contract: "+contractknownBigMapContract.address);
+    });
+
+    exec("cd ../integration-tests && sed -i 's/sandbox_known_tzip1216_contract/"+contract12.address+"/g' config.ts", (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+      }
+      console.log("Integration tests sandbox config updated with Tzip12BigMapsOffChain contract address: "+contract12.address);
     });
 
     exec("cd ../packages/taquito/src && sed -i 's/flextesa_default_lambda_address/"+lambdaContractAddress+"/g' constants.ts", (error, stdout, stderr) => {
