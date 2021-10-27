d12854ec2da568adceca0e28efd4055a99794fd5
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-10-27 09:13:31 -0700

    installed

--- packages/taquito/src/version.ts
+++ packages/taquito/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "68be71fd14ffd87bd0a0f91dab60d69c8665e88c",
+    "commitHash": "2607fbcd72457e765e22b077082c9b74e21911ea",
     "version": "10.2.0"
 };
 /* tslint:enable */
