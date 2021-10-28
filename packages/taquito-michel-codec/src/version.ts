3c5f656c6f581a6fa61f99a54929bf35a1c93266
Eugene Zagidullin <eugene@ecadlabs.com>
2021-10-28 20:37:48 +0300

    Lock TypeScript to 4.1.x

--- packages/taquito-michel-codec/src/version.ts
+++ packages/taquito-michel-codec/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "824566380a9e36a86e61ad79cff631a552280500",
+    "commitHash": "94a117312532a72191d794be9df64572c21e750b",
     "version": "10.2.0"
 };
 /* tslint:enable */
