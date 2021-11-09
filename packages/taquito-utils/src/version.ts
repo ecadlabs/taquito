412fd73d7c7d3ebeafc2976314cf4235a5bbb16d
Roxane Letourneau <roxane@ecadlabs.com>
2021-11-09 15:52:49 -0800

    Fixed package-locks

--- packages/taquito-utils/src/version.ts
+++ packages/taquito-utils/src/version.ts
@@ -2,12 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-<<<<<<< HEAD
-    "commitHash": "d12854ec2da568adceca0e28efd4055a99794fd5",
-    "version": "10.2.0"
-=======
-    "commitHash": "9774316a5cc2f7abfad1e668a096b9fe324bf255",
+    "commitHash": "a231fc484350468844a41ef6167d2ecfeb9bbd06",
     "version": "11.0.0-beta-RC.0"
->>>>>>> master
 };
 /* tslint:enable */
