4411c2b126f5cfd98fa82b828c5b5d4f3f23e017
Roxane Letourneau <roxane@ecadlabs.com>
2021-11-09 11:52:06 -0800

    chore(releng) bump version to 11.0.0-beta-RC.0

--- packages/taquito-michel-codec/src/version.ts
+++ packages/taquito-michel-codec/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "94a117312532a72191d794be9df64572c21e750b",
-    "version": "10.2.0"
+    "commitHash": "9774316a5cc2f7abfad1e668a096b9fe324bf255",
+    "version": "11.0.0-beta-RC.0"
 };
 /* tslint:enable */
