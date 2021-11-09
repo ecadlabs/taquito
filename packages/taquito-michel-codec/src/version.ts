433cfec1e3a57a2d9ce07e25436e3189833f9beb
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-11-09 15:16:48 -0800

    add version changes

--- packages/taquito-michel-codec/src/version.ts
+++ packages/taquito-michel-codec/src/version.ts
@@ -2,16 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    "commitHash": "d12854ec2da568adceca0e28efd4055a99794fd5",
-=======
-    "commitHash": "94a117312532a72191d794be9df64572c21e750b",
->>>>>>> master
-    "version": "10.2.0"
-=======
-    "commitHash": "9774316a5cc2f7abfad1e668a096b9fe324bf255",
+    "commitHash": "a231fc484350468844a41ef6167d2ecfeb9bbd06",
     "version": "11.0.0-beta-RC.0"
->>>>>>> master
 };
 /* tslint:enable */
