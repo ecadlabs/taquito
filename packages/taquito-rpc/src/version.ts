ebf17c580c8e1087836f6b2b6a22763fed31ab26
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-11-09 15:11:15 -0800

    accet changes from master

--- packages/taquito-rpc/src/version.ts
+++ packages/taquito-rpc/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "9774316a5cc2f7abfad1e668a096b9fe324bf255",
+    "commitHash": "8d0b57a0ec42bd570a8fce83fe8da3fbb7977b1b",
     "version": "11.0.0-beta-RC.0"
 };
 /* tslint:enable */
