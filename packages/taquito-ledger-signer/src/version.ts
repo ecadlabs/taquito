6a6aa2914ff146006b0c975eba8557b7be318149
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-11-12 10:36:49 -0800

    trying to fix build

--- packages/taquito-ledger-signer/src/version.ts
+++ packages/taquito-ledger-signer/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "ebf17c580c8e1087836f6b2b6a22763fed31ab26",
+    "commitHash": "a2419574b999ecf64d42cdce85f669fb07141bf0",
     "version": "11.0.0-beta-RC.0"
 };
 /* tslint:enable */
