c558fa5d7bd378a9ed3079cbb5bb8865f3e45d2f
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-11-17 16:44:42 -0800

    redo package-lock.json for website package

--- packages/taquito-tezbridge-signer/src/version.ts
+++ packages/taquito-tezbridge-signer/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "f7dbf5647ce1a35266ad921e6dd2aeef0b4aff93",
+    "commitHash": "cf0c22b58cdfbf5a947f7c2b5525e581202388d6",
     "version": "11.0.0"
 };
 /* tslint:enable */
