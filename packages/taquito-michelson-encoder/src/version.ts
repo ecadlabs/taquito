7ac079d5918ee3c98ca3c24daf4d1dee0c01986e
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-11-17 16:50:42 -0800

    redo package-lock.json for other packages

--- packages/taquito-michelson-encoder/src/version.ts
+++ packages/taquito-michelson-encoder/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "cf0c22b58cdfbf5a947f7c2b5525e581202388d6",
+    "commitHash": "c558fa5d7bd378a9ed3079cbb5bb8865f3e45d2f",
     "version": "11.0.0"
 };
 /* tslint:enable */
