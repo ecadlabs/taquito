791bf6aec6c0694b755ecf22b01b3e09a5206b76
roxaneletourneau <roxane@ecadlabs.com>
2021-11-30 17:08:40 -0800

    chore(releng) bump version to 11.0.2

--- packages/taquito-utils/src/version.ts
+++ packages/taquito-utils/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "b24f5a90e164be2903c4cc5fdc244f509534d24a",
-    "version": "11.0.1"
+    "commitHash": "e4fb2f0ab9a459640e474bf65ae4578f4b948c63",
+    "version": "11.0.2"
 };
 /* tslint:enable */
