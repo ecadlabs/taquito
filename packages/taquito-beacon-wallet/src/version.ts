47102f94199943b2cc435ead3967851648a0bc79
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-12-06 13:43:13 -0800

    fix conflict

--- packages/taquito-beacon-wallet/src/version.ts
+++ packages/taquito-beacon-wallet/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "d1ae52d5c5b1d1ec5a680f755b996718f1bfc48b",
+    "commitHash": "1d78d3b2a87231c96aa5b689a390aee96db9093a",
     "version": "11.0.2"
 };
 /* tslint:enable */
