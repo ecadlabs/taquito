34c1188a4e00a0ee890901be77d7beb55ab64bff
Davis Sawali <dsawali@Daviss-MacBook-Pro.local>
2021-12-07 22:32:59 -0800

    updgraded versions docusaurus package versions and made adjustments

---
+++ website/plugins/webpack5plugin/index.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line
+module.exports = function (context, options) {
+  return {
+    name: 'webpack5-plugin',
+    // eslint-disable-next-line
+    configureWebpack(config, isServer, utils) {
+      return {
+        resolve: {
+          fallback: {
+            fs: false,
+            http: false,
+            https: false,
+            os: false,
+            path: false,
+            stream: false,
+            crypto: false
+          },
+        },
+      };
+    },
+  };
+};
