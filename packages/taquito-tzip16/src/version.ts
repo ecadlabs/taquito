adbe6a3e25c623b5a32bec7113866da5b75fe9bb
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-12-22 14:03:26 -0800

    fixes for contract.ts types

--- packages/taquito-tzip16/src/version.ts
+++ packages/taquito-tzip16/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "e1723ab725df70755b4cd73b75aa981f913d0d5c",
+    "commitHash": "e88dba370cc3cfac5246d0f3d4db33b0868ffbc3",
     "version": "11.1.0"
 };
 /* tslint:enable */
