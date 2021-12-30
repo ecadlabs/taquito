13ce9d6b695c1d3e53239932881500ca17abee66
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-12-30 04:50:00 -0800

    resolve merge conflicts

--- packages/taquito-tzip16/src/version.ts
+++ packages/taquito-tzip16/src/version.ts
@@ -2,7 +2,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "commitHash": "e88dba370cc3cfac5246d0f3d4db33b0868ffbc3",
+    "commitHash": "eb8e26c3074789c651ebdfbe30ddecbeee6c4034",
     "version": "11.1.0"
 };
 /* tslint:enable */
