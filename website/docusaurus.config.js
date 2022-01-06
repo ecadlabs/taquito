1e860e5ce7294bdb886bc9031b4d0a1eaa19a4b0
GImbrailo <ginoimbrailo@gmail.com>
2022-01-06 08:25:05 -0800

    Fix cloudflare typedoc builds (#1287)
    * chore: changed includeCurrentVersion to ture
    
    * chore: moved folder back to project root directory
    
    * Renamed deploy package job
    
    * chore: changed docs path id docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -110,9 +110,9 @@ module.exports = {
           customCss: require.resolve('./src/css/custom.css'),
         },
         docs: {
-          path: './docs',
+          path: '../docs',
           sidebarPath: require.resolve('./sidebars.json'),
-          includeCurrentVersion: false
+          includeCurrentVersion: true
         }
       }
     ],
