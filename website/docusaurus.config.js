5bd6a3a1bdebee5ee00ebd919024ad14fd9ba926
Davis Sawali <dsawali@Daviss-MacBook-Pro.local>
2022-01-06 17:20:00 -0800

    Enabled contextualSearch in algolia search configs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -107,6 +107,7 @@ module.exports = {
       apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
       indexName: 'taquito',
       appId: process.env.ALGOLIA_APPLICATION_ID,
+      contextualSearch: true
     }
   },
   presets: [
