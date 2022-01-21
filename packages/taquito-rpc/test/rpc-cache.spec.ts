bb1c71975d7a70561ace7401331fb7665ce4f236
Davis Sawali <davis.sawali@ecadlabs.com>
2022-01-21 14:07:56 -0800

    Added more internal validation to various packages

--- packages/taquito-rpc/test/rpc-cache.spec.ts
+++ packages/taquito-rpc/test/rpc-cache.spec.ts
@@ -38,6 +38,9 @@ describe('RpcClientCache test', () => {
   let rpcCache: RpcClientCache;
   let mockRpcClient: any;
 
+  const address = 'tz1QZ6KY7d3BuZDT1d19dUxoQrtFPN2QJ3hn';
+  const contractAddress = 'KT1Fe71jyjrxFg9ZrYqtvaX7uQjcLo7svE4D';
+
   beforeEach(() => {
     mockRpcClient = {
       getRpcUrl: jest.fn(),
@@ -116,15 +119,15 @@ describe('RpcClientCache test', () => {
     await rpcCache.getBlockHash();
     await rpcCache.getBlock();
     await rpcCache.getLiveBlocks();
-    await rpcCache.getBalance('address');
-    await rpcCache.getStorage('address');
-    await rpcCache.getScript('address');
-    await rpcCache.getNormalizedScript('address');
-    await rpcCache.getContract('address');
-    await rpcCache.getManagerKey('address');
-    await rpcCache.getDelegate('address');
+    await rpcCache.getBalance(address);
+    await rpcCache.getStorage(contractAddress);
+    await rpcCache.getScript(contractAddress);
+    await rpcCache.getNormalizedScript(contractAddress);
+    await rpcCache.getContract(contractAddress);
+    await rpcCache.getManagerKey(contractAddress);
+    await rpcCache.getDelegate(address);
     await rpcCache.getBigMapExpr('72', 'expruPtxxirR4BVqFH43VcmEFZqHaQHJhZQDRVTMgSYAGGgBhBRxfp');
-    await rpcCache.getDelegates('address');
+    await rpcCache.getDelegates(address);
     await rpcCache.getConstants();
     await rpcCache.getBlockHeader();
     await rpcCache.getBlockMetadata();
@@ -137,7 +140,7 @@ describe('RpcClientCache test', () => {
     await rpcCache.getCurrentQuorum();
     await rpcCache.getVotesListings();
     await rpcCache.getProposals();
-    await rpcCache.getEntrypoints('address');
+    await rpcCache.getEntrypoints(contractAddress);
     await rpcCache.getChainId();
     await rpcCache.packData({
       data: { bytes: '0000b24ac1e1759565d5c9b69af8450ce7ea3d1ee64c' },
@@ -149,25 +152,25 @@ describe('RpcClientCache test', () => {
     expect(rpcCache.getAllCachedData()['rpcTest/getBlockHash/head/'].response).toEqual(blockHash);
     expect(rpcCache.getAllCachedData()['rpcTest/getBlock/head/'].response).toEqual(blockResponse);
     expect(rpcCache.getAllCachedData()['rpcTest/getLiveBlocks/head/'].response).toEqual(liveBlocks);
-    expect(rpcCache.getAllCachedData()['rpcTest/getBalance/head/address/'].response).toEqual(
+    expect(rpcCache.getAllCachedData()[`rpcTest/getBalance/head/${address}/`].response).toEqual(
       balance
     );
-    expect(rpcCache.getAllCachedData()['rpcTest/getStorage/head/address/'].response).toEqual(
+    expect(rpcCache.getAllCachedData()[`rpcTest/getStorage/head/${contractAddress}/`].response).toEqual(
       storage
     );
-    expect(rpcCache.getAllCachedData()['rpcTest/getScript/head/address/'].response).toEqual(script);
+    expect(rpcCache.getAllCachedData()[`rpcTest/getScript/head/${contractAddress}/`].response).toEqual(script);
     expect(
       rpcCache.getAllCachedData()[
-        'rpcTest/getNormalizedScript/head/address/{"unparsing_mode":"Readable"}/'
+        `rpcTest/getNormalizedScript/head/${contractAddress}/{"unparsing_mode":"Readable"}/`
       ].response
     ).toEqual(script);
-    expect(rpcCache.getAllCachedData()['rpcTest/getContract/head/address/'].response).toEqual(
+    expect(rpcCache.getAllCachedData()[`rpcTest/getContract/head/${contractAddress}/`].response).toEqual(
       contract
     );
-    expect(rpcCache.getAllCachedData()['rpcTest/getManagerKey/head/address/'].response).toEqual(
+    expect(rpcCache.getAllCachedData()[`rpcTest/getManagerKey/head/${contractAddress}/`].response).toEqual(
       managerKey
     );
-    expect(rpcCache.getAllCachedData()['rpcTest/getDelegate/head/address/'].response).toEqual(
+    expect(rpcCache.getAllCachedData()[`rpcTest/getDelegate/head/${address}/`].response).toEqual(
       delegate
     );
     expect(
@@ -175,7 +178,7 @@ describe('RpcClientCache test', () => {
         'rpcTest/getBigMapExpr/head/72/expruPtxxirR4BVqFH43VcmEFZqHaQHJhZQDRVTMgSYAGGgBhBRxfp/'
       ].response
     ).toEqual(bigmapValue);
-    expect(rpcCache.getAllCachedData()['rpcTest/getDelegates/head/address/'].response).toEqual(
+    expect(rpcCache.getAllCachedData()[`rpcTest/getDelegates/head/${address}/`].response).toEqual(
       delegates
     );
     expect(rpcCache.getAllCachedData()['rpcTest/getConstants/head/'].response).toEqual(constants);
@@ -202,7 +205,7 @@ describe('RpcClientCache test', () => {
     expect(rpcCache.getAllCachedData()['rpcTest/getVotesListings/head/'].response).toEqual(
       votesListing
     );
-    expect(rpcCache.getAllCachedData()['rpcTest/getEntrypoints/head/address/'].response).toEqual(
+    expect(rpcCache.getAllCachedData()[`rpcTest/getEntrypoints/head/${contractAddress}/`].response).toEqual(
       entryPoints
     );
     expect(rpcCache.getAllCachedData()['rpcTest/getChainId/'].response).toEqual(chainId);
@@ -227,19 +230,19 @@ describe('RpcClientCache test', () => {
     await rpcCache.getBlockHash(block);
     await rpcCache.getBlock(block);
     await rpcCache.getLiveBlocks(block);
-    await rpcCache.getBalance('address', block);
-    await rpcCache.getStorage('address', block);
-    await rpcCache.getScript('address', block);
-    await rpcCache.getNormalizedScript('address', { unparsing_mode: 'Readable' }, block);
-    await rpcCache.getContract('address', block);
-    await rpcCache.getManagerKey('address', block);
-    await rpcCache.getDelegate('address', block);
+    await rpcCache.getBalance(address, block);
+    await rpcCache.getStorage(contractAddress, block);
+    await rpcCache.getScript(contractAddress, block);
+    await rpcCache.getNormalizedScript(contractAddress, { unparsing_mode: 'Readable' }, block);
+    await rpcCache.getContract(contractAddress, block);
+    await rpcCache.getManagerKey(contractAddress, block);
+    await rpcCache.getDelegate(address, block);
     await rpcCache.getBigMapExpr(
       '72',
       'expruPtxxirR4BVqFH43VcmEFZqHaQHJhZQDRVTMgSYAGGgBhBRxfp',
       block
     );
-    await rpcCache.getDelegates('address', block);
+    await rpcCache.getDelegates(address, block);
     await rpcCache.getConstants(block);
     await rpcCache.getBlockHeader(block);
     await rpcCache.getBlockMetadata(block);
@@ -252,7 +255,7 @@ describe('RpcClientCache test', () => {
     await rpcCache.getCurrentQuorum(block);
     await rpcCache.getVotesListings(block);
     await rpcCache.getProposals(block);
-    await rpcCache.getEntrypoints('address', block);
+    await rpcCache.getEntrypoints(contractAddress, block);
     await rpcCache.getChainId();
     await rpcCache.packData(
       { data: { bytes: '0000b24ac1e1759565d5c9b69af8450ce7ea3d1ee64c' }, type: { prim: 'bytes' } },
@@ -271,27 +274,27 @@ describe('RpcClientCache test', () => {
       liveBlocks
     );
     expect(
-      rpcCache.getAllCachedData()[`rpcTest/getBalance/${block.block}/address/`].response
+      rpcCache.getAllCachedData()[`rpcTest/getBalance/${block.block}/${address}/`].response
     ).toEqual(balance);
     expect(
-      rpcCache.getAllCachedData()[`rpcTest/getStorage/${block.block}/address/`].response
+      rpcCache.getAllCachedData()[`rpcTest/getStorage/${block.block}/${contractAddress}/`].response
     ).toEqual(storage);
     expect(
-      rpcCache.getAllCachedData()[`rpcTest/getScript/${block.block}/address/`].response
+      rpcCache.getAllCachedData()[`rpcTest/getScript/${block.block}/${contractAddress}/`].response
     ).toEqual(script);
     expect(
       rpcCache.getAllCachedData()[
-        `rpcTest/getNormalizedScript/${block.block}/address/{"unparsing_mode":"Readable"}/`
+        `rpcTest/getNormalizedScript/${block.block}/${contractAddress}/{"unparsing_mode":"Readable"}/`
       ].response
     ).toEqual(script);
     expect(
-      rpcCache.getAllCachedData()[`rpcTest/getContract/${block.block}/address/`].response
+      rpcCache.getAllCachedData()[`rpcTest/getContract/${block.block}/${contractAddress}/`].response
     ).toEqual(contract);
     expect(
-      rpcCache.getAllCachedData()[`rpcTest/getManagerKey/${block.block}/address/`].response
+      rpcCache.getAllCachedData()[`rpcTest/getManagerKey/${block.block}/${contractAddress}/`].response
     ).toEqual(managerKey);
     expect(
-      rpcCache.getAllCachedData()[`rpcTest/getDelegate/${block.block}/address/`].response
+      rpcCache.getAllCachedData()[`rpcTest/getDelegate/${block.block}/${address}/`].response
     ).toEqual(delegate);
     expect(
       rpcCache.getAllCachedData()[
@@ -299,7 +302,7 @@ describe('RpcClientCache test', () => {
       ].response
     ).toEqual(bigmapValue);
     expect(
-      rpcCache.getAllCachedData()[`rpcTest/getDelegates/${block.block}/address/`].response
+      rpcCache.getAllCachedData()[`rpcTest/getDelegates/${block.block}/${address}/`].response
     ).toEqual(delegates);
     expect(rpcCache.getAllCachedData()[`rpcTest/getConstants/${block.block}/`].response).toEqual(
       constants
@@ -333,7 +336,7 @@ describe('RpcClientCache test', () => {
       rpcCache.getAllCachedData()[`rpcTest/getVotesListings/${block.block}/`].response
     ).toEqual(votesListing);
     expect(
-      rpcCache.getAllCachedData()[`rpcTest/getEntrypoints/${block.block}/address/`].response
+      rpcCache.getAllCachedData()[`rpcTest/getEntrypoints/${block.block}/${contractAddress}/`].response
     ).toEqual(entryPoints);
     expect(rpcCache.getAllCachedData()[`rpcTest/getChainId/`].response).toEqual(chainId);
     expect(
@@ -356,15 +359,15 @@ describe('RpcClientCache test', () => {
     await rpcCache.getBlockHash();
     await rpcCache.getBlock();
     await rpcCache.getLiveBlocks();
-    await rpcCache.getBalance('address');
-    await rpcCache.getStorage('address');
-    await rpcCache.getScript('address');
-    await rpcCache.getNormalizedScript('address');
-    await rpcCache.getContract('address');
-    await rpcCache.getManagerKey('address');
-    await rpcCache.getDelegate('address');
+    await rpcCache.getBalance(address);
+    await rpcCache.getStorage(contractAddress);
+    await rpcCache.getScript(contractAddress);
+    await rpcCache.getNormalizedScript(contractAddress);
+    await rpcCache.getContract(contractAddress);
+    await rpcCache.getManagerKey(contractAddress);
+    await rpcCache.getDelegate(address);
     await rpcCache.getBigMapExpr('72', 'expruPtxxirR4BVqFH43VcmEFZqHaQHJhZQDRVTMgSYAGGgBhBRxfp');
-    await rpcCache.getDelegates('address');
+    await rpcCache.getDelegates(address);
     await rpcCache.getConstants();
     await rpcCache.getBlockHeader();
     await rpcCache.getBlockMetadata();
@@ -377,7 +380,7 @@ describe('RpcClientCache test', () => {
     await rpcCache.getCurrentQuorum();
     await rpcCache.getVotesListings();
     await rpcCache.getProposals();
-    await rpcCache.getEntrypoints('address');
+    await rpcCache.getEntrypoints(contractAddress);
     await rpcCache.getChainId();
     await rpcCache.packData({
       data: { bytes: '0000b24ac1e1759565d5c9b69af8450ce7ea3d1ee64c' },
