b7537beb727fa140ec81c4f93a794e7bc9de7a43
roxaneletourneau <roxane@ecadlabs.com>
2022-01-25 18:54:49 -0800

    Add missing file

--- packages/taquito-utils/rollup.config.ts
+++ packages/taquito-utils/rollup.config.ts
@@ -16,6 +16,7 @@ export default {
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [
     'blakejs',
+    'bs58check',
     'typedarray-to-buffer'
   ],
   watch: {
