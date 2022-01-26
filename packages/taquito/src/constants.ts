b3ac1d7fe64176606c44658b495f42a2fdca25bf
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-01-26 09:02:16 -0800

    changes per review

--- packages/taquito/src/constants.ts
+++ packages/taquito/src/constants.ts
@@ -54,8 +54,7 @@ export enum DefaultLambdaAddresses {
   FLORENCENET = 'KT1KCe3YqGnudsiCWb5twbe2DH5T3EMdLpSE',
   GRANADANET = 'KT1BCun2vsA4GBQvsKAuGD5x873MfW2jsN9z',
   HANGZHOUNET = 'KT1PWtBAr1hjK9M9s9oZNZFbfzPdkkD6PSJR',
-  ITHACANET = 'KT1CsEGfRHWeuUQFh9LfVFLVMbm7DFBuHPPU',
-  ITHACANET2 = 'KT1KzvybYb2zAHHMkkXpZhR6Sm2g7KSUx1wa',
+  ITHACANET = 'KT1KzvybYb2zAHHMkkXpZhR6Sm2g7KSUx1wa',
 }
 
 export enum ChainIds {
@@ -66,6 +65,5 @@ export enum ChainIds {
   FLORENCENET = 'NetXxkAx4woPLyu',
   GRANADANET = 'NetXz969SFaFn8k',
   HANGZHOUNET = 'NetXZSsxBpMQeAT',
-  ITHACANET = 'NetXbhmtAbMukLc',
-  ITHACANET2 = 'NetXbhmtAbMukLc',
+  ITHACANET = 'NetXnHfVqm9iesp',
 }
