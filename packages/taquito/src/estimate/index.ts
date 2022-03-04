cb38510ed82562d440b4824953fe5455e0c395a9
roxaneletourneau <roxane@ecadlabs.com>
2022-03-03 19:26:28 -0800

    feat(estimate): fix estimate to work with wallet
    Estimation is done with the configured signer. If there is no configured sihner, estimation is done
    using the public key hash of the configured wallet. Taquito does not have access to the public key
    of the wallet (only the pkh), so wont estimate reveal operation. If the account is unrevealed, an
    exception is throwned.
    
    re #1387

--- packages/taquito/src/estimate/index.ts
+++ packages/taquito/src/estimate/index.ts
@@ -2,3 +2,4 @@ export * from './estimate';
 export * from './estimate-provider-interface';
 export * from './naive-estimate-provider';
 export * from './rpc-estimate-provider';
+export * from './error';
