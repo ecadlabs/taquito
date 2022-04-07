3bafa54c5ceac7a2678d0dc2936ad48893f69ae6
Hamid Goudarzi <51421954+HamidGoudarzi1988@users.noreply.github.com>
2022-04-07 12:43:55 -0700

    Revert "fix logo"
    This reverts commit f0067e61d9c0f39b7b2c44b987dfc0f5522691f0.

--- website/src/components/FooterTop/FooterTop.js
+++ website/src/components/FooterTop/FooterTop.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './FooterTop.module.css';
-import SVG from '../../../static/img/ECAD_logo.svg';
+import React from "react";
+import clsx from "clsx";
+import styles from "./FooterTop.module.css";
+
 const FeatureList = [
-  {
+	{
     title: 'Powered by',
-    Svg: require('../../../static/img/ECAD_logo.svg').default,
-    url: 'https://ecadlabs.com/',
+    image: "../../../static/img/ECAD_logo.svg",
+    url: 'https://tezos-homebase.herokuapp.com/explorer/daos', 
   },
+	,
 ];
 
-function Feature({ title, url, image }) {
-  return (
-    <div className={styles.Contentcontainer}>
-      <h5 className={styles.headline}>{title}</h5>
-      <a href={url}>
-        <SVG className={styles.featureSvg} alt={title} />
-      </a>
-    </div>
-  );
+function Feature({ title, url,image }) {
+	return (
+		<div className={styles.Contentcontainer}>
+			<h5 className={styles.headline}>{title}</h5>
+			<a href={url}>
+				<img className={styles.featureSvg} alt={title} src={image} />
+			</a>
+
+		</div>
+	);
 }
 
 export default function FooterTop() {
-  return (
-    <section className={styles.features}>
-      <div className={styles.container}>
-        <Feature {...FeatureList[0]} />
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className={styles.container}>
+				<Feature {...FeatureList[0]} />
+			</div>
+		</section>
+	);
 }
