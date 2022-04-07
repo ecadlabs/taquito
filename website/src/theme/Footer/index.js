2a376d539b5ee743fce54ee87a6debc4257499ca
Hamid Goudarzi <51421954+HamidGoudarzi1988@users.noreply.github.com>
2022-04-07 11:17:55 -0700

    footer index

--- website/src/theme/Footer/index.js
+++ website/src/theme/Footer/index.js
@@ -1,67 +1,82 @@
 /**
- * Copyright (c) 2017-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 import React from 'react';
-import classnames from 'classnames';
-
+import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useThemeConfig } from '@docusaurus/theme-common';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import isInternalUrl from '@docusaurus/isInternalUrl';
 import styles from './styles.module.css';
+import ThemedImage from '@theme/ThemedImage';
+import IconExternalLink from '@theme/IconExternalLink';
+import FooterForm from '../../components/FooterForm/FooterForm';
 
-function FooterLink({to, href, label, ...props}) {
+function FooterLink({ to, href, label, prependBaseUrlToHref, reactComponent, ...props }) {
   const toUrl = useBaseUrl(to);
+  const normalizedHref = useBaseUrl(href, {
+    forcePrependBaseUrl: true,
+  });
   return (
     <Link
       className="footer__link-item"
       {...(href
         ? {
-            target: '_blank',
-            rel: 'noopener noreferrer',
-            href,
+            href: prependBaseUrlToHref ? normalizedHref : href,
           }
         : {
             to: toUrl,
           })}
-      {...props}>
-      {label}
+      {...props}
+    >
+      {href && !isInternalUrl(href) ? (
+        <span>
+          {label}
+          <IconExternalLink />
+        </span>
+      ) : (
+        label
+      )}
     </Link>
   );
 }
 
-const FooterLogo = ({url, alt}) => (
-  <img className="footer__logo" alt={alt} src={url} />
+const FooterLogo = ({ sources, alt, width, height }) => (
+  <ThemedImage className="footer__logo" alt={alt} sources={sources} width={width} height={height} />
 );
 
 function Footer() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  const {themeConfig = {}} = siteConfig;
-  const {footer} = themeConfig;
-
-  const {copyright, links = [], logo = {}} = footer || {};
-  const logoUrl = useBaseUrl(logo.src);
-
+  const { footer } = useThemeConfig();
+  const { copyright, links = [], logo = {} } = footer || {};
+  const sources = {
+    light: useBaseUrl(logo.src),
+    dark: useBaseUrl(logo.srcDark || logo.src),
+  };
+  const handleSubmit = (e) => {
+    if (!email) {
+      e.preventDefault();
+    }
+  };
   if (!footer) {
     return null;
   }
 
   return (
     <footer
-      className={classnames('footer', {
+      className={clsx('footer', {
         'footer--dark': footer.style === 'dark',
-      })}>
+      })}
+    >
       <div className="container">
         {links && links.length > 0 && (
           <div className="row footer__links">
             {links.map((linkItem, i) => (
               <div key={i} className="col footer__col">
                 {linkItem.title != null ? (
-                  <h4 className="footer__title">{linkItem.title}</h4>
+                  <div className="footer__title">{linkItem.title}</div>
                 ) : null}
                 {linkItem.items != null &&
                 Array.isArray(linkItem.items) &&
@@ -69,17 +84,29 @@ function Footer() {
                   <ul className="footer__items">
                     {linkItem.items.map((item, key) =>
                       item.html ? (
-                        <div
-                          key={key}
-                          dangerouslySetInnerHTML={{
-                            __html: item.html,
-                          }}
-                        />
+                        item.html === 'form' ? (
+                          <li
+                            key={key}
+                            className="footer__item" // Developer provided the HTML, so assume it's safe.
+                            // eslint-disable-next-line react/no-danger
+                          >
+                            <FooterForm />
+                          </li>
+                        ) : (
+                          <li
+                            key={key}
+                            className="footer__item" // Developer provided the HTML, so assume it's safe.
+                            // eslint-disable-next-line react/no-danger
+                            dangerouslySetInnerHTML={{
+                              __html: item.html,
+                            }}
+                          />
+                        )
                       ) : (
                         <li key={item.href || item.to} className="footer__item">
                           <FooterLink {...item} />
                         </li>
-                      ),
+                      )
                     )}
                   </ul>
                 ) : null}
@@ -87,26 +114,30 @@ function Footer() {
             ))}
           </div>
         )}
+
         {(logo || copyright) && (
-          <div className="text--center">
-            {logo && logo.src && (
+          <div className="footer__bottom text--center">
+            {logo && (logo.src || logo.srcDark) && (
               <div className="margin-bottom--sm">
                 {logo.href ? (
-                  <a
-                    href={logo.href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={styles.footerLogoLink}>
-                    <FooterLogo alt={logo.alt} url={logoUrl} />
-                  </a>
+                  <Link href={logo.href} className={styles.footerLogoLink}>
+                    <FooterLogo
+                      alt={logo.alt}
+                      sources={sources}
+                      width={logo.width}
+                      height={logo.height}
+                    />
+                  </Link>
                 ) : (
-                  <FooterLogo alt={logo.alt} url={logoUrl} />
+                  <FooterLogo alt={logo.alt} sources={sources} />
                 )}
               </div>
             )}
-            {copyright}
           </div>
         )}
+      </div>
+      <div className="footer__copyright">
+        {`Copyright © ${new Date().getFullYear()} ECAD Labs - Open Source MIT License`}
       </div>
     </footer>
   );
