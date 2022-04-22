db5a5a1ec2fb4e9881303bf1e178dbe859204695
roxaneletourneau <roxane@ecadlabs.com>
2022-04-22 16:03:31 -0700

    chore(releng) bump version to 12.1.0-beta-RC.0

--- packages/taquito-ledger-signer/src/version.ts
+++ packages/taquito-ledger-signer/src/version.ts
@@ -1,5 +1,6 @@
+
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 export const VERSION = {
-  commitHash: '02ebaa8ef1920e8b6a81f0d0978f28cff0a2d256',
-  version: '12.0.3',
+    "commitHash": "e71912611826272f68d2bc1025eb8218417d28af",
+    "version": "12.1.0-beta-RC.0"
 };
