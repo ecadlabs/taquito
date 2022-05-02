fd894511288190ca207a91a95b9d127b3401039b
Zainen <72581075+zainen@users.noreply.github.com>
2022-05-02 14:02:53 -0700

    fix sequential big map and contract schema (#1583)
    * fix sequential big map and contract schema
    
    one remaining break, example-activate
    
    after fix lerna, update faucet
    
    include isolated faucet for activate, update faucet
    
    * reorder scripts
    
    * add comment for deploy-docs insufficient funds
    
    * add missing new balance log

--- example/contract-schema.ts
+++ example/contract-schema.ts
@@ -1,12 +1,26 @@
 import { TezosToolkit } from '@taquito/taquito';
-import { InMemorySigner } from '@taquito/signer';
+import { InMemorySigner, importKey } from '@taquito/signer';
+import Faucet from './faucet-interface';
+import BigNumber from 'bignumber.js';
+
+const {email, password, mnemonic, activation_code} = require("./faucet-default-values.json") as Faucet
 
 
 async function example() {
     const provider = 'https://ithacanet.ecadinfra.com/';
     const signer: any = new InMemorySigner('edskRtmEwZxRzwd1obV9pJzAoLoxXFWTSHbgqpDBRHx1Ktzo5yVuJ37e2R4nzjLnNbxFU4UiBU1iHzAy52pK5YBRpaFwLbByca');
-        const tezos = new TezosToolkit(provider);
+    const tezos = new TezosToolkit(provider);
     tezos.setSignerProvider( signer );
+
+    // additional lender if sender is broke
+    const tezosLender = new TezosToolkit(provider);
+    await importKey(
+        tezosLender,
+        email,
+        password,
+        mnemonic.join(' '),
+        activation_code
+      );
     try {
         console.log("signer pkh:");
         console.log(await signer.publicKeyHash());
@@ -14,7 +28,19 @@ async function example() {
         console.log("Printing contract methods...");
         console.log(contract.methods);
         console.log("Showing initial storage...");
-        console.log(await contract.storage())
+        console.log(await contract.storage());
+        console.log("checking balance")
+        const balance = await tezos.tz.getBalance(await signer.publicKeyHash());
+        const balanceMin = new BigNumber(100);
+        if (balance < balanceMin) {
+            console.log("topping up funds")
+            console.log("Balance before", balance)
+            const sendMutez = await tezosLender.contract.transfer({ to: await signer.publicKeyHash(), amount: 100 });
+            await sendMutez.confirmation();
+            const newBalance = await tezos.tz.getBalance(await signer.publicKeyHash());
+            console.log("Balance after", newBalance)
+        }
+        console.log("Sending")
         const op = await contract.methods.mint("tz1bwsEWCwSEXdRvnJxvegQZKeX5dj6oKEys", 100).send()
         console.log('Awaiting confirmation...');
         await op.confirmation();
