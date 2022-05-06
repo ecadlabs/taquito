9714bd952f05eaddd9bc2676f277ffe50b0f9abd
Davis Sawali <davis.sawali@ecadlabs.com>
2022-05-06 10:39:26 -0700

    Added error descriptions

--- packages/taquito-sapling/src/error.ts
+++ packages/taquito-sapling/src/error.ts
@@ -1,10 +1,18 @@
-export class InvalidMerkleTreeError extends Error {
-  public name = 'InvalidMerkleTreeError';
-  constructor(public tree: string) {
-    super(`The following Merkle tree is invalid: ${JSON.stringify(tree)}`);
+/**
+ *  @category Error
+ *  @description Error that indicates an invalid Merkle root being passed
+ */
+export class InvalidMerkleRootError extends Error {
+  public name = 'InvalidMerkleRootError';
+  constructor(public root: string) {
+    super(`The following Merkle tree is invalid: ${JSON.stringify(root)}`);
   }
 }
 
+/**
+ *  @category Error
+ *  @description Error that indicates a failure when trying to construct the Merkle tree
+ */
 export class TreeConstructionFailure extends Error {
   public name = 'TreeConstructionFailure';
   constructor(public message: string) {
