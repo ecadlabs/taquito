ca92028f5235ffa5e273da8516e7d62c1c126929
Micahel Kernaghan <michaelkernaghan@ecadlabs.com>
2022-05-18 20:02:55 -0700

    add more explanation more annot tests

--- integration-tests/contract-security-wrong-field-annotations.spec.ts
+++ integration-tests/contract-security-wrong-field-annotations.spec.ts
@@ -2,11 +2,39 @@ import { CONFIGS } from './config';
 import { securityWrongAnnotations } from './data/security-wrong-annotations-contract';
 
 // TC-009: Check whether wrong field annotations are leading to failed transactions.
+/** 
+ * The contract used has six entrypoints
+ * 
+ * entrypoint:          parameter:
+ * 
+ * add                  (pair %add (nat %valueA) (nat %valueB))
+ * addNoAnnot           (pair %addNoAnnot nat nat)
+ * addWrongAnnot        (pair %addWrongAnnot (nat %a) (nat %b))
+ * 
+ * call                 (pair %call (string %entrypoint) (pair %param (nat %valueA) (nat %valueB)))
+ * callNoAnnot          (pair %callNoAnnot (string %entrypoint) (pair %param nat nat))
+ * callWrongAnnot       (pair %callWrongAnnot (string %entrypoint) (pair %param (nat %a) (nat %b)))
+ * 
+ * The test checks 9 cases: For each call entrypoint the tests call each of the three add entrypoints.
+ * Its not entriely clear what should happen in each case. 
+ * 
+ * For example, 
+ *    the first case calls "add" and passes nat %valueA and nat %valueB to CONTRACT %add (pair (nat %valueA) (nat %valueB)), so the annots and params match 
+ *    the second case calls "addNoAnnot" and passes nat and nat to CONTRACT %addNoAnnot (pair (nat %valueA) (nat %valueB)), and here the  annots and params 
+ *    do not match because there are no annots.
+ *    the third case calls "addWrongAnnot" and passes nat a and nat b (where a and b are the annots) to CONTRACT %add (pair nat nat) and here the annots 
+ *    and params do not match.
+ * 
+ *    When run the tests get unexpected exceptions such as 
+ *        [a] Value is not a number: undefined
+ *        [valueA] Value is not a number: undefined
+ *        
+ */
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
   const Tezos = lib;
 
-  describe(`Test contracts to verfiy wrong field annotations are leading to failed transactions using: ${rpc}`, () => {
+  describe(`Test contracts to verify wrong field annotations are leading to failed transactions using: ${rpc}`, () => {
     beforeEach(async (done) => {
       await setup();
       done();
@@ -20,6 +48,8 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
       await addition.confirmation();
       const contractAddition = await addition.contract();
 
+      console.log(contractAddition.address)
+
       //Calls entrypoint: (pair %add (nat %valueA) (nat %valueB))
       //Calling Michelson code: CONTRACT %add (pair (nat %valueA) (nat %valueB)) ;
 
@@ -29,7 +59,7 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
         .then((contract) => {
           return contract.methodsObject
             .call({
-              entrypoint: 'Add',
+              entrypoint: 'add',
               param: {
                 valueA: 1,
                 valueB: 2,
