6c0f16797684e29d2f318e1a4c4d5d032f43d163
Micahel Kernaghan <michaelkernaghan@ecadlabs.com>
2022-05-19 20:32:13 -0700

    collect the non-behaving tests here for research

---
+++ integration-tests/contract-security-balance-instruction.spec.ts
@@ -0,0 +1,112 @@
+import { CONFIGS } from './config';
+
+// TC-008: Obtained balance of a smart contract using the BALANCE instruction does not change during the execution of the entrypoint's own code.
+// Testcase just checks whether this is still the case.
+
+CONFIGS().forEach(({ lib, rpc, setup }) => {
+  const Tezos = lib;
+
+  describe(`Test contracts using: ${rpc}`, () => {
+    beforeEach(async (done) => {
+      await setup();
+      done();
+    });
+
+    it("Verify Obtained balance of a smart contract using the BALANCE instruction does not change during the execution of the entrypoint's own code", async (done) => {
+      try {
+        const opTezTransferA = await Tezos.contract.originate({
+          balance: '0.000010',
+          code: `        { parameter (option address) ;
+            storage (pair (mutez %at_end) (mutez %at_start)) ;
+            code { UNPAIR ;
+                   SWAP ;
+                   BALANCE ;
+                   SWAP ;
+                   CAR ;
+                   PAIR ;
+                   SWAP ;
+                   IF_NONE
+                     { NIL operation }
+                     { CONTRACT (option address) ;
+                       IF_NONE { PUSH string "none" ; FAILWITH } {} ;
+                       PUSH mutez 5 ;
+                       NONE address ;
+                       TRANSFER_TOKENS ;
+                       NIL operation ;
+                       SWAP ;
+                       CONS } ;
+                   SWAP ;
+                   CDR ;
+                   BALANCE ;
+                   PAIR ;
+                   SWAP ;
+                   PAIR } }`,
+          init: '(Pair 0 0)',
+        });
+
+        await opTezTransferA.confirmation();
+        expect(opTezTransferA.hash).toBeDefined();
+        expect(opTezTransferA.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
+        const TezTransferAContract = await opTezTransferA.contract();
+        expect(await TezTransferAContract.storage()).toBeTruthy();
+
+        const opTezTransferB = await Tezos.contract.originate({
+          code: `        { parameter (option address) ;
+            storage (pair (mutez %at_end) (mutez %at_start)) ;
+            code { UNPAIR ;
+                   SWAP ;
+                   BALANCE ;
+                   SWAP ;
+                   CAR ;
+                   PAIR ;
+                   SWAP ;
+                   IF_NONE
+                     { NIL operation }
+                     { CONTRACT (option address) ;
+                       IF_NONE { PUSH string "none" ; FAILWITH } {} ;
+                       PUSH mutez 5 ;
+                       NONE address ;
+                       TRANSFER_TOKENS ;
+                       NIL operation ;
+                       SWAP ;
+                       CONS } ;
+                   SWAP ;
+                   CDR ;
+                   BALANCE ;
+                   PAIR ;
+                   SWAP ;
+                   PAIR } }`,
+          init: '(Pair 0 0)',
+        });
+
+        await opTezTransferB.confirmation();
+        expect(opTezTransferB.hash).toBeDefined();
+        expect(opTezTransferB.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
+        const TezTransferBContract = await opTezTransferB.contract();
+        expect(await TezTransferBContract.storage()).toBeTruthy();
+
+        await Tezos.contract
+          .at(TezTransferAContract.address)
+          .then((contract) => {
+            return contract.methods.default(TezTransferBContract.address).send();
+          })
+          .then((op) => {
+            return op.confirmation().then(() => op.hash);
+          })
+
+           const storageA = await TezTransferAContract.storage();
+           expect(storageA).toContain({at_end: "10", at_start: "10"});
+           /// Should be {"at_end": "5", "at_start": "5"}
+
+           const storageB = await TezTransferBContract.storage();
+           expect(storageB).toContain({at_end: "10", at_start: "10"});
+           /// Should be {"at_end": "5", "at_start": "5"}
+      } catch (error: any) {
+        
+      }
+      done();
+    });
+  });
+});
+
+// This test was transcribed to Taquito from bash scripts at https://github.com/InferenceAG/TezosSecurityBaselineChecking
---
+++ integration-tests/contract-security-wrong-field-annotations.spec.ts
@@ -0,0 +1,291 @@
+import { CONFIGS } from './config';
+import { securityWrongAnnotations } from './data/security-wrong-annotations-contract';
+
+// TC-009: Check whether wrong field annotations are leading to failed transactions.
+/** 
+ * An example of the test contract is here: https://better-call.dev/ithacanet/KT1C4NcV4ebR6gS7SzbavkkSx3HYhWTBPVJ9/operations
+ * 
+ * The contract used has six entrypoints:
+ * 
+ * entrypoint:          parameter:
+ * 
+ * add                  (pair %add (nat %valueA) (nat %valueB))
+ * addNoAnnot           (pair %addNoAnnot nat nat)
+ * addWrongAnnot        (pair %addWrongAnnot (nat %a) (nat %b))
+ * 
+ * call                 (pair %call (string %entrypoint) (pair %param (nat %valueA) (nat %valueB)))
+ * callNoAnnot          (pair %callNoAnnot (string %entrypoint) (pair %param nat nat))
+ * callWrongAnnot       (pair %callWrongAnnot (string %entrypoint) (pair %param (nat %a) (nat %b)))
+ * 
+ * The test checks 9 cases: For each call entrypoint the tests call each of the three add entrypoints.
+ * Its not entriely clear what should happen in each case. 
+ * 
+ * For example, 
+ *    the first case calls "add" and passes nat %valueA and nat %valueB to CONTRACT %add (pair (nat %valueA) (nat %valueB)), so the annots and params match 
+ *    the second case calls "addNoAnnot" and passes nat and nat to CONTRACT %addNoAnnot (pair (nat %valueA) (nat %valueB)), and here the  annots and params 
+ *    do not match because there are no annots.
+ *    the third case calls "addWrongAnnot" and passes nat a and nat b (where a and b are the annots) to CONTRACT %add (pair nat nat) and here the annots 
+ *    and params do not match.
+ * 
+ *    When run the tests get unexpected exceptions such as 
+ *        [a] Value is not a number: undefined
+ *        [valueA] Value is not a number: undefined
+ *        
+ */
+
+CONFIGS().forEach(({ lib, rpc, setup }) => {
+  const Tezos = lib;
+
+  describe(`Test contracts to verify wrong field annotations are leading to failed transactions using: ${rpc}`, () => {
+    beforeEach(async (done) => {
+      await setup();
+      done();
+    });
+
+    it('Verify call Add', async () => {
+      const addition = await Tezos.contract.originate({
+        code: securityWrongAnnotations,
+        init: `0`,
+      });
+      await addition.confirmation();
+      const contractAddition = await addition.contract();
+
+      console.log(contractAddition.address)
+
+      //Calls entrypoint: (pair %add (nat %valueA) (nat %valueB))
+      //Calling Michelson code: CONTRACT %add (pair (nat %valueA) (nat %valueB)) ;
+
+      try {
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .call({
+              entrypoint: 'add',
+              param: {
+                valueA: 1,
+                valueB: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+      } catch (ex) {
+        expect(ex).toEqual(expect.objectContaining({
+          message: expect.stringContaining('[a] Value is not a number: undefined')
+        }))
+      }
+
+      // Calls entrypoint: (pair %addNoAnnot nat nat))
+      // Calling Michelson code: CONTRACT %addNoAnnot (pair (nat %valueA) (nat %valueB)) ;     
+      try {
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .call({
+              entrypoint: 'addNoAnnot',
+              param: {
+                valueA: 1,
+                valueB: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+      } catch (ex) {
+        expect(ex).toEqual(expect.objectContaining({
+          message: expect.stringContaining('[a] Value is not a number: undefined')
+        }))
+      }
+  
+      // Calls entrypoint: (pair %addWrongAnnot (nat %a) (nat %b))
+      // Calling Michelson code: CONTRACT %addWrongAnnot (pair (nat %valueA) (nat %valueB)) ;
+      try{
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .call({
+              entrypoint: 'AddWrongAnnot',
+              param: {
+                a: 1,
+                b: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+    } catch (ex) {
+      expect(ex).toEqual(expect.objectContaining({
+        message: expect.stringContaining('[valueA] Value is not a number: undefined')
+      }))
+    }
+  
+      // Calls entrypoint: (pair %add (nat %valueA) (nat %valueB))
+      // Calling Michelson code: CONTRACT %add (pair nat nat) ;
+      
+      try {
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .callNoAnnot({
+              entrypoint: 'Add',
+              param: {
+                1: 1,
+                2: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+      } catch (ex) {
+        expect(ex).toEqual(expect.objectContaining({
+          message: expect.stringContaining('[a] Value is not a number: undefined')
+        }))
+      }
+  
+      // Calls entrypoint: CONTRACT %addNoAnnot (pair nat nat) ;
+      // Calling Michelson code: CONTRACT %addNoAnnot (pair (nat %valueA) (nat %valueB)) ;
+      
+      try {
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .callNoAnnot({
+              entrypoint: 'AddNoAnnot',
+              param: {
+                1: 1,
+                2: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+      } catch (ex) {
+        expect(ex).toEqual(expect.objectContaining({
+          message: expect.stringContaining('[a] Value is not a number: undefined')
+        }))
+      }
+ 
+      // Calls entrypoint: CONTRACT %addWrongAnnot (pair nat nat) ;
+      // Calling Michelson code: CONTRACT %addWrongAnnot (pair (nat %valueA) (nat %valueB)) ;
+      
+      try {
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .callNoAnnot({
+              entrypoint: 'AddWrongAnnot',
+              param: {
+                1: 1,
+                2: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+      } catch (ex) {
+        expect(ex).toEqual(expect.objectContaining({
+          message: expect.stringContaining('[a] Value is not a number: undefined')
+        }))
+      }
+ 
+      // Calls entrypoint: (pair %add (nat %valueA) (nat %valueB))
+      // Calling Michelson code: CONTRACT %add (pair (nat %a) (nat %b)) ;
+
+      try {
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .callWrongAnnot({
+              entrypoint: 'Add',
+              param: {
+                valueA: 1,
+                valueB: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+      } catch (ex) {
+        expect(ex).toEqual(expect.objectContaining({
+          message: expect.stringContaining('[a] Value is not a number: undefined')
+        }))
+      }
+  
+      // Calls entrypoint: (pair %addNoAnnot nat nat))
+      // Calling Michelson code: CONTRACT %add (pair (nat %a) (nat %b))
+
+      try {
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .callWrongAnnot({
+              entrypoint: 'AddNoAnnot',
+              param: {
+                a: 1,
+                b: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+      } catch (ex) {
+        expect(ex).toEqual(expect.objectContaining({
+          message: expect.stringContaining('[a] Value is not a number: undefined')
+        }))
+      }
+   
+      // Calls entrypoint: (pair %addWrongAnnot (nat %a) (nat %b))
+      // Calling Michelson code: CONTRACT %add (pair (nat %a) (nat %b))
+      
+      try{
+      const contractAddition = await addition.contract();
+      const op = await Tezos.contract
+        .at(contractAddition.address)
+        .then((contract) => {
+          return contract.methodsObject
+            .callWrongAnnot({
+              entrypoint: 'AddWrongAnnot',
+              param: {
+                a: 1,
+                b: 2,
+              },
+            })
+            .send();
+        })
+        .then((op) => {
+          return op.confirmation(3).then(() => op.hash);
+        })
+      } catch (ex) {
+        expect(ex).toEqual(expect.objectContaining({
+          message: expect.stringContaining('[a] Value is not a number: undefined')
+        }))
+      }
+    });
+  });
+});
+
+// This test was transcribed to Taquito from bash scripts at https://github.com/InferenceAG/TezosSecurityBaselineChecking
