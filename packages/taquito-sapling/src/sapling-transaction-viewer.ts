28b7e6c7735488ed1f43fd4318620d188965a565
Roxane Létourneau <65877347+roxaneletourneau@users.noreply.github.com>
2022-06-21 12:56:26 -0700

    feat(sapling): saplingTransactionViewer class (#1601)
    * feat(sapling): saplingTransactionViewer class
    
    Allows to retrieve and decrypt sapling transactions and fetch the balance
    
    re #1571
    
    * Fix build issue
    
    * removed unnecessary test
    
    * changes per review

--- packages/taquito-sapling/src/sapling-transaction-viewer.ts
+++ packages/taquito-sapling/src/sapling-transaction-viewer.ts
@@ -1,23 +1,275 @@
-import * as sapling from '@airgap/sapling-wasm'
+import * as sapling from '@airgap/sapling-wasm';
+import BigNumber from 'bignumber.js';
+import { hex2buf, mergebuf } from '@taquito/utils';
+import { CommitmentsAndCiphertexts, RpcClient, RpcClientInterface } from '@taquito/rpc';
+import blake from 'blakejs';
+import { openSecretBox } from '@stablelib/nacl';
+import { InMemoryViewingKey } from './in-memory-viewing-key';
+import { bufToUint8Array, convertValueToBigNumber, readableFormat } from './helpers';
 
+const KDF_KEY = 'KDFSaplingForTezosV1';
+const OCK_KEY = 'OCK_keystringderivation_TEZOS';
+
+export interface SaplingIncomingAndOutgoingTransaction {
+  incoming: SaplingIncomingTransaction[];
+  outgoing: SaplingOutgoingTransaction[];
+}
+export interface SaplingIncomingTransaction {
+  value: BigNumber;
+  memo: string;
+  paymentAddress: string;
+  isSpent: boolean;
+}
+
+export interface SaplingOutgoingTransaction {
+  value: BigNumber;
+  memo: string;
+  paymentAddress: string;
+}
+
+export interface SaplingTransactionPropertiesRaw {
+  value: Uint8Array;
+  memo: Uint8Array;
+  paymentAddress: Uint8Array;
+  rcm: Uint8Array;
+}
+/**
+ * @description Allows to retrieve and decrypt sapling transactions
+ */
 export class SaplingTransactionViewer {
-    #viewingKey: Buffer;
-    constructor(viewingKey: Buffer) {
-        this.#viewingKey = viewingKey
-    }
+  #viewingKeyProvider: InMemoryViewingKey;
+  #rpcClient: RpcClientInterface;
 
-    static async fromSpendingKey(spendingKey: string, password?: string) {
-        if (password) {
-            // decode
-            // TODO
+  constructor(inMemoryViewingKey: InMemoryViewingKey, rpc: string | RpcClientInterface) {
+    this.#viewingKeyProvider = inMemoryViewingKey;
+    if (typeof rpc === 'string') {
+      this.#rpcClient = new RpcClient(rpc);
+    } else {
+      this.#rpcClient = rpc;
+    }
+  }
+
+  /**
+   * @description Retrieve the unspent balance associated with the configured viewing key
+   *
+   * @param saplingId sapling id that can be found in the contract storage
+   * @returns the balance represented as a BigNumber
+   *
+   */
+  async getBalance(saplingId: string): Promise<BigNumber> {
+    let balance = new BigNumber(0);
+    const { commitments_and_ciphertexts, nullifiers } = await this.#rpcClient.getSaplingDiffById(
+      saplingId
+    );
+    for (let i = 0; i < commitments_and_ciphertexts.length; i++) {
+      const decrypted = await this.decryptCiphertextAsReceiver(commitments_and_ciphertexts[i]);
+      if (decrypted) {
+        const valueBigNumber = convertValueToBigNumber(decrypted.value);
+        const isSpent = await this.isSpent(
+          decrypted.paymentAddress,
+          valueBigNumber.toString(),
+          decrypted.rcm,
+          i,
+          nullifiers
+        );
+        if (!isSpent) {
+          balance = balance.plus(valueBigNumber);
         }
-        const vk = await sapling.getExtendedFullViewingKeyFromSpendingKey(spendingKey);
-        return new SaplingTransactionViewer(vk);
+      }
     }
+    return balance;
+  }
 
-    getViewingKey(){
-        return this.#viewingKey;
+  /**
+   * @description Retrieve all the incoming and outgoing transactions associated with the configured viewing key.
+   * The response properties are in Uint8Array format; use the getIncomingAndOutgoingTransactions method for readable properties
+   *
+   * @param saplingId sapling id that can be found in the contract storage
+   *
+   */
+  async getIncomingAndOutgoingTransactionsRaw(saplingId: string) {
+    const incoming = [];
+    const outgoing = [];
+    const { commitments_and_ciphertexts, nullifiers } = await this.#rpcClient.getSaplingDiffById(
+      saplingId
+    );
+    for (let i = 0; i < commitments_and_ciphertexts.length; i++) {
+      const decryptedAsReceiver = await this.decryptCiphertextAsReceiver(
+        commitments_and_ciphertexts[i]
+      );
+      const decryptedAsSender = await this.decryptCiphertextAsSender(
+        commitments_and_ciphertexts[i]
+      );
+      if (decryptedAsReceiver) {
+        const balance = convertValueToBigNumber(decryptedAsReceiver.value);
+        const isSpent = await this.isSpent(
+          decryptedAsReceiver.paymentAddress,
+          balance.toString(),
+          decryptedAsReceiver.rcm,
+          i,
+          nullifiers
+        );
+        incoming.push({ ...decryptedAsReceiver, isSpent });
+      }
+      if (decryptedAsSender) {
+        outgoing.push(decryptedAsSender);
+      }
     }
+    return {
+      incoming,
+      outgoing,
+    };
+  }
 
-    
+  /**
+   * @description Retrieve all the incoming and outgoing decoded transactions associated with the configured viewing key
+   *
+   * @param saplingId sapling id that can be found in the contract storage
+   *
+   */
+  async getIncomingAndOutgoingTransactions(
+    saplingId: string
+  ): Promise<SaplingIncomingAndOutgoingTransaction> {
+    const tx = await this.getIncomingAndOutgoingTransactionsRaw(saplingId);
+    const incoming = tx.incoming.map(({ isSpent, ...rest }) => {
+      return { ...readableFormat(rest), isSpent };
+    });
+    const outgoing = tx.outgoing.map((outgoingTx) => {
+      return readableFormat(outgoingTx);
+    });
+    return { incoming, outgoing };
+  }
+
+  private async decryptCiphertextAsReceiver(
+    commitmentsAndCiphertexts: CommitmentsAndCiphertexts
+  ): Promise<SaplingTransactionPropertiesRaw | undefined> {
+    const commitment = commitmentsAndCiphertexts[0];
+    const { epk, payload_enc, nonce_enc } = commitmentsAndCiphertexts[1];
+
+    const ivk = await this.#viewingKeyProvider.getIncomingViewingKey();
+    const keyAgreement = await sapling.keyAgreement(epk, ivk);
+    const keyAgreementHash = blake.blake2b(keyAgreement, Buffer.from(KDF_KEY), 32);
+
+    const decrypted = await this.decryptCiphertext(
+      keyAgreementHash,
+      hex2buf(nonce_enc),
+      hex2buf(payload_enc)
+    );
+
+    if (decrypted) {
+      const { diversifier, value, rcm, memo } = this.extractTransactionProperties(decrypted);
+      const paymentAddress = bufToUint8Array(
+        await sapling.getRawPaymentAddressFromIncomingViewingKey(ivk, diversifier)
+      );
+
+      try {
+        const valid = await sapling.verifyCommitment(
+          commitment,
+          paymentAddress,
+          convertValueToBigNumber(value).toString(),
+          rcm
+        );
+        if (valid) {
+          return { value, memo, paymentAddress, rcm };
+        }
+      } catch (ex: any) {
+        if (!/invalid value/.test(ex)) {
+          throw ex;
+        }
+      }
+    }
+  }
+
+  private async decryptCiphertextAsSender(
+    commitmentsAndCiphertexts: CommitmentsAndCiphertexts
+  ): Promise<SaplingTransactionPropertiesRaw | undefined> {
+    const commitment = commitmentsAndCiphertexts[0];
+    const { epk, payload_enc, nonce_enc, payload_out, nonce_out, cv } =
+      commitmentsAndCiphertexts[1];
+
+    const ovk = await this.#viewingKeyProvider.getOutgoingViewingKey();
+    const concat = cv.concat(commitment, epk, ovk.toString('hex'));
+    const ock = blake.blake2b(Buffer.from(concat, 'hex'), Buffer.from(OCK_KEY), 32);
+
+    const decryptedOut = await this.decryptCiphertext(
+      ock,
+      hex2buf(nonce_out),
+      hex2buf(payload_out)
+    );
+
+    if (decryptedOut) {
+      const { pkd, esk } = this.extractPkdAndEsk(decryptedOut);
+      const keyAgreement = await sapling.keyAgreement(pkd, esk);
+      const keyAgreementHash = blake.blake2b(keyAgreement, Buffer.from(KDF_KEY), 32);
+
+      const decryptedEnc = await this.decryptCiphertext(
+        keyAgreementHash,
+        hex2buf(nonce_enc),
+        hex2buf(payload_enc)
+      );
+
+      if (decryptedEnc) {
+        const { diversifier, value, rcm, memo } = this.extractTransactionProperties(decryptedEnc);
+        const paymentAddress = mergebuf(diversifier, pkd);
+
+        try {
+          const isValid = await sapling.verifyCommitment(
+            commitment,
+            paymentAddress,
+            convertValueToBigNumber(value).toString(),
+            rcm
+          );
+          if (isValid) {
+            return { value, memo, paymentAddress, rcm };
+          }
+        } catch (ex: any) {
+          if (!/invalid value/.test(ex)) {
+            throw ex;
+          }
+        }
+      }
+    }
+  }
+
+  private async decryptCiphertext(
+    keyAgreementHash: Uint8Array,
+    nonce: Uint8Array,
+    payload: Uint8Array
+  ) {
+    return openSecretBox(keyAgreementHash, nonce, payload);
+  }
+
+  private extractTransactionProperties(decrypted: Uint8Array) {
+    return {
+      diversifier: decrypted.slice(0, 11),
+      value: decrypted.slice(11, 19),
+      rcm: decrypted.slice(19, 51),
+      memoSize: decrypted.slice(51, 55),
+      memo: decrypted.slice(55),
+    };
+  }
+
+  private extractPkdAndEsk(decrypted: Uint8Array) {
+    return {
+      pkd: decrypted.slice(0, 32),
+      esk: decrypted.slice(32),
+    };
+  }
+
+  private async isSpent(
+    address: Uint8Array,
+    value: string,
+    rcm: Uint8Array,
+    position: number,
+    nullifiers: string[]
+  ) {
+    const computedNullifier = await sapling.computeNullifier(
+      this.#viewingKeyProvider.getFullViewingKey(),
+      address,
+      value,
+      rcm,
+      position
+    );
+    return nullifiers.includes(computedNullifier.toString('hex'));
+  }
 }
---
+++ packages/taquito-sapling/test/sapling-transaction-viewer.spec.ts
@@ -0,0 +1,159 @@
+import { SaplingTransactionViewer } from '../src/sapling-transaction-viewer';
+import { InMemoryViewingKey } from '../src/in-memory-viewing-key';
+import { saplingStateDiff, saplingStateDiffMemo4 } from './data';
+import BigNumber from 'bignumber.js';
+
+describe('SaplingTransactionViewer', () => {
+  let mockRpcClient: any;
+  let viewingKeyInMemory: InMemoryViewingKey;
+
+  beforeEach(async () => {
+    mockRpcClient = {
+      getSaplingDiffById: jest.fn(),
+    };
+    mockRpcClient.getSaplingDiffById.mockResolvedValue(saplingStateDiff);
+
+    viewingKeyInMemory = await InMemoryViewingKey.fromSpendingKey(
+      'sask27SLmU9herddJFjvuQq9aVuzTx8nmu3zttpMCDWBH2V2MbvUcnxrH5AWGXXybqiqn9THT8GMQhJumHgM2rPQi3sx6FmD2WY4tXrEk7YupAQ9VG9MoM5YkpQFMqS6JkcWiZZCYz4ZWXDdi8Jycgv6qKXauWPA6EY7vVJUZX8Dh2rJGrCRm7huLQK3UpHkHar4sv332z6Cqwqcc7Drz7VzkGeUidG2MWQJvUvzPVzpnHPJx'
+    );
+  });
+
+  it('Should be instantiable', async (done) => {
+    const txViewer = new SaplingTransactionViewer(viewingKeyInMemory, mockRpcClient);
+    expect(txViewer).toBeDefined();
+    done();
+  });
+
+  it('Retrieve the correct balance from sapling state', async (done) => {
+    const txViewer = new SaplingTransactionViewer(viewingKeyInMemory, mockRpcClient);
+    expect(await txViewer.getBalance('3899')).toEqual(new BigNumber(5000000));
+    done();
+  });
+
+  it('Retrieve incoming and outgoing transactions in readable format', async (done) => {
+    const txViewer = new SaplingTransactionViewer(viewingKeyInMemory, mockRpcClient);
+    expect(await txViewer.getIncomingAndOutgoingTransactions('3899')).toEqual({
+      incoming: [
+        {
+          value: new BigNumber('10000000'),
+          memo: '',
+          paymentAddress: 'zet12YrifNvzLj2D897URmAKpvazMDucwkFEPUGrmmDUE1E7aHXnLU2oTTxUA8igg7sxb',
+          isSpent: true,
+        },
+        {
+          value: new BigNumber('0'),
+          memo: '',
+          paymentAddress: 'zet12xEiGz8bTjRWxAE5kanYUQjZv27JSrUE9JymsT8FrMrZbETtHqCnkLGN7MumgFWNr',
+          isSpent: true,
+        },
+        {
+          value: new BigNumber('10000000'),
+          memo: '',
+          paymentAddress: 'zet12xEiGz8bTjRWxAE5kanYUQjZv27JSrUE9JymsT8FrMrZbETtHqCnkLGN7MumgFWNr',
+          isSpent: true,
+        },
+        {
+          value: new BigNumber('6000000'),
+          memo: '',
+          paymentAddress: 'zet13NrtDFk1CwK3LwRMS84tHoer9JXoh47HemGYqLFmYGUyvX2qRhiYtgdrv8XMbpJtE',
+          isSpent: true,
+        },
+        {
+          value: new BigNumber('5000000'),
+          memo: '',
+          paymentAddress: 'zet13sURbJMkAE6qkMZLDsHypuihJLcvEpfpfjxT3jNAqTpYjgKey5QGXgePPbeJFgqXG',
+          isSpent: false,
+        },
+      ],
+      outgoing: [
+        {
+          value: new BigNumber('10000000'),
+          memo: '',
+          paymentAddress: 'zet13JTYHFem23nK34x9AXg7rWRSKXXixCRZwN8Wd9q2HkWZiW5GjqHfvT53WFPHMZszM',
+        },
+        {
+          value: new BigNumber('0'),
+          memo: '',
+          paymentAddress: 'zet12xEiGz8bTjRWxAE5kanYUQjZv27JSrUE9JymsT8FrMrZbETtHqCnkLGN7MumgFWNr',
+        },
+        {
+          value: new BigNumber('6000000'),
+          memo: '',
+          paymentAddress: 'zet13NrtDFk1CwK3LwRMS84tHoer9JXoh47HemGYqLFmYGUyvX2qRhiYtgdrv8XMbpJtE',
+        },
+        {
+          value: new BigNumber('4000000'),
+          memo: 'taquito',
+          paymentAddress: 'zet134HVqfm5DVp8PZccMeQQjeNXFJZg2VyZigpBSR31ppdfvddkRuqGyEWPLiaXULtDR',
+        },
+        {
+          value: new BigNumber('5000000'),
+          memo: '',
+          paymentAddress: 'zet13sURbJMkAE6qkMZLDsHypuihJLcvEpfpfjxT3jNAqTpYjgKey5QGXgePPbeJFgqXG',
+        },
+      ],
+    });
+    done();
+  });
+
+  it('Retrieve incoming and outgoing transactions raw format', async (done) => {
+    mockRpcClient.getSaplingDiffById.mockResolvedValue(saplingStateDiffMemo4);
+
+    viewingKeyInMemory = await InMemoryViewingKey.fromSpendingKey(
+      'sask27SLmU9herddJFjvuQq9aVuzTx8nmu3zttpMCDWBH2V2MbvUcnxrH5AWGXXybqiqn9THT8GMQhJumHgM2rPQi3sx6FmD2WY4tXrEk7YupAQ9VG9MoM5YkpQFMqS6JkcWiZZCYz4ZWXDdi8Jycgv6qKXauWPA6EY7vVJUZX8Dh2rJGrCRm7huLQK3UpHkHar4sv332z6Cqwqcc7Drz7VzkGeUidG2MWQJvUvzPVzpnHPJx'
+    );
+
+    const txViewer = new SaplingTransactionViewer(viewingKeyInMemory, mockRpcClient);
+    expect(await txViewer.getIncomingAndOutgoingTransactionsRaw('3899')).toEqual({
+      incoming: [
+        {
+          value: new Uint8Array([0, 0, 0, 0, 0, 152, 150, 128]),
+          memo: new Uint8Array([116, 97, 99, 111]),
+          paymentAddress: new Uint8Array([
+            166, 143, 83, 137, 83, 41, 234, 208, 203, 208, 178, 171, 132, 45, 244, 237, 184, 210,
+            132, 27, 72, 208, 32, 138, 128, 112, 14, 34, 153, 61, 38, 89, 83, 150, 9, 75, 32, 82,
+            221, 192, 178, 66, 70,
+          ]),
+          rcm: new Uint8Array([
+            36, 172, 48, 219, 73, 224, 5, 79, 74, 89, 93, 209, 250, 102, 226, 180, 52, 197, 67, 240,
+            191, 75, 110, 117, 240, 29, 231, 138, 161, 173, 197, 0,
+          ]),
+          isSpent: false,
+        },
+      ],
+      outgoing: [],
+    });
+    done();
+  });
+
+  it('should ignore transactions having invalid commitment', async (done) => {
+    viewingKeyInMemory = await InMemoryViewingKey.fromSpendingKey(
+      'sask27SLmU9herddJ9EmreYA6y3Jmpxvrzk5891R8Z4osTGCoxfevAV9HafAAntWhRDHNxytBCN9qtFE7eBk9DE2ewEzvXUSSKRhrjF7pshtoc4oWGBS7Z1JJ4VHNanXCDc3qdUb45cp6QJS8sbzQqZNFycNyjNr3cXdodUfrGyHDsiX8JHPaL9ASJxauwmVqRmT6rYKEUagJUQEVzdHJ72Wd6ewEC1mHrMpvyNHkkcSkaLRr'
+    );
+    const txViewer = new SaplingTransactionViewer(viewingKeyInMemory, mockRpcClient);
+    expect(await txViewer.getIncomingAndOutgoingTransactions('3899')).toEqual({
+      incoming: [
+        {
+          value: new BigNumber('7000000'),
+          memo: 'test',
+          paymentAddress: 'zet14599uF9M7xXsJ9ALhHEdvcRTiE43RLbSTScbEAkozjStD2EA7iBVbYiMkuGDzhi4p',
+          isSpent: false,
+        },
+        {
+          value: new BigNumber('7000000'),
+          memo: 'test',
+          paymentAddress: 'zet12mvUcMftBAkvn58h5gNVCwiySx2w2Qb4pjP5F5qqZ7xBnVmy7ckRDPGmj2hzZti2s',
+          isSpent: true,
+        },
+      ],
+      outgoing: [
+        {
+          value: new BigNumber('1000000'),
+          memo: 'hi',
+          paymentAddress: 'zet144JrAT1waTbYdFWPzJGPSGgDyy3vmDDPSWaxmWGJuhwpn3F6RL48kcWMDxAwGGFWY',
+        },
+      ],
+    });
+    done();
+  });
+});
