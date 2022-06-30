dcc15fd7fd08d4c725f4e58c788adb538f074020
Micahel Kernaghan <michaelkernaghan@ecadlabs.com>
2022-06-30 07:21:17 -0700

    update rpc values for mondaynet test

--- integration-tests/contract-extended-multisig-scenario.spec.ts
+++ integration-tests/contract-extended-multisig-scenario.spec.ts
@@ -1,3 +1,16 @@
+/**
+ * CB: I would like to have your opinion about the design and eventual missing features:
+      - There is currently no way to add a new manager. Would it be okay to have an entrypoint that any existing manager can call to add a new manager 
+        or have an entrypoint that needs approval before being called?
+      - When a manager wants a proposal to be approved by the other managers, he sends a lambda that returns a list of operations to the propose entrypoint 
+        with an expiry date
+      - The other managers can call the approve entrypoint to, well, approve the proposal
+      - When the required number of approvals is reached (this number is set in the storage and can be modified), the proposal becomes available for execution
+      - One of the managers calls the execute entrypoint that will run the lambda from the proposal
+This design seems to me more convenient as it lets the managers come and approve the proposal whenever they have time (within the limit of the expiry date) 
+instead of sending all the signatures to the contract and process them (which also increases drastically the gas cost). 
+*/
+
 import { CONFIGS } from "./config";
 import { MANAGER_LAMBDA } from "@taquito/taquito";
 import { extended_multisig } from "./data/extended_multisig";
@@ -113,8 +126,8 @@ CONFIGS().forEach(({ lib, rpc, setup, createAddress }) => {
       const signature1 = await account1.signer.sign(packed, new Uint8Array())
       const signature2 = await account2.signer.sign(packed, new Uint8Array())
 
-      console.log("signature1 : "+signature1)
-      console.log("signature2 : "+signature2)
+      console.log("signature1 : "+JSON.stringify(signature1))
+      console.log("signature2 : "+JSON.stringify(signature2))
 
       const op2 = await contract.methods.main(
         // Counter
