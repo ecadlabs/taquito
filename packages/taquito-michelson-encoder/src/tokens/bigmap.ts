0bfae19f18f2f9b26c6ceb6d4742a33e12321986
Jimmy Chen Chen <jimmy.chenchen@ecadlabs.com>
2022-07-04 19:05:50 -0700

    Test that taqueria points to taquito successfully

--- packages/taquito-michelson-encoder/src/tokens/bigmap.ts
+++ packages/taquito-michelson-encoder/src/tokens/bigmap.ts
@@ -70,6 +70,7 @@ export class BigMapToken extends Token {
   }
 
   private objLitToMichelsonMap(val: any): any {
+    console.log("JCC val is: ", val);
     if (val instanceof MichelsonMap) return val;
     if (typeof val === 'object') {
       if (Object.keys(val).length === 0) {
