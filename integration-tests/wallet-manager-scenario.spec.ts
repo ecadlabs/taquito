02939d41e2ad33a6e2181464df3ab3128ca89b5e
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-07-22 07:32:36 -0700

    fixes

--- integration-tests/wallet-manager-scenario.spec.ts
+++ integration-tests/wallet-manager-scenario.spec.ts
@@ -4,14 +4,16 @@ import { MANAGER_LAMBDA, Protocols } from "@taquito/taquito";
 
 CONFIGS().forEach(({ lib, rpc, setup, knownBaker, knownContract, protocol }) => {
   const Tezos = lib;
+  const jakartanet = (protocol === Protocols.PtJakart2) ? test : test.skip;
   const mondaynet = (protocol === Protocols.ProtoALpha) ? test : test.skip;
+  const kathmandunet = (protocol === Protocols.PtKathman) ? test : test.skip;
 
   describe(`Manager TZ: ${rpc}`, () => {
     beforeEach(async (done) => {
       await setup()
       done()
     })
-    test('tests manager transfer scenarios with wallet APi contract', async (done) => {
+    jakartanet('tests manager transfer scenarios with wallet APi contract', async (done) => {
       const op = await Tezos.wallet.originate({
         balance: "1",
         code: managerCode,
@@ -56,6 +58,51 @@ CONFIGS().forEach(({ lib, rpc, setup, knownBaker, knownContract, protocol }) => 
       done();
     })
 
+    kathmandunet('tests manager transfer scenarios with wallet APi contract', async (done) => {
+      const op = await Tezos.wallet.originate({
+        balance: "1",
+        code: managerCode,
+        init: { "string": await Tezos.signer.publicKeyHash() },
+      }).send();
+      const contract = await op.contract();
+      expect(op.status).toBeTruthy
+      // Transfer from implicit account (tz1) to contract (kt1_alice)
+      // A regular transfer operation is made. No smart contract calls required for this scenario.
+      const opTransferToContract = await Tezos.wallet.transfer({ to: contract.address, amount: 0.01 }).send();
+      await opTransferToContract.confirmation();
+      expect(op.status).toBeTruthy
+      // Transfer from contract (kt1_alice) to implicit account (tz1)
+      // We pass a lambda function to the kt1_alice contracts `do` entrypoint. The lambda code causes the contract to transfer
+      // the specified number (50) of mutez to the target address.
+      const opTransfer = await contract.methods.do(MANAGER_LAMBDA.transferImplicit("tz1eY5Aqa1kXDFoiebL28emyXFoneAoVg1zh", 5)).send({ amount: 0 })
+      await opTransfer.confirmation();
+      expect(opTransfer.status).toBeTruthy
+      // Set delegate on contract kt1_alice by passing a lambda function to kt1_alice's `do` entrypoint
+      const opSetDelegate = await contract.methods.do(MANAGER_LAMBDA.setDelegate(knownBaker)).send({ amount: 0 })
+      await opSetDelegate.confirmation();
+      expect(opSetDelegate.status).toBeTruthy
+      // Remove delegate on contract kt1_alice by passing a lambda function to kt1_alice's `do` entrypoint
+      const removeDelegateOp = await contract.methods.do(MANAGER_LAMBDA.removeDelegate()).send({ amount: 0 })
+      await removeDelegateOp.confirmation();
+      expect(removeDelegateOp.status).toBeTruthy
+      // Transfer from contract (kt1_alice) to contract (kt1 bob)
+      // Notice that we are instructing the kt1_alice contract to send 1 token to kt1_bob. The transfer value is passed to the
+      // lambda helper function. The transfer amount in the actual transfer operation is 0. We are not transferring the token
+      // in the transfer operation, we are instructing the contract to transfer the token using the `do` entrypoint of the kt1_alice
+      // contract.
+      const transferToContractOp = await contract.methods.do(MANAGER_LAMBDA.transferToContract(knownContract, 1)).send({ amount: 0 })
+      await transferToContractOp.confirmation();
+      expect(transferToContractOp.status).toBeTruthy
+
+      try {
+        await contract.methods.do(MANAGER_LAMBDA.transferImplicit("tz1eY5Aqa1kXDFoiebL28emyXFoneAoVg1zh", 50 * 1000000)).send({ amount: 0 })
+        fail('Should throw during transfer with amount higher than balance')
+      } catch (ex: any) {
+        expect(ex.message).toMatch('subtraction_underflow') 
+      }
+      done();
+    })
+
     mondaynet('tests manager transfer scenarios with wallet APi contract', async (done) => {
       const op = await Tezos.wallet.originate({
         balance: "1",
@@ -96,10 +143,7 @@ CONFIGS().forEach(({ lib, rpc, setup, knownBaker, knownContract, protocol }) => 
         await contract.methods.do(MANAGER_LAMBDA.transferImplicit("tz1eY5Aqa1kXDFoiebL28emyXFoneAoVg1zh", 50 * 1000000)).send({ amount: 0 })
         fail('Should throw during transfer with amount higher than balance')
       } catch (ex: any) {
-        expect(ex.message).toContain('(temporary) proto.alpha.tez.subtraction_underflow')
-        (protocol === Protocols.PtKathman) ? 
-        expect(ex.message).toMatch('(temporary) proto.014-PtKathma.tez.subtraction_underflow') 
-        : expect(ex.message).toMatch('(temporary) proto.013-PtJakart.contract.balance_too_low') 
+        expect(ex.message).toMatch('subtraction_underflow') 
       }
       done();
     })
