ba171d6a0700db95bca67d63c15c5719e6851eb4
roxaneletourneau <roxane@ecadlabs.com>
2022-08-10 15:56:39 -0700

    Fix per review

--- packages/taquito/src/read-provider/interface.ts
+++ packages/taquito/src/read-provider/interface.ts
@@ -114,8 +114,8 @@ export interface TzReadProvider {
 
   /**
    * @description Access the sapling state of a smart contract.
-   * @param contractAddress Address of the smart contract
-   * @param block from which we want to retrieve the sapling state
+   * @param contractAddress The address of the smart contract
+   * @param block The block you want to retrieve the sapling state from
    */
   getSaplingDiffByContract(
     contractAddress: string,
