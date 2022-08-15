12a8923182775b285289571bb204787ef1a457d0
Davis Sawali <davis.sawali@ecadlabs.com>
2022-08-15 13:39:36 -0700

    added plugin to use env variables

--- website/src/theme/firebase.tsx
+++ website/src/theme/firebase.tsx
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
-
 // Initialize Firebase and Firestore
-const app = initializeApp(firebaseConfig)
+const app = initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY as string,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.FIREBASE_APP_ID as string, 
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID as string
+});
+
 const db = getFirestore(app)
 export {db}
