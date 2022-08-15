63d0fdeb003b859189cc13542e666b165fa09399
roxaneletourneau <roxane@ecadlabs.com>
2022-08-15 15:34:32 -0700

    Revomed unexpected changes to contract-simple-origination test file

--- integration-tests/contract-simple-origination.spec.ts
+++ integration-tests/contract-simple-origination.spec.ts
@@ -1,9 +1,7 @@
-import { Schema } from "@taquito/michelson-encoder";
 import { CONFIGS } from "./config";
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
   const Tezos = lib;
-  const test = require('jest-retries');
 
   describe(`Originating a contract api using: ${rpc}`, () => {
 
@@ -11,12 +9,21 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
       await setup()
       done()
     })
-    test('Simple origination scenario', 2, async (done: () => void) => {
-      const schema = new Schema({
-        "prim": "sapling_transaction",
-        "args": [{ "prim":"8" }]
-    });
-      console.log(schema.generateSchema())
+    test('Simple origination scenario', async (done) => {
+      const op = await Tezos.contract.originate({
+        balance: "1",
+        code: `parameter string;
+        storage string;
+        code {CAR;
+              PUSH string "Hello ";
+              CONCAT;
+              NIL operation; PAIR};
+        `,
+        init: `"test"`
+      })
+      await op.confirmation()
+      expect(op.hash).toBeDefined();
+      expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY)
       done();
     });
   });
