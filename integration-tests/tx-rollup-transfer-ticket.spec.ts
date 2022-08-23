d93b812cf7018c8af4e66c3a77179960686b7011
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2022-08-23 15:22:30 -0700

    try test

--- integration-tests/tx-rollup-transfer-ticket.spec.ts
+++ integration-tests/tx-rollup-transfer-ticket.spec.ts
@@ -1,11 +1,9 @@
 import { Protocols } from "@taquito/taquito";
-import { RpcContractProvider } from "taquito/src/contract/rpc-contract-provider";
-import { TransferTicketOperation } from "taquito/src/operations/transfer-ticket-operation";
 import { TransferTicketParams } from "taquito/src/operations/types";
 import { CONFIGS } from "./config";
 
 
-CONFIGS().forEach(({ lib, setup, protocol }) => {
+CONFIGS().forEach(({ lib, setup, protocol, txRollupDepositContract, txRollupWithdrawContract }) => {
   const Tezos = lib;
   const mondaynet = protocol === Protocols.ProtoALpha ? test : test.skip;
 
@@ -15,27 +13,32 @@ CONFIGS().forEach(({ lib, setup, protocol }) => {
       done();
     })
     mondaynet("transfer tickets L2 to L1 final step in toru node rollup back to L1", async (done) => {
-      // const transferTicketOperation = new TransferTicketOperation()
-      const estimateParams: TransferTicketParams = {
-        source: "tz1",
+      // if (txRollupDepositContract && txRollupWithdrawContract) {
 
-        ticketContents: {},
-        ticketTy: {},
-        ticketTicketer: '',
-        ticketAmount: 2,
-        destination: "tz1",
-        entrypoint: "default"
-      }
-      const estimate = await Tezos.estimate.transferTicket(estimateParams)
+        const estimateParams: TransferTicketParams = {
+          source: "tz1",
 
-      const params: TransferTicketParams = {
-        ...estimateParams,
-        fee: estimate.burnFeeMutez,
-        gasLimit: estimate.gasLimit,
-        storageLimit: estimate.storageLimit
-      }
+          ticketContents: { "string": "foobar" },
+          ticketTy: { "prim": "string" },
+          ticketTicketer: txRollupDepositContract || "KT1ENUSEPM5vEoLCB4FnNz5ndCTGFi1Qc314",
+          ticketAmount: 1,
+          destination: txRollupWithdrawContract || "KT1VwmpwP1CjxB5oyBv3WRQctpKF4S5DQw8v",
+          entrypoint: "default"
+        };
+        const estimate = await Tezos.estimate.transferTicket(estimateParams);
 
-      const result = await Tezos.contract.transferTicket(params)
+        const params: TransferTicketParams = {
+          ...estimateParams,
+          fee: estimate.burnFeeMutez,
+          gasLimit: estimate.gasLimit,
+          storageLimit: estimate.storageLimit
+        };
+
+        const result = await Tezos.contract.transferTicket(params);
+
+        expect(result).toEqual({})
+      // }
+      done();
     })
   })
 })
