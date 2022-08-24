81d2332265dba46716bd4fedbea09c11861536e9
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2022-08-24 14:48:09 -0700

    add proper values

--- integration-tests/tx-rollup-transfer-ticket.spec.ts
+++ integration-tests/tx-rollup-transfer-ticket.spec.ts
@@ -2,11 +2,12 @@ import { OperationContentsAndResultTransferTicket } from "@taquito/rpc";
 import { Protocols } from "@taquito/taquito";
 import { TransferTicketParams } from "taquito/src/operations/types";
 import { CONFIGS } from "./config";
+import { InMemorySigner } from "@taquito/signer"
 
 
 CONFIGS().forEach(({ lib, setup, protocol, txRollupDepositContract, txRollupWithdrawContract }) => {
   const Tezos = lib;
-  const mondaynet = protocol === Protocols.ProtoALpha ? test : test.skip;
+  const mondaynet = protocol === Protocols.ProtoALpha ? test: test.skip;
 
   describe(`mondaynet test TransferTicket operation`, () => {
     beforeEach(async (done) => {
@@ -14,7 +15,10 @@ CONFIGS().forEach(({ lib, setup, protocol, txRollupDepositContract, txRollupWith
       done();
     })
     mondaynet("transfer tickets L2 to L1 final step in toru node rollup back to L1", async (done) => {
+      try {
+
       // if (txRollupDepositContract && txRollupWithdrawContract) {
+        // secret key will need to be the person the tickets were withdrawn to
         const signer = await InMemorySigner.fromSecretKey("edsk2vSxPjmjjvSGoXQUTgUiRw91Ws9FzFF9V63CZNgdF1a6UWDvWZ")
         await Tezos.setProvider({signer})
         const estimateParams: TransferTicketParams = {
@@ -42,17 +46,21 @@ CONFIGS().forEach(({ lib, setup, protocol, txRollupDepositContract, txRollupWith
         expect(estimate.gasLimit).toBeLessThan(Number.POSITIVE_INFINITY)
         expect(estimate.storageLimit).toBeLessThan(Number.POSITIVE_INFINITY)
 
-
         expect(transferResult.kind).toEqual('transfer_ticket');
-        expect(transferResult.destination).toEqual(txRollupWithdrawContract);
-        expect(transferResult.ticket_ticketer).toEqual(txRollupDepositContract);
-        expect(transferResult.ticket_amount).toBeLessThan(Number.POSITIVE_INFINITY);
+        const expectedDest = txRollupWithdrawContract ? txRollupWithdrawContract : "KT1VwmpwP1CjxB5oyBv3WRQctpKF4S5DQw8v"
+        const expectedTicketer = txRollupDepositContract ? txRollupDepositContract : "KT1ENUSEPM5vEoLCB4FnNz5ndCTGFi1Qc314"
+        expect(transferResult.destination).toEqual(expectedDest);
+        expect(transferResult.ticket_ticketer).toEqual(expectedTicketer);
+        expect(transferResult.ticket_amount).toEqual('1');
         expect(transferResult.ticket_ty).toEqual(params.ticketTy);
         expect(transferResult.entrypoint).toEqual('default');
         expect(transferResult.metadata.operation_result.status).toEqual('applied')
         expect(transferResult.metadata.operation_result.balance_updates).toEqual([])
         expect(transferResult.metadata).toBeDefined()
       // }
+      } catch (err) {
+        throw err;
+      }
       done();
     })
   })
