15aaadfe11f80fa39f630f40e80afae742ddb191
Davis Sawali <davis.sawali@ecadlabs.com>
2022-08-29 13:40:11 -0700

    removed unused variable

---
+++ packages/taquito/src/operations/increase-paid-storage-operation.ts
@@ -0,0 +1,73 @@
+import {
+  OperationContentsAndResult,
+  OperationContentsAndResultIncreasePaidStorage,
+} from '@taquito/rpc';
+import { Context } from '../context';
+import { Operation } from './operations';
+import {
+  FeeConsumingOperation,
+  ForgedBytes,
+  GasConsumingOperation,
+  RPCIncreasePaidStorageOperation,
+  StorageConsumingOperation,
+} from './types';
+
+/**
+ *
+ * @description
+ *
+ *
+ */
+
+export class IncreasePaidStorageOperation
+  extends Operation
+  implements GasConsumingOperation, StorageConsumingOperation, FeeConsumingOperation
+{
+  constructor(
+    hash: string,
+    private readonly params: RPCIncreasePaidStorageOperation,
+    public readonly source: string,
+    raw: ForgedBytes,
+    results: OperationContentsAndResult[],
+    context: Context
+  ) {
+    super(hash, raw, results, context);
+  }
+
+  get operationResults() {
+    const increasePaidStorageOp =
+      Array.isArray(this.results) &&
+      (this.results.find(
+        (op) => op.kind === 'increase_paid_storage'
+      ) as OperationContentsAndResultIncreasePaidStorage);
+    const result =
+      increasePaidStorageOp &&
+      increasePaidStorageOp.metadata &&
+      increasePaidStorageOp.metadata.operation_result;
+    return result ? result : undefined;
+  }
+
+  get status() {
+    return this.operationResults?.status ?? 'unknown';
+  }
+
+  get fee() {
+    return this.params.fee;
+  }
+
+  get gasLimit() {
+    return this.params.gas_limit;
+  }
+
+  get storageLimit() {
+    return this.params.storage_limit;
+  }
+
+  get errors() {
+    return this.operationResults?.errors;
+  }
+
+  get consumedMilliGas() {
+    return this.operationResults?.consumed_milligas;
+  }
+}
