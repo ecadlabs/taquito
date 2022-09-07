a61c205ecf391c0c29740dc50fdf7b7beb4932ea
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2022-09-07 11:43:46 -0700

    add timeout and check for finalized

--- integration-tests/tx-rollup-transfer-ticket.spec.ts
+++ integration-tests/tx-rollup-transfer-ticket.spec.ts
@@ -1,10 +1,11 @@
-import { OperationContentsAndResultTransferTicket } from "@taquito/rpc";
+import { OperationContentsAndResultTransferTicket, RpcClient } from "@taquito/rpc";
 import { Protocols } from "@taquito/taquito";
 import { TransferTicketParams } from "taquito/src/operations/types";
 import { CONFIGS } from "./config";
 import { InMemorySigner } from "@taquito/signer"
 import { ticketsWithdrawal } from "./data/tickets_withdrawal_contract";
 import { ticketsDeposit } from "./data/tickets_deposit_contract";
+import { HttpBackend } from "@taquito/http-utils";
 
 
 CONFIGS().forEach(({ lib, setup, protocol, txRollupDepositContract, txRollupWithdrawContract }) => {
@@ -18,47 +19,56 @@ CONFIGS().forEach(({ lib, setup, protocol, txRollupDepositContract, txRollupWith
     })
     mondaynet("transfer tickets L2 to L1 final step in toru node rollup back to L1", async (done) => {
       try {
-      // if (txRollupDepositContract && txRollupWithdrawContract) {
+
+        const backend = new HttpBackend
+        const req = await backend.createRequest<{metadata: {finalized: boolean}}>({ url: 'http://mondaynet.ecadinfra.com:9999/block/head', method: 'GET'})
+        expect(typeof req.metadata.finalized).toEqual('boolean')
+        const checkFinalized = async () => {
+          if (req.metadata.finalized) {
+            const signer = await InMemorySigner.fromSecretKey(process.env['TX_ROLLUP_TICKETS_OWNER_SECRET'] || '')
+            await Tezos.setProvider({signer})
+            const estimateParams: TransferTicketParams = {
+              ticketContents: { "string": "foobar" },
+              ticketTy: { "prim": "string" },
+              ticketTicketer: txRollupDepositContract,
+              ticketAmount: 1,
+              destination: txRollupWithdrawContract,
+              entrypoint: "default"
+            };
+            const estimate = await Tezos.estimate.transferTicket(estimateParams);
+
+            const params: TransferTicketParams = {
+              ...estimateParams,
+              fee: estimate.burnFeeMutez,
+              gasLimit: estimate.gasLimit,
+              storageLimit: estimate.storageLimit
+            };
+
+            const res = await Tezos.contract.transferTicket(params);
+            const results = res.results
+            const transferResult = results[0] as OperationContentsAndResultTransferTicket
+
+            expect(estimate.burnFeeMutez).toBeLessThan(Number.POSITIVE_INFINITY)
+            expect(estimate.gasLimit).toBeLessThan(Number.POSITIVE_INFINITY)
+            expect(estimate.storageLimit).toBeLessThan(Number.POSITIVE_INFINITY)
+
+            expect(transferResult.kind).toEqual('transfer_ticket');
+            const expectedDest = txRollupWithdrawContract
+            const expectedTicketer = txRollupDepositContract
+            expect(transferResult.destination).toEqual(expectedDest);
+            expect(transferResult.ticket_ticketer).toEqual(expectedTicketer);
+            expect(transferResult.ticket_amount).toEqual('1');
+            expect(transferResult.ticket_ty).toEqual(params.ticketTy);
+            expect(transferResult.entrypoint).toEqual('default');
+            expect(transferResult.metadata.operation_result.status).toEqual('applied')
+            expect(transferResult.metadata.operation_result.balance_updates).toEqual([])
+            expect(transferResult.metadata).toBeDefined()
+          }
+          return setTimeout(() => checkFinalized, 60000)
+        }
+        await checkFinalized()
         // secret key will need to be the person the tickets were withdrawn to
-        const signer = await InMemorySigner.fromSecretKey(process.env['TX_ROLLUP_TICKETS_OWNER_SECRET'] || '')
-        await Tezos.setProvider({signer})
-        const estimateParams: TransferTicketParams = {
-          ticketContents: { "string": "foobar" },
-          ticketTy: { "prim": "string" },
-          ticketTicketer: txRollupDepositContract,
-          ticketAmount: 1,
-          destination: txRollupWithdrawContract,
-          entrypoint: "default"
-        };
-        const estimate = await Tezos.estimate.transferTicket(estimateParams);
 
-        const params: TransferTicketParams = {
-          ...estimateParams,
-          fee: estimate.burnFeeMutez,
-          gasLimit: estimate.gasLimit,
-          storageLimit: estimate.storageLimit
-        };
-
-        const res = await Tezos.contract.transferTicket(params);
-        const results = res.results
-        const transferResult = results[0] as OperationContentsAndResultTransferTicket
-
-        expect(estimate.burnFeeMutez).toBeLessThan(Number.POSITIVE_INFINITY)
-        expect(estimate.gasLimit).toBeLessThan(Number.POSITIVE_INFINITY)
-        expect(estimate.storageLimit).toBeLessThan(Number.POSITIVE_INFINITY)
-
-        expect(transferResult.kind).toEqual('transfer_ticket');
-        const expectedDest = txRollupWithdrawContract
-        const expectedTicketer = txRollupDepositContract
-        expect(transferResult.destination).toEqual(expectedDest);
-        expect(transferResult.ticket_ticketer).toEqual(expectedTicketer);
-        expect(transferResult.ticket_amount).toEqual('1');
-        expect(transferResult.ticket_ty).toEqual(params.ticketTy);
-        expect(transferResult.entrypoint).toEqual('default');
-        expect(transferResult.metadata.operation_result.status).toEqual('applied')
-        expect(transferResult.metadata.operation_result.balance_updates).toEqual([])
-        expect(transferResult.metadata).toBeDefined()
-      // }
       } catch (err) {
         throw err;
       }
