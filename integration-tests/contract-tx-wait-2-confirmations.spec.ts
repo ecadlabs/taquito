4a0d5672c3a72a08eedc4d3ad384a45cfdf2788a
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-09-21 08:22:25 -0700

    pr split nine - add 10 files (#1817)
    * pr split nine - add 10 files
    
    * changes per review
    
    * forgt to save one file
    
    * change per review
    
    * changes per review

--- integration-tests/contract-tx-wait-2-confirmations.spec.ts
+++ integration-tests/contract-tx-wait-2-confirmations.spec.ts
@@ -2,14 +2,14 @@ import { CONFIGS } from "./config";
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
   const Tezos = lib;
-  describe(`Test tx and waiting for 2 confirmations using: ${rpc}`, () => {
+   describe(`Test tx and waiting for 2 confirmations through contract api using: ${rpc}`, () => {
 
     beforeEach(async (done) => {
       await setup()
       done()
     })
-    test('transfers 0.02 tez and waits for 2 confirmations', async (done) => {
-      const op = await Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 0.02 })
+    test('Verify contract.transfer for 2 XTZ to an address and waits for 2 confirmations', async (done) => {
+      const op = await Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 2 })
       await op.confirmation()
       expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
       expect(Number(op.consumedGas)).toBeGreaterThan(0);
