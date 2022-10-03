2c19db7ccd57976f9348f87bacb049fcefad46f5
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-10-03 15:11:38 -0700

    test(mondaynet): mondaynet gap probe

--- integration-tests/known-contracts-ProtoALph.ts
+++ integration-tests/known-contracts-ProtoALph.ts
@@ -1,5 +1,5 @@
-export const knownContractProtoALph = "KT1LfR66GcVP9nBPwUf34xUC4go2wb1NYex8";
-export const knownBigMapContractProtoALph = "KT1MsswzJwY3AAnWXMDc5f5ZmLG89bf8kweW";
-export const knownTzip12BigMapOffChainContractProtoALph = "KT1V1yUaK8svcHwGbwL6EwTiQBW2Wjsb3tu9";
-export const knownSaplingContractProtoALph = "KT1NLd6Q9ZdDbjiKp53rhUUTsD4RrGMJv21v";
-export const knownOnChainViewContractAddressProtoALph = "KT1G4Lkj2rvPHc6EAoZXPcSTqPebdN4TTb58";
+export const knownContractProtoALph = "KT1RGr4RZKGJTtRhWS7Awrf9mdDY3X8hWGXf";
+export const knownBigMapContractProtoALph = "KT1RqJRpocvvtqygn1YUko9VGzJpXYvX2FQ5";
+export const knownTzip12BigMapOffChainContractProtoALph = "KT1N8ztbzbh4rb4nNQ5ANv9DdbWbqDRy4w9W";
+export const knownSaplingContractProtoALph = "KT1Uye8Y7SoJmPAR723J5BS9dekzSkTnkdKY";
+export const knownOnChainViewContractAddressProtoALph = "KT1UF7P8fRGiapqyAkpFYpMLuBujbGdHyRum";
--- integration-tests/known-contracts-PtKathman.ts
+++ integration-tests/known-contracts-PtKathman.ts
@@ -1,5 +1,5 @@
-export const knownContractPtKathman = "KT1HV6q4sVuoR2WpegZAQzwbHDRJC41bdHuM";
-export const knownBigMapContractPtKathman = "KT1MCMyxx8XfSwYKEozctujb7MsVCqjePKke";
-export const knownTzip12BigMapOffChainContractPtKathman = "KT1LGjmbdMEUAks1KmVZ2F2GtamJCeyaUpvm";
-export const knownSaplingContractPtKathman = "KT1WbNeWhByy3upjNbN6Dh47cFZTnCTk4eBJ";
-export const knownOnChainViewContractAddressPtKathman = "KT1Tx3U67PnKXaVW9ZD5YdG1Kh93cxyvBhEF";
+export const knownContractPtKathman = "KT1D18x7eTCjWCNzqoZvQ5hxubv8PckT4L6v";
+export const knownBigMapContractPtKathman = "KT1BgVFy6gvom1kVd8dRGnpTtLtjyv2XRovZ";
+export const knownTzip12BigMapOffChainContractPtKathman = "KT1Mtuo2J2RgWZYT6effKeA9doXSSHBaQjjf";
+export const knownSaplingContractPtKathman = "KT1CzeXL6zCaArc92nVTsyvbm1F15iKfUJGq";
+export const knownOnChainViewContractAddressPtKathman = "KT1HN9jDVKmKe7Jfam8AeJNKPcP9Xe4fjC77";
