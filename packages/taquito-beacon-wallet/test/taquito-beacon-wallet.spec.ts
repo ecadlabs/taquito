fb68890f81996ed902f11aed2347f77f4c24ae99
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-10-04 14:25:34 -0700

    more tests

--- packages/taquito-beacon-wallet/test/taquito-beacon-wallet.spec.ts
+++ packages/taquito-beacon-wallet/test/taquito-beacon-wallet.spec.ts
@@ -13,37 +13,35 @@ describe('Beacon Wallet tests', () => {
     getPKH: jest.Mock<any>,
     requestPermissions: jest.Mock<any>,
   };
+  let mockClient: {
+    getActiveAccount: jest.Mock<any>,
+    setActiveAccount: jest.Mock<any>
+    requestPermissions: jest.Mock<any>
+  };
   const testPKH = 'tz1h3rQ8wBxFd8L9B3d7Jhaawu6Z568XU3xY'
 
   beforeEach(() => {
     jest.resetAllMocks()
 
+    mockClient = {
+      getActiveAccount: jest.fn(),
+      setActiveAccount: jest.fn(),
+      requestPermissions: jest.fn()
+    }
+
     mockBeacon = {
       getPKH: jest.fn(),
-      requestPermissions: jest.fn()
+      requestPermissions: jest.fn(),
     };
 
-    mockBeacon.getPKH.mockResolvedValue(testPKH)
-    mockBeacon.requestPermissions.mockResolvedValue({
+    mockClient.setActiveAccount.mockResolvedValue(testPKH)
+    mockClient.requestPermissions.mockResolvedValue({
       network: {
         type: 'custom' as NetworkType,
       }
     })
+    mockBeacon.getPKH.mockResolvedValue(testPKH)
   })
-
-
-  it(`Verify getPKH`, async () => {
-    // const client = getDAppClientInstance({ name: 'My Sample DApp' })
-    // const perms = client.requestPermissions(mockBeacon.requestPermissions())  
-
-    // console.log(perms)   
-    // expect(await client.getActiveAccount()).toEqual(testPKH);
-    //  const pkh = await current.getPKH();
-    //  expect(pkh).toEqual(testPKH);
-    expect(
-      mockBeacon.requestPermissions()
-    ).toBeCalled
-  });
 
   it('Verify that BeaconWallet is instantiable', () => {
     expect(new BeaconWallet({ name: 'testWallet' })).toBeInstanceOf(BeaconWallet);
@@ -55,6 +53,16 @@ describe('Beacon Wallet tests', () => {
 
   it('Verify BeaconWallet permissions scopes not granted error', () => {
     expect(new MissingRequiredScopes([PermissionScope.OPERATION_REQUEST])).toBeInstanceOf(Error);
+  });
+
+  it('Verify that requestPermissions is a function', async () => {
+    const wallet = new BeaconWallet({ name: 'testWallet' })
+    expect(typeof (await wallet.requestPermissions)).toEqual('function')
+    expect((await wallet.requestPermissions)).toBeDefined
+    const network = { type: NetworkType.JAKARTANET };
+    const permissions = mockBeacon.requestPermissions({ network });
+    expect(permissions).toBeDefined
+    console.log(permissions)
   });
 
   it('Verify that permissions must be called before getPKH', async () => {
@@ -100,6 +108,43 @@ describe('Beacon Wallet tests', () => {
     const formattedParam = await wallet.formatParameters({ gasLimit: 40 });
     expect(formattedParam.gasLimit).toEqual('40');
   });
+
+  it(`Verify removeDefaultParameters for fees`, async () => {
+    const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
+    const formattedParam = await wallet.removeDefaultParams({ fee: 10 }, { fee: 30 });
+    expect(formattedParam.fee).toEqual(30);
+  });
+
+  it(`Verify removeDefaultParameters for storageLimit`, async () => {
+    const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
+    const formattedParam = await wallet.removeDefaultParams({ storageLimit: 2000 }, { storageLimit: 165 });
+    expect(formattedParam.storageLimit).toEqual(165);
+  });
+
+  it(`Verify removeDefaultParameters for gas limit`, async () => {
+    const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
+    const formattedParam = await wallet.removeDefaultParams({ gasLimit: 40 }, { gasLimit: 80 });
+    expect(formattedParam.gasLimit).toEqual(80);
+  });
+
+  it(`Verify disconnect`, async () => {
+    const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
+    const disconnected = await wallet.disconnect();
+    expect(disconnected).toBeTruthy;
+  });
+
+  it(`Verify clearActiveAccount`, async () => {
+    const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
+    const cleared = await wallet.clearActiveAccount()
+    expect(cleared).toBeTruthy;
+  });
+
+  // it(`Verify validateRequiredScopesOrFail`, async () => {
+  //   const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
+  //   const mapped = await wallet.validateRequiredScopesOrFail('sign', 'sign')
+  //   expect(mapped).toBeTruthy;
+  //   console.log(mapped)
+  // });
 
 
 });
