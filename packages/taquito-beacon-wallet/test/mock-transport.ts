c866f1072dee8188953795dc1cfaab2562a57c6d
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-10-06 14:31:34 -0700

    test(fix the pr): fix the PR

--- packages/taquito-beacon-wallet/test/mock-transport.ts
+++
@@ -1,6 +0,0 @@
-import { Transport } from '@airgap/beacon-core';
-
-export class MockTransport extends Transport {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  public async listen() {}
-}
