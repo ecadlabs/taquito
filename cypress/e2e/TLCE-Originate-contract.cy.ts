8ad2125822b11b093d9f63506029927535dcc78d
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-10-16 14:54:49 -0700

    update

--- cypress/e2e/TLCE-Originate-contract.cy.ts
+++ cypress/e2e/TLCE-Originate-contract.cy.ts
@@ -4,7 +4,7 @@ import { base_url, disclaimer, runButton, playgroundPreview } from './base'
 
 describe('Taquito Live Code Examples - Originate - contract', () => {
 
-  Cypress.config('defaultCommandTimeout', 30000);
+  Cypress.config('defaultCommandTimeout', 60000);
   const page_under_test = base_url + "originate"
 
   it('a. Initializing storage using a Plain Old JavaScript Object - contract', () => {
