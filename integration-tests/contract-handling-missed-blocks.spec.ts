c34efeb9130503da2562e5575b39b8a406a6c1b7
Roxane Létourneau <65877347+roxaneletourneau@users.noreply.github.com>
2022-10-17 13:58:41 -0400

    fix(confirmationPollingTimeoutSecond): fix for contract/wallet API (#2024)
    * fix(confirmationpollingtimeoutsecond): fixed confirmationPollingTimeoutSecond contract/wallet API
    
    The timeout was not taken into consideration on both the contract and wallet API when confirming the
    operations. In cases where an operation was not found, our integration tests would never end instead
    of throwing a timeout error.
    
    fix #2006
    
    * moved sleep util function to the config file

--- integration-tests/contract-handling-missed-blocks.spec.ts
+++ integration-tests/contract-handling-missed-blocks.spec.ts
@@ -1,19 +1,23 @@
 import { PollingSubscribeProvider } from "@taquito/taquito";
+import BigNumber from "bignumber.js";
 import { CONFIGS } from "./config";
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
   const Tezos = lib;
-   describe(`Test handling of missed blocks through contract api using: ${rpc}`, () => {
+  let timeBetweenBlocks: BigNumber;
+  describe(`Test handling of missed blocks through contract api using: ${rpc}`, () => {
 
     beforeEach(async (done) => {
-      await setup()
+      await setup();
+      timeBetweenBlocks = (await Tezos.rpc.getConstants()).delay_increment_per_round ?? new BigNumber(15);
       done()
     })
+
     it('Verify the operation is found even if the poller skipped blocks', async (done) => {
       // To simulate the behavior where blocks are skipped, we use a polling interval higher than the time between blocks
       // Before fixing issue #1783, if the block containing the operation was skipped, the operation was never found, and the test ended with a timeout
       // After fixing issue #1783, if blocks are skipped, they will be retrieved and inspected by the Operation class
-      Tezos.setStreamProvider(Tezos.getFactory(PollingSubscribeProvider)({ pollingIntervalMilliseconds: 60000 }));
+      Tezos.setStreamProvider(Tezos.getFactory(PollingSubscribeProvider)({ pollingIntervalMilliseconds: Number(timeBetweenBlocks.multipliedBy(4000)) }));
       const op = await Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 2 });
       await op.confirmation();
       expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
