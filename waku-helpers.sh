dd0f463a2eddb31573fb8fc64a81744ef1f53991
Edward Garson <egarson@gmail.com>
2022-10-21 10:19:08 -0700

    Add subscribe_to_topics, debug capability

--- waku-helpers.sh
+++ waku-helpers.sh
@@ -20,12 +20,18 @@ _read_dot_env_file() {
     export $(grep -v '^#' .env | xargs)
 }
 
+_DEBUG=0  # 0=on, !0=off
+_debugging() { (($_DEBUG == 0)); }
+# Conveniently refresh current shell with changes made to this file
+_reload_waku_helpers() { source waku-helpers.sh; }
+alias rwh=_reload_waku_helpers
+
 # Start a local nwaku node that connects to two known peers on localhost, 
 # enabling rest, rest-admin and rest-private.
 # See: https://github.com/status-im/nwaku/blob/master/docs/operators/how-to/connect.md
 start_waku_node() {
     _read_dot_env_file
-    (($+NWAKU_LOCATION)) || { _err 'NWAKU_DIR variable not set in .env' && return 2 }
+    (($+NWAKU_DIR)) || { _err 'NWAKU_DIR variable not set in .env' && return 2 }
     binary_not_found='build/wakunode2 not found (did you build?)' # Build it: https://is.gd/NXeAnv
     [[ ! -f $NWAKU_DIR/build/wakunode2 ]] && _err "${NWAKU_DIR}/${binary_not_found}" && return 3
     $NWAKU_DIR/build/wakunode2 \
@@ -38,15 +44,30 @@ alias swn=start_waku_node
 # N.B. nwaku features both a JSON RPC api and a RESTful one; this calls JSON RPC
 _call_json_rpc() {
     (($# != 1)) && _err 'Usage: _call_json_rpc <payload>' && return 1
-    curl -d $1 --silent --header "Content-Type: application/json" http://localhost:8545
+    payload=$1
+    # TODO Validate that localhost:8545 is an active TCP connection
+    curl -d $payload --silent --header "Content-Type: application/json" http://localhost:8545
 }
 
-perform_health_check() {
-    payload='{"jsonrpc":"2.0","id":"id","method":"get_waku_v2_debug_v1_info", "params":[]}'
+_make_payload() { echo "{ \"jsonrpc\":\"2.0\",\"id\":\"id\",\"method\":\"$1\", \"params\":[$2] }"; }
+
+get_waku_debug_info() {
+    payload=$(_make_payload 'get_waku_v2_debug_v1_info')
     _call_json_rpc $payload | jq
 }
-alias phc=perform_health_check
+alias gwd=get_waku_debug_info
 
 # RESTful alternative to the above JSON RPC. Seems tastier to me.
 # See https://github.com/status-im/nwaku/blob/master/docs/api/v2/rest-api.md
-perform_health_check2() { curl http://localhost:8645/debug/v1/info -s | jq; }
+get_waku_debug_info2() { curl http://localhost:8645/debug/v1/info -s | jq; }
+alias gwd2=get_waku_debug_info2
+
+# Subscribe to the given topic(s) [via JSON RPC].
+# Pass multiple topics as a single arg: `subscribe_to_topic '"topic_1","topic_2"'`
+subscribe_to_topics() {
+    (($# != 1)) && _err 'Usage: subscribe_to_topic '\''"t1","t2"'\''' && return 1
+    payload=$(_make_payload 'post_waku_v2_relay_v1_subscriptions' "[$@]")
+    _debugging && echo $payload
+    _call_json_rpc $payload
+}
+alias stt=subscribe_to_topics
