c233869a4a07eb7210e6766a87a1aa6396848743
roxaneletourneau <roxane@ecadlabs.com>
2022-10-24 12:29:50 -0700

    Use the wallet API for transfer operation and added a live code example

--- packages/taquito-wallet-connect/src/taquito-wallet-connect.ts
+++ packages/taquito-wallet-connect/src/taquito-wallet-connect.ts
@@ -6,81 +6,7 @@
 import Client from '@walletconnect/sign-client';
 import { SignClientTypes, ProposalTypes, SessionTypes } from '@walletconnect/types';
 import QRCodeModal from '@walletconnect/qrcode-modal';
-
-export class WalletConnect2 {
-  public client: Client;
-  private session: SessionTypes.Struct | undefined;
-
-  constructor(client: Client) {
-    this.client = client;
-  }
-
-  static async init(options: SignClientTypes.Options) {
-    const client = await Client.init(options);
-    return new WalletConnect2(client);
-  }
-
-  async requestPermissions(scope: {
-    pairingTopic?: string;
-    requiredNamespaces: ProposalTypes.RequiredNamespaces;
-  }) {
-    try {
-      console.log('Requesting permission...');
-      const { uri, approval } = await this.client.connect(scope);
-
-      if (uri) {
-        QRCodeModal.open(uri, () => {
-          console.log('EVENT', 'QR Code Modal closed');
-        });
-      }
-      this.session = await approval();
-      console.log('Established session:', this.session);
-    } catch (e) {
-      console.log(e);
-    } finally {
-      QRCodeModal.close();
-    }
-  }
-
-  async signPayload(payload: string) {
-    const topic = this.session?.topic ?? '';
-    const response = await this.client.request<string>({
-      topic,
-      chainId: this.session?.requiredNamespaces['tezos'].chains[0] ?? '',
-      request: {
-        method: 'tezos_signMessage',
-        params: {
-          address: await this.getPKH(),
-          expression: payload,
-        },
-      },
-    });
-    return response;
-  }
-
-  async sendOperations(params: any[]) {
-    const topic = this.session?.topic ?? '';
-    const response = await this.client.request<string>({
-      topic,
-      chainId: this.session?.requiredNamespaces['tezos'].chains[0] ?? '',
-      request: {
-        method: 'tezos_sendOperations',
-        params: {
-          account: await this.getPKH(),
-          operations: params,
-        },
-      },
-    });
-    return response;
-  }
-
-  async getPKH() {
-    return this.session?.namespaces['tezos'].accounts[0].split(':')[2] ?? '';
-  }
-}
-/* import { WalletDelegateParams, WalletOriginateParams, WalletProvider, WalletTransferParams } from '@taquito/taquito';
-import Client from "@walletconnect/sign-client";
-import { SignClientTypes, ProposalTypes, SessionTypes } from "@walletconnect/types";
+import { createTransferOperation, WalletDelegateParams, WalletOriginateParams, WalletProvider, WalletTransferParams } from '@taquito/taquito';
 
 export class WalletConnect2 implements WalletProvider {
     public client: Client;
@@ -92,54 +18,54 @@ export class WalletConnect2 implements WalletProvider {
 
     static async init(options: SignClientTypes.Options) {
         const client = await Client.init(options);
-        return new WalletConnect2(client)
+        return new WalletConnect2(client);
     }
 
     async requestPermissions(scope: {
-        pairingTopic?: string,
-        requiredNamespaces: ProposalTypes.RequiredNamespaces,
+        pairingTopic?: string;
+        requiredNamespaces: ProposalTypes.RequiredNamespaces;
     }) {
-        const { approval } = await this.client.connect(scope);
-        this.session = await approval();
+        try {
+            console.log('Requesting permission...');
+            const { uri, approval } = await this.client.connect(scope);
+
+            if (uri) {
+                QRCodeModal.open(uri, () => {
+                    console.log('EVENT', 'QR Code Modal closed');
+                });
+            }
+            this.session = await approval();
+            console.log('Established session:', this.session);
+        } catch (e) {
+            console.log(e);
+        } finally {
+            QRCodeModal.close();
+        }
     }
 
     async signPayload(payload: string) {
-        const topic = this.session?.topic?? '';
+        const topic = this.session?.topic ?? '';
         const response = await this.client.request<string>({
             topic,
-            chainId: '',
+            chainId: this.session?.requiredNamespaces['tezos'].chains[0] ?? '',
             request: {
-                method: 'tezos_operationRequest',
+                method: 'tezos_signMessage',
                 params: {
                     address: await this.getPKH(),
-                    message: payload
+                    expression: payload,
                 },
             },
-        })
+        });
         return response;
     }
 
-    getActiveAccount() {
-        return this.client.pairing.getAll({ active: true })
-    }
+    async mapTransferParamsToWalletParams(params: () => Promise<WalletTransferParams>) {
+        const walletParams: WalletTransferParams = await params();
 
-    async sendOperations(params: any[]) {
-        const response = await this.client.request<string>({
-            topic: '',
-            chainId: '',
-            request: {
-                method: 'tezos_operationRequest',
-                params,
-            },
-        })
-        return response;
-    }
-
-    async getPKH() {
-        return ''
-    }
-    async mapTransferParamsToWalletParams(_params: () => Promise<WalletTransferParams>) {
-        return {}
+        return this.removeDefaultParams(
+            walletParams,
+            await createTransferOperation(this.formatParameters(walletParams))
+        );
     }
 
     async mapOriginateParamsToWalletParams(_params: () => Promise<WalletOriginateParams>) {
@@ -150,4 +76,55 @@ export class WalletConnect2 implements WalletProvider {
         return {}
     }
 
-} */
+    formatParameters(params: any) {
+        if (params.fee) {
+            params.fee = params.fee.toString();
+        }
+        if (params.storageLimit) {
+            params.storageLimit = params.storageLimit.toString();
+        }
+        if (params.gasLimit) {
+            params.gasLimit = params.gasLimit.toString();
+        }
+        return params;
+    }
+
+    removeDefaultParams(
+        params: WalletTransferParams | WalletOriginateParams | WalletDelegateParams,
+        operatedParams: any
+    ) {
+        // If fee, storageLimit or gasLimit is undefined by user
+        // in case of beacon wallet, dont override it by
+        // defaults.
+        if (!params.fee) {
+            delete operatedParams.fee;
+        }
+        if (!params.storageLimit) {
+            delete operatedParams.storage_limit;
+        }
+        if (!params.gasLimit) {
+            delete operatedParams.gas_limit;
+        }
+        return operatedParams;
+    }
+
+    async sendOperations(params: any[]) {
+        const topic = this.session?.topic ?? '';
+        const response = await this.client.request<string>({
+            topic,
+            chainId: this.session?.requiredNamespaces['tezos'].chains[0] ?? '',
+            request: {
+                method: 'tezos_sendOperations',
+                params: {
+                    account: await this.getPKH(),
+                    operations: params,
+                },
+            },
+        });
+        return response;
+    }
+
+    async getPKH() {
+        return this.session?.namespaces['tezos'].accounts[0].split(':')[2] ?? '';
+    }
+}
