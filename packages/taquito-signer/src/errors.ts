ba1c064ba0ab3788a288cdbe931fd85f3369a6e4
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2022-10-28 11:31:35 -0700

    add type description for errors

--- packages/taquito-signer/src/errors.ts
+++ packages/taquito-signer/src/errors.ts
@@ -1,3 +1,8 @@
+
+/**
+ * @description derivation path 44'/1729' expected for tezos && following must be hardened for ed25519
+ * @name custom message based on error
+ */
 export class InvalidDerivationPathError extends Error {
   public name = 'InvalidDerivationPathError';
   constructor(public message: string) {
@@ -5,6 +10,10 @@ export class InvalidDerivationPathError extends Error {
   }
 }
 
+/**
+ * @description Malformed mnemonic not valid
+ * @name custom name for error
+ */
 export class InvalidMnemonicError extends Error {
   public name = 'InvalidMnemonicError';
   constructor(public message: string) {
@@ -12,6 +21,10 @@ export class InvalidMnemonicError extends Error {
   }
 }
 
+/**
+ * @description Unsupported curve
+ * @name curve name
+ */
 export class InvalidCurveError extends Error {
   public name = 'InvalidCurveError';
   constructor(public curve: string) {
@@ -19,6 +32,9 @@ export class InvalidCurveError extends Error {
   }
 }
 
+/**
+ * @description feature under construciton
+ */
 export class ToBeImplemented extends Error {
   public name = 'ToBeImplemented';
   constructor() {
