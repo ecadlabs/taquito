7021e00433b6f723cbb090ccc4bcfa732ab73a2d
roxaneletourneau <roxane@ecadlabs.com>
2022-11-09 13:40:32 -0800

    Add example session with multiple chains

--- packages/taquito-wallet-connect-2/test/data.ts
+++ packages/taquito-wallet-connect-2/test/data.ts
@@ -43,3 +43,52 @@ export const sessionExample = {
     },
   },
 };
+
+export const sessionMultipleChains = {
+  topic: '5b7d09ec60b46ba32617549cfc62fcc7a79edff14212a0fbecaf38f6aefa09e2',
+  relay: {
+    protocol: 'irn',
+  },
+  expiry: 1668634369,
+  namespaces: {
+    tezos: {
+      accounts: [
+        'tezos:ghostnet:tz2AJ8DYxeRSUWr8zS5DcFfJYzTSNYzALxSh',
+        'tezos:ghostnet:tz2BxqkU3UvZrqA22vbEaSGyjR9bEQwc4k2G',
+        'tezos:ghostnet:tz2JobFgDoJ5HZ1jAoMgZCyNdbBEdAstkytV',
+        'tezos:limanet:tz2AJ8DYxeRSUWr8zS5DcFfJYzTSNYzALxSh',
+        'tezos:limanet:tz2BxqkU3UvZrqA22vbEaSGyjR9bEQwc4k2G',
+        'tezos:limanet:tz2JobFgDoJ5HZ1jAoMgZCyNdbBEdAstkytV',
+      ],
+      methods: ['tezos_signExpression'],
+      events: [],
+    },
+  },
+  acknowledged: true,
+  controller: '9acfbaf43ca1e9a6e1e27d3841c7b71a68176b401f1f29670870e3ca35094d62',
+  self: {
+    publicKey: '72dfcd018c5a636c311a0214c19f24e1e52a0f38082e31e3971af9b0296f4767',
+    metadata: {
+      description: '',
+      url: 'http://localhost:3000',
+      icons: [],
+      name: '',
+    },
+  },
+  peer: {
+    publicKey: '9acfbaf43ca1e9a6e1e27d3841c7b71a68176b401f1f29670870e3ca35094d62',
+    metadata: {
+      name: 'React Wallet',
+      description: 'React Wallet for WalletConnect',
+      url: 'https://walletconnect.com/',
+      icons: ['https://avatars.githubusercontent.com/u/37784886'],
+    },
+  },
+  requiredNamespaces: {
+    tezos: {
+      methods: ['tezos_signExpression'],
+      chains: ['tezos:ghostnet', 'tezos:limanet'],
+      events: [],
+    },
+  },
+};
