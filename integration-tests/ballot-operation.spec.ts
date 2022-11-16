16844f314cf5114342ed4f384a10ddb4bd7acd47
Davis Sawali <davis.sawali@ecadlabs.com>
2022-11-16 14:48:02 -0800

    add integration test for ballot and proposal operation

--- integration-tests/ballot-operation.spec.ts
+++ integration-tests/ballot-operation.spec.ts
@@ -1,17 +1,30 @@
-import { CONFIGS } from './config';
-import { Protocols } from '@taquito/taquito';
+import { CONFIGS, sleep } from './config';
 
-CONFIGS().forEach(({ lib, protocol, setup } ) => {
+CONFIGS().forEach(async ({ lib, protocol, setup, rpc }) => {
   const Tezos = lib;
-  const kathmandunet = protocol === Protocols.PtKathman ? test: test.skip;
+  
+  const flextesanet = (rpc === 'http://0.0.0.0:20000') ? it : it.skip;
 
   describe(`Ballot operation test (${protocol})`, () => {
-    beforeEach(async (done) => {
+    beforeAll(async (done) => {
       await setup();
+      
       done();
     });
 
-    kathmandunet('Inject Ballot operation (ballot vote)', async (done) => {
+    flextesanet('Submit a proposal and inject ballot vote', async (done) => {
+      const proposalsOp = await Tezos.contract.proposals({
+        proposals: ['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']
+      });
+
+      await proposalsOp.confirmation();
+
+      expect(proposalsOp.operationResults).toBeDefined();
+      expect(proposalsOp.operationResults?.proposals).toEqual(['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']);
+      expect(proposalsOp.includedInBlock).toBeDefined();
+      
+      await sleep(16000);
+
       const op = await Tezos.contract.ballot({
         proposal: 'ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK',
         ballot: 'yay'
@@ -20,9 +33,11 @@ CONFIGS().forEach(({ lib, protocol, setup } ) => {
       await op.confirmation();
 
       expect(op.operationResults).toBeDefined();
+      expect(op.operationResults?.proposal).toEqual('ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK');
+      expect(op.operationResults?.ballot).toEqual('yay');
       expect(op.includedInBlock).toBeDefined();
 
       done();
-    })
+    });
   })
 })
