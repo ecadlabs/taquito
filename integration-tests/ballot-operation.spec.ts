d1d38d360583e21983ac27ba674086588e6ff110
Davis Sawali <davis.sawali@ecadlabs.com>
2022-11-17 13:59:47 -0800

    updated test setup scheme

--- integration-tests/ballot-operation.spec.ts
+++ integration-tests/ballot-operation.spec.ts
@@ -10,50 +10,51 @@ CONFIGS().forEach(async ({ lib, protocol, setup, rpc }) => {
   describe(`Ballot operation test (${protocol})`, () => {
     beforeAll(async (done) => {
       await setup();
+
+      try {
+        const period = await Tezos.rpc.getCurrentPeriod();
+        const sleepTime = period.remaining * 1000;
+        await sleep(sleepTime);
+        
+        const proposalsOp = await Tezos.contract.proposals({
+          proposals: ['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']
+        });
+
+        await proposalsOp.confirmation();
+      } catch(e) {
+        console.log(e);
+      }
       done();
     });
 
     flextesanet('Submit a proposal and inject ballot vote', async (done) => {
-      
+      // expect(proposalsOp.operationResults).toBeDefined();
+      // expect(proposalsOp.operationResults?.proposals).toEqual(['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']);
+      // expect(proposalsOp.includedInBlock).toBeDefined();
+
       const period = await Tezos.rpc.getCurrentPeriod();
-      const sleepTime = period.remaining * 1000;
-      await sleep(sleepTime);
-      
-      const proposalsOp = await Tezos.contract.proposals({
-        proposals: ['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']
-      });
-
-      await proposalsOp.confirmation();
-
-      expect(proposalsOp.operationResults).toBeDefined();
-      expect(proposalsOp.operationResults?.proposals).toEqual(['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']);
-      expect(proposalsOp.includedInBlock).toBeDefined();
+      // console.log(JSON.stringify(period));
 
 
-      const proposal = await Tezos.rpc.getCurrentProposal();
-      if (proposal !== 'ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK') {
-        console.log('WRONG PROPOSAL');
+      if (period.voting_period.kind === 'proposal') {
+        await sleep(period.remaining * 1000);
+      } else if (period.voting_period.kind === 'exploration') {
+        try {
+          const op = await Tezos.contract.ballot({
+            proposal: 'ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK',
+            ballot: 'yay'
+          });
+          await op.confirmation();
+  
+          expect(op.operationResults).toBeDefined();
+          expect(op.operationResults?.proposal).toEqual('ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK');
+          expect(op.operationResults?.ballot).toEqual('yay');
+          expect(op.includedInBlock).toBeDefined();
+  
+        } catch(e) {
+          console.log(e)
+        }
       }
-
-      const period2 = await Tezos.rpc.getCurrentPeriod();
-      console.log(JSON.stringify(period2));
-
-
-      if (period2.voting_period.kind === 'proposal') {
-        await sleep(period2.remaining * 1000);
-      }
-
-      const op = await Tezos.contract.ballot({
-        proposal: 'ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK',
-        ballot: 'yay'
-      });
-      
-      await op.confirmation();
-
-      expect(op.operationResults).toBeDefined();
-      expect(op.operationResults?.proposal).toEqual('ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK');
-      expect(op.operationResults?.ballot).toEqual('yay');
-      expect(op.includedInBlock).toBeDefined();
 
       done();
     });
