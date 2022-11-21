82143fda31243eaa575ffc2884f4d104e24adc49
huianyang <hui-an.yang@ecadlabs.com>
2022-11-21 14:30:17 -0800

    fix: fixs pr run

--- integration-tests/local-forging.spec.ts
+++ integration-tests/local-forging.spec.ts
@@ -1,5 +1,5 @@
 import { CONFIGS } from "./config";
-import { commonCases, kathmanduCases, limaCases } from './data/allTestsCases';
+import { commonCases, limaCases } from './data/allTestsCases';
 import { LocalForger, ProtocolsHash } from '@taquito/local-forging'
 import { Protocols, TezosToolkit } from "@taquito/taquito";
 
@@ -14,19 +14,6 @@ CONFIGS().forEach(({ rpc, protocol }) => {
         commonCases.forEach(({ name, operation, expected }) => {
 
             it(`Verify that .forge for local forge will return same result as for network forge for rpc: ${name} (${rpc})`, async done => {
-                const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
-
-                const result = await localForger.forge(operation);
-                const rpcResult = await Tezos.rpc.forgeOperations(operation);
-                expect(result).toEqual(rpcResult);
-                expect(await localForger.parse(result)).toEqual(expected || operation);
-
-                done();
-            });
-        });
-
-        kathmanduCases.forEach(({ name, operation, expected }) => {
-            kathmandunet(`Should give the same result as when forging with the rpc: ${name} (${rpc})`, async done => {
                 const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
 
                 const result = await localForger.forge(operation);
