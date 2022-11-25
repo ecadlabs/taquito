d474f2ac7abec2e8faddba47cf403a0c0203c7d5
Zainen <72581075+zainen@users.noreply.github.com>
2022-11-25 12:38:11 -0800

    implement InMemorySigner.fromMnemonic method tz1/2/3 supported (#2116)
    * initial commit 1 working test
    
    add untracked
    
    add case to handle curve when needed
    
    slight refactor
    
    add error cases, errors to throw, error condiditons
    
    add comments
    
    start pr comments
    
    lint, format, pr comments
    
    add typedoc for method and website
    
    initial commit 1 working test
    
    add untracked
    
    add case to handle curve when needed
    
    slight refactor
    
    add error cases, errors to throw, error condiditons
    
    add comments
    
    start pr comments
    
    lint, format, pr comments
    
    start work on tz2
    
    start work with Eugene
    
    fix check for length
    
    work so far attempt
    
    bring over Eugenes work, sucessful tz2 address menmonic to tz2, clean up tz2 and make cleaner with helpers, needed proper typecasting / more tests, todo implement tz3
    
    add support for/ refactor for tz1/2/3 with tests for all plus more derivation path types and errors
    
    slight refactor for conditionals
    
    spacing
    
    fix md doc, add test case for incorrect word used in menmonic
    
    slight error change
    
    pr fixes. refactor todo, bump taquito versions
    
    update tests, correct errors
    
    add method and help descriptions
    
    update package
    
    debug site
    
    fix example error
    
    fix md example
    
    potential drop -> package.lock update
    
    fixes and export
    
    add import exports
    
    trim tests, remove publicKey as its not used
    
    test change website
    
    clean up
    
    pr fixes
    
    pr fixes
    
    fix derivation path usage
    
    * add working test
    
    * refactor params
    
    * ammend test
    
    * remove semicolon
    
    * fix duplication
    
    * add log to debug flex-tesa
    
    * adjust for flextesa
    
    * random mnemonic
    
    * format files
    
    * cont davis comments
    
    * fix test
    
    Co-authored-by: Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>

---
+++ integration-tests/fromMnemonic-send-tez.spec.ts
@@ -0,0 +1,83 @@
+import { OperationContentsAndResultTransaction } from "@taquito/rpc";
+import { InMemorySigner } from "@taquito/signer";
+import { TezosToolkit } from "@taquito/taquito";
+import { CONFIGS } from "./config";
+import * as Bip39 from 'bip39';
+
+CONFIGS().forEach(({ lib, rpc, setup }) => {
+  let Funder: TezosToolkit;
+  describe(`Test fromMnemonic instantiation with rpc: ${rpc}`, () => {
+    let mnemonic: string;
+
+    beforeAll(async (done) => {
+      mnemonic = Bip39.generateMnemonic();
+      Funder = lib;
+      await setup();
+      done();
+    });
+    it('Should create an InMemorySigner (ed25519) with the fromMnemonic method and transfer tez to an account', async (done) => {
+      const Tezos = new TezosToolkit(rpc);
+      // with all default values
+      const signer = InMemorySigner.fromMnemonic({ mnemonic });
+      Tezos.setSignerProvider(signer);
+
+      const funderPKH = await Funder.wallet.pkh();
+      const tezosPKH = await Tezos.wallet.pkh();
+
+      const fundOp = await Funder.wallet.transfer({ to: tezosPKH, amount: 2 }).send();
+      await fundOp.confirmation();
+
+      const returnOp = await Tezos.wallet.transfer({ to: funderPKH, amount: 1 }).send();
+      await returnOp.confirmation();
+
+      const status = await returnOp.status();
+      expect(status).toEqual('applied');
+
+      const balance = await Tezos.tz.getBalance(tezosPKH);
+      expect(Number(balance)).toBeGreaterThan(0);
+      done();
+    });
+    it('Should create an InMemorySigner (secp256k1) with the fromMnemonic method and transfer tez to an account', async (done) => {
+      const Tezos = new TezosToolkit(rpc);
+      // with all default values
+      const signer = InMemorySigner.fromMnemonic({ mnemonic, curve: 'secp256k1' });
+      Tezos.setSignerProvider(signer);
+
+      const funderPKH = await Funder.wallet.pkh();
+      const tezosPKH = await Tezos.wallet.pkh();
+
+      const fundOp = await Funder.wallet.transfer({ to: tezosPKH, amount: 2 }).send();
+      await fundOp.confirmation();
+
+      const returnOp = await Tezos.wallet.transfer({ to: funderPKH, amount: 1, }).send();
+      await returnOp.confirmation();
+
+      const status = await returnOp.status();
+      expect(status).toEqual('applied');
+      const balance = await Tezos.tz.getBalance(tezosPKH);
+      expect(Number(balance)).toBeGreaterThan(0);
+      done();
+    });
+    it('Should create an InMemorySigner (p256) with the fromMnemonic method and transfer tez to an account', async (done) => {
+      const Tezos = new TezosToolkit(rpc);
+      // with all default values
+      const signer = InMemorySigner.fromMnemonic({ mnemonic, curve: 'p256' });
+      Tezos.setSignerProvider(signer);
+
+      const funderPKH = await Funder.wallet.pkh();
+      const tezosPKH = await Tezos.wallet.pkh();
+
+      const fundOp = await Funder.wallet.transfer({ to: tezosPKH, amount: 2 }).send();
+      await fundOp.confirmation();
+
+      const returnOp = await Tezos.wallet.transfer({ to: funderPKH, amount: 1 }).send();
+      await returnOp.confirmation();
+
+      const status = await returnOp.status();
+      expect(status).toEqual('applied');
+      const balance = await Tezos.tz.getBalance(tezosPKH);
+      expect(Number(balance)).toBeGreaterThan(0);
+      done();
+    });
+  });
+});
---
+++ packages/taquito-signer/src/derivation-tools/utils.ts
@@ -0,0 +1,14 @@
+import { InvalidHexStringError } from "@taquito/utils";
+
+export function parseHex(s: string): Uint8Array {
+  const res: number[] = [];
+  for (let i = 0; i < s.length; i += 2) {
+    const ss = s.slice(i, i + 2);
+    const x = parseInt(ss, 16);
+    if (Number.isNaN(x)) {
+      throw new InvalidHexStringError(`invalid hexadecimal number ${ss}`);
+    }
+    res.push(x);
+  }
+  return new Uint8Array(res);
+}
