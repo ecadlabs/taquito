3c18d7e01e164a3dfdfe920059266172edde08d6
roxaneletourneau <roxane@ecadlabs.com>
2022-12-02 13:28:49 -0800

    Try fix test

--- integration-tests/injecting-multiple-manager-operations-in-a-block.spec.ts
+++ integration-tests/injecting-multiple-manager-operations-in-a-block.spec.ts
@@ -12,8 +12,10 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
 
         it('Verify that doing transfers without awaiting the confirmation after each will fail', async (done) => {
             try {
-                const op1 = await Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 1 });
-                const op2 = await Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 2 });
+                const op1Promise = Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 1 });
+                const op2Promise = Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 2 });
+
+                const [op1, op2] = await Promise.all([op1Promise, op2Promise])
 
                 await op1.confirmation();
                 await op2.confirmation();
