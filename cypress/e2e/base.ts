366692d951c533d62794bf12a9825f2520126cda
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-12-05 14:55:46 -0800

    spark joy

--- cypress/e2e/base.ts
+++ cypress/e2e/base.ts
@@ -1,4 +1,5 @@
 //export const base_url = "https://tezostaquito.io/docs/next/"
+// added this to provoke a change for commit
 export const base_url = "https://7d7eaa4d--tezostaquito.netlify.app/docs/next/"
 export const disclaimer = "This is unreleased documentation for Taquito Next version."
 export const runButton = "[class='button_G4Yn runButton_oaHB']"
