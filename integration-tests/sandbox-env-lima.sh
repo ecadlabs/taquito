590f56e14d678bd09d46d9017fd5f67f7f1171c8
huianyang <hui-an.yang@ecadlabs.com>
2022-12-08 10:42:15 -0500

    chore: revert the last change

---
+++ integration-tests/sandbox-env-lima.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export RUN_LIMANET_WITH_SECRET_KEY=true
+export SECRET_KEY=edsk3RFgDiCt7tWB2oe96w1eRw72iYiiqZPLu9nnEY23MYRp2d8Kkx
+export TEZOS_RPC_LIMANET=http://localhost:20001
+export RPC_CACHE_MILLISECONDS=0
+export TEZOS_BAKER=tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb
