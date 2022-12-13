8934c4b8c310e4cdbc700b0bd5199ed5afad4d5f
Daniele Lisi <22307776+danielelisi@users.noreply.github.com>
2022-12-12 17:49:36 -0800

    Use public facing keygen

--- integration-tests/config.ts
+++ integration-tests/config.ts
@@ -91,7 +91,7 @@ const kathmandunetEphemeral = {
   protocol: Protocols.PtKathman,
   signerConfig: {
     type: SignerType.EPHEMERAL_KEY as SignerType.EPHEMERAL_KEY,
-    keyUrl: process.env['TEZOS_KEYGEN_URL'] || 'http://key-gen-1.i.tez.ie:3000/kathmandunet',
+    keyUrl: 'https://api.tez.ie/keys/kathmandunet',
     requestHeaders: { Authorization: 'Bearer taquito-example' },
   }
 };
@@ -112,7 +112,7 @@ const limanetEphemeral = {
   protocol: Protocols.PtLimaPtL,
   signerConfig: {
     type: SignerType.EPHEMERAL_KEY as SignerType.EPHEMERAL_KEY,
-    keyUrl: process.env['TEZOS_KEYGEN_URL'] || 'http://key-gen-1.i.tez.ie:3000/limanet',
+    keyUrl: 'https://api.tez.ie/keys/limanet',
     requestHeaders: { Authorization: 'Bearer taquito-example' },
   },
 };
@@ -134,7 +134,7 @@ const mondaynetEphemeral = {
   protocol: Protocols.ProtoALpha,
   signerConfig: {
     type: SignerType.EPHEMERAL_KEY as SignerType.EPHEMERAL_KEY,
-    keyUrl: process.env['TEZOS_KEYGEN_URL'] || 'http://key-gen-1.i.tez.ie:3010/mondaynet',
+    keyUrl: 'http://key-gen-1.i.tez.ie:3010/mondaynet',
     requestHeaders: { Authorization: 'Bearer taquito-example' },
   },
 };
