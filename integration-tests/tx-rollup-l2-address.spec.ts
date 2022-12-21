ee1d0a839708f6f8b179b6117cc70dcdf9ddde05
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2022-12-21 11:58:05 -0800

    remove kath l2 contract test

--- integration-tests/tx-rollup-l2-address.spec.ts
+++ integration-tests/tx-rollup-l2-address.spec.ts
@@ -33,66 +33,6 @@ CONFIGS().forEach(({ lib, setup, protocol, txRollupAddress, rpc }) => {
             UNIT;
             PAIR;
             TICKET;
-            PAIR;
-
-            # amount for transfering
-            PUSH mutez 0;
-            SWAP;
-
-            # deposit
-            TRANSFER_TOKENS;
-
-            DIP { NIL operation };
-            CONS;
-
-            DIP { PUSH unit Unit };
-            PAIR;
-          }
-          { FAIL ; }
-        }
-        `,
-        storage: "Unit"
-      });
-      await op.confirmation();
-      expect(op.hash).toBeDefined();
-      expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
-
-      const contract = await op.contract();
-
-      const storage = await contract.storage();
-      expect(JSON.stringify(storage)).toEqual(JSON.stringify(UnitValue));
-
-      const ticketDeposit = await contract.methods.default(txRollupAddress, 'tz4VHgLiRx5ZZjwU2QaybHc11EMJk3NcyvVc', '1').send();
-      await ticketDeposit.confirmation();
-
-
-      expect(ticketDeposit.hash).toBeDefined();
-      expect(ticketDeposit.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
-      expect(JSON.stringify(await contract.storage())).toEqual(JSON.stringify(UnitValue));
-      done();
-    });
-
-    Limanet(`Originate a contract with a hex string type tz1 & tz4 in initial storage tz4 & string`, async (done) => {
-      const op = await tezos.contract.originate({
-        code: `
-        parameter (pair address tx_rollup_l2_address string);
-        storage (unit);
-        code {
-          # cast the address to contract type
-          CAR;
-          UNPAIR;
-          CONTRACT %deposit (pair (ticket (pair unit string)) tx_rollup_l2_address);
-
-          IF_SOME {
-            SWAP;
-            UNPAIR; SWAP;
-
-            # create a ticket
-            PUSH nat 10;
-            SWAP;
-            UNIT;
-            PAIR;
-            TICKET;
             ASSERT_SOME;
             PAIR;
 
