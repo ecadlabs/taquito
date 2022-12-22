543362f0afb948157516630065fe50f5048dc8a2
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-12-22 15:02:10 -0800

    missed a conflict

--- integration-tests/known-contracts-PtLimaPtL.ts
+++ integration-tests/known-contracts-PtLimaPtL.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-export const knownContractPtLimaPtL = "KT1EkS7gNaWqPTt4y4QVqJurwMDPFK2g8boy";
-export const knownBigMapContractPtLimaPtL = "KT1TjZW4ujuCNJfsvxdqS333iE6MNTFA6pqm";
-export const knownTzip12BigMapOffChainContractPtLimaPtL = "KT1CpUjwSRt3C7ecyF9f3xvBY4f8G7JFmKjM";
-export const knownSaplingContractPtLimaPtL = "KT1Hv3brnhecSXNhovC5GvxZgmHY8hAd6D2m";
-export const knownOnChainViewContractAddressPtLimaPtL = "KT1Kmzc6zb44AsNs897aGDoU7Qj1TeorY6vr";
-export const txRollupAddressPtLimaPtL = "txr1Xz6LiZSspZ9bJu2SC5r97B1ikCBAAYKdQ";
-=======
 export const knownContractPtLimaPtL = "KT1Q8j4ahiuMu6cTtD42JmUFNtdc6aE1ctUk";
 export const knownBigMapContractPtLimaPtL = "KT1STPt3btDjttzsZ7MBmQkkfU3FykeAgYnW";
 export const knownTzip12BigMapOffChainContractPtLimaPtL = "KT1Kh3xkT7dvn4ULqhwgxq1p4yz2kGx58o5E";
 export const knownSaplingContractPtLimaPtL = "KT1B1WJhcBYY1ZuWZmgbp2CsPFHEffNZVrEj";
 export const knownOnChainViewContractAddressPtLimaPtL = "KT1MU2tLa61FVUTa1HzVS6FZjVPBZQBhZabM";
 export const txRollupAddressPtLimaPtL = "txr1SNAYyB37mfJwqByczbbRbZj41yYzfDB9F";
->>>>>>> d38f451bb1cb141a6ce7dd668de211130d248fd7
