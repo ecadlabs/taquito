bce4d07061db4b580801e31d8410ce3158272196
Zainen <72581075+zainen@users.noreply.github.com>
2022-12-28 17:18:09 -0800

    one constant change (#2252)
    Co-authored-by: Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>

--- integration-tests/known-contracts-ProtoALph.ts
+++ integration-tests/known-contracts-ProtoALph.ts
@@ -1,6 +1,6 @@
-export const knownContractProtoALph = "KT1P5C4CUrMVeYY9CpH7kiLXQxodooAwqqan";
-export const knownBigMapContractProtoALph = "KT1ToqxMfMjmpjw8LLmF3vZwRBAVaz9QNdmA";
-export const knownTzip12BigMapOffChainContractProtoALph = "KT1AARAA1KeZ7J5GeDo47cg6MkJs3wYUxTUU";
-export const knownSaplingContractProtoALph = "KT1WjMnSStshfwFb5EpkNHC7jD98UmNzpvEc";
-export const knownOnChainViewContractAddressProtoALph = "KT1TNwhNYohAxk93F9yd8bLe8TfBm4xAczv7";
+export const knownContractProtoALph = "KT1WheWMSSsB7MCcNxHKjKgmqUYdoiqcFKQY";
+export const knownBigMapContractProtoALph = "KT1Gnuoa3nTK6gg4YF98eQngjndR1B7XH1Hq";
+export const knownTzip12BigMapOffChainContractProtoALph = "KT1Qqe8p7K9LEU5E12A5RnnyPMvpn4RyUQ2z";
+export const knownSaplingContractProtoALph = "KT1VtjTtDwyyDsAEA91vypDeuvq4Jdn2xXpP";
+export const knownOnChainViewContractAddressProtoALph = "KT1S57UG18gtobgNmiKRh6kHEcRe8VTyNmHV";
 export const txRollupAddressProtoALph = "";
