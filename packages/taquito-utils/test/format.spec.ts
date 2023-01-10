214eb655d172d0fe514fd21409c4ccff20f8e04c
Zainen <72581075+zainen@users.noreply.github.com>
2023-01-09 16:41:12 -0800

    fix bytes in signing data example (#2263)
    * fix bytes in signing data example
    
    * fix and add test
    
    * clarify
    
    * clarify
    
    * correct
    
    * fix bytes padding
    
    * fix padding
    
    * correct padded slice length
    
    * change bytes added
    
    * fix example and test
    
    * remove unneeded
    
    * test remove unneeded
    
    * fix test
    
    * save file
    
    * clarify test name
    
    * add describe block
    
    Co-authored-by: Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>

--- packages/taquito-utils/test/format.spec.ts
+++ packages/taquito-utils/test/format.spec.ts
@@ -1,5 +1,6 @@
 import { format } from '../src/format';
 import BigNumber from 'bignumber.js';
+import { bytes2Char, char2Bytes } from '../src/taquito-utils';
 
 describe('Format', () => {
   it('Should convert mutez to tz', () => {
@@ -20,5 +21,28 @@ describe('Format', () => {
 
   it('Should convert tz to mutez', () => {
     expect(format('tz', 'mutez', 1)).toEqual(new BigNumber(1000000));
+  });
+
+});
+
+describe('tezostaquito.io example signing formatting', () => {
+  it('Should be valid bytes tezostaquito example https://tezostaquito.io/docs/next/signing#generating-a-signature-with-beacon-sdk with proper padding for bytes length', () => {
+    const formattedInput: string = [
+      'Tezos Signed Message:',
+      'some url',
+      '18:43:34Z',
+      'something',
+    ].join(' ');
+
+    const bytes = char2Bytes(formattedInput);
+    const bytesLength = (bytes.length / 2).toString(16);
+    const addPadding = `00000000${bytesLength}`;
+    const paddedBytesLength = addPadding.slice(addPadding.length - 8);
+    const payloadBytes = '05' + '01' + paddedBytesLength + bytes;
+    const regex = new RegExp('^(0x|0X)?[a-fA-F0-9]+$');
+    const check = regex.test(payloadBytes);
+    expect(check).toEqual(true);
+    expect(payloadBytes.length % 2).toEqual(0);
+    expect(bytes2Char(payloadBytes)).toEqual(bytes2Char('05' + '01' + paddedBytesLength) + formattedInput);
   });
 });
