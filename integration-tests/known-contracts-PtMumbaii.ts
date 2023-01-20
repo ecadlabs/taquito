e1f278fd3265aa36edf701c9b7d3fa1734ee3dc9
Davis Sawali <davis.sawali@ecadlabs.com>
2023-01-19 16:58:06 -0800

    update Taquito test configs to support Mumbai runs

---
+++ integration-tests/known-contracts-PtMumbaii.ts
@@ -0,0 +1,6 @@
+export const knownContractPtMumbaii = "KT1Pt75B5Sgf5nz4m7GX1yGCA36pR2Lf6Eit";
+export const knownBigMapContractPtMumbaii = "KT1ToCtqCSAqK85sYmD3f1AUC1h17Ho9J8J4";
+export const knownTzip12BigMapOffChainContractPtMumbaii = "KT1AJwEfLhur9snYyv86GAXdoQZqygED3X3b";
+export const knownSaplingContractPtMumbaii = "KT1MU6HasZvDrnDk7TdNVGj9zFVfDDGGrkFD";
+export const knownOnChainViewContractAddressPtMumbaii = "KT1Mon6pBMP2M48tRSCAuSTgccsnwwG7PnHw";
+export const txRollupAddressPtMumbaii = "txr1YzCiyQKjiNykr3cHoD9AVJqn9wtq81Cuc";
