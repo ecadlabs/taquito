8de16b94dc30b223c17a25407a9d6d7ea0d42691
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2023-01-23 12:18:04 -0800

    chore: modify paths for imports

--- integration-tests/data/instructions-with-bytes-contracts.ts
+++ integration-tests/data/instructions-with-bytes-contracts.ts
@@ -1,9 +1,9 @@
-import fs from 'fs'
+import fs from 'fs';
+import path from 'path';
 
-
-export const addContract =  fs.readFileSync(__dirname + '/../../packages/taquito-michel-codec/test/contracts_016/opcodes/and_bytes.tz').toString();
-export const lslContract =  fs.readFileSync(__dirname + '/../../packages/taquito-michel-codec/test/contracts_016/opcodes/lsl_bytes.tz').toString();
-export const lsrContract =  fs.readFileSync(__dirname + '/../../packages/taquito-michel-codec/test/contracts_016/opcodes/lsr_bytes.tz').toString();
-export const notContract =  fs.readFileSync(__dirname + '/../../packages/taquito-michel-codec/test/contracts_016/opcodes/not_bytes.tz').toString();
-export const orContract =  fs.readFileSync(__dirname + '/../../packages/taquito-michel-codec/test/contracts_016/opcodes/or_bytes.tz').toString();
-export const xorContract =  fs.readFileSync(__dirname + '/../../packages/taquito-michel-codec/test/contracts_016/opcodes/xor_bytes.tz').toString();
+export const addContract =  fs.readFileSync(path.resolve(`${__dirname}/../../packages/taquito-michel-codec/test/contracts_016/opcodes/and_bytes.tz`)).toString();
+export const lslContract =  fs.readFileSync(path.resolve(`${__dirname}/../../packages/taquito-michel-codec/test/contracts_016/opcodes/lsl_bytes.tz`)).toString();
+export const lsrContract =  fs.readFileSync(path.resolve(`${__dirname}/../../packages/taquito-michel-codec/test/contracts_016/opcodes/lsr_bytes.tz`)).toString();
+export const notContract =  fs.readFileSync(path.resolve(`${__dirname}/../../packages/taquito-michel-codec/test/contracts_016/opcodes/not_bytes.tz`)).toString();
+export const orContract =  fs.readFileSync(path.resolve(`${__dirname}/../../packages/taquito-michel-codec/test/contracts_016/opcodes/or_bytes.tz`)).toString();
+export const xorContract =  fs.readFileSync(path.resolve(`${__dirname}/../../packages/taquito-michel-codec/test/contracts_016/opcodes/xor_bytes.tz`)).toString();
