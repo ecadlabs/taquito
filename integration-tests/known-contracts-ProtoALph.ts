291ddede4e158d71ac26ad05fa6c9d6c5b03ce2d
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2023-01-23 11:49:33 -0800

    chore: update mondaynet contracts

--- integration-tests/known-contracts-ProtoALph.ts
+++ integration-tests/known-contracts-ProtoALph.ts
@@ -1,6 +1,6 @@
-export const knownContractProtoALph = "KT1M3V5uDENHahkwgboVgXdYW5RJ2DS4W1Wg";
-export const knownBigMapContractProtoALph = "KT1UR4NPP7QWGoD8McpVLW8xbK64BJXRnsih";
-export const knownTzip12BigMapOffChainContractProtoALph = "KT1KQCNdi8xWSscrGmRCgbFdQWwaun7bnivs";
-export const knownSaplingContractProtoALph = "KT1A2m13khuHMneRU4Ep3UjAxy2Pt6x9CC3u";
-export const knownOnChainViewContractAddressProtoALph = "KT19BzzXMnBCpB5NriqDSLvSyq3EosDTTeKo";
+export const knownContractProtoALph = "KT1EPtdjWCyggtzEBUrnvmLpeSTGFnqnRJd2";
+export const knownBigMapContractProtoALph = "KT1NVaHgn4SAcTZoUd8FhxJ2n3c4wRheHqiR";
+export const knownTzip12BigMapOffChainContractProtoALph = "KT1ErUELv5vFfJ6mQm4Q3kM3ngj6k8UfjCRQ";
+export const knownSaplingContractProtoALph = "KT1V3Ukd6VkCjnRfyvzZcprcCS8NDE6RA2WC";
+export const knownOnChainViewContractAddressProtoALph = "KT1NcfXnrDehg9L8Cc52qm6NjdS4z6rKHhz4";
 export const txRollupAddressProtoALph = "";
