2921403ea0b71565797fd7194a0b9a6e4e9311e5
huianyang <hui-an.yang@ecadlabs.com>
2023-01-31 08:24:16 -0800

    docs: updated docs for getTicketBalance rpc endpoint
    #2270

--- packages/taquito-rpc/src/taquito-rpc.ts
+++ packages/taquito-rpc/src/taquito-rpc.ts
@@ -1132,10 +1132,10 @@ export class RpcClient implements RpcClientInterface {
   /**
    *
    * @param contract address of the contract we want to retrieve ticket balance of
+   * @param ticket object to specify ticketer, content type and content
    * @param options contains generic configuration for rpc calls
-   *
    * @description Access the contract's balance of ticket with specified ticketer, content type, and content.
-   *
+   * @example ticket{ ticketer: 'address', content_type: { prim: "string" }, content: { string: 'ticket1' } }
    * @see https://tezos.gitlab.io/protocols/016_mumbai.html#rpc-changes
    */
   async getTicketBalance(
@@ -1158,9 +1158,7 @@ export class RpcClient implements RpcClientInterface {
    *
    * @param contract address of the contract we want to retrieve ticket balance of
    * @param options contains generic configuration for rpc calls
-   *
    * @description Access the complete list of tickets owned by the given contract by scanning the contract's storage.
-   *
    * @see https://tezos.gitlab.io/protocols/016_mumbai.html#rpc-changes
    */
   async getAllTicketBalances(
