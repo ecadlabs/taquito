2f4869802fabc91042cc070d9a3402833bd1eba9
Zainen <72581075+zainen@users.noreply.github.com>
2023-02-15 15:49:25 -0800

    feat(local-forger): 2308 support smart rollup originate local forging (#2339)
    feat: start implementation with tests smart rollup add messages need refactor
    
    feat: add forging for execute outbox message with mock bytes change bytes name
    
    chore: more expects for execute outbox message test
    
    refactor: change param need add messages
    
    fix: fix missed decoders and remove unneeded
    
    refactor: integration tests  sr originate, add message. fix decoder and needs more testing
    
    fix: properly parse array from add messages message
    
    needs cleanup and refactor
    
    refactor: change name
    
    feat: support forging execute_outbox_message
    
    fix: dont test sr against less than mumbai
    
    fix: fix broken test from proper implementation
    
    fix: bad file structure for tests
    
    refactor: update naming
    
    refactor: add better names and refactor unneeded
    
    format alltest cases
    
    refactor: format and missed name change
    
    refactor: update type doc fix test name
    
    Co-authored-by: Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>

--- integration-tests/local-forging.spec.ts
+++ integration-tests/local-forging.spec.ts
@@ -1,18 +1,17 @@
 import { CONFIGS } from "./config";
-import { commonCases } from './data/allTestsCases';
+import { commonCases, mumbaiCases } from './data/allTestsCases';
 import { LocalForger, ProtocolsHash } from '@taquito/local-forging'
-import { TezosToolkit } from "@taquito/taquito";
+import { Protocols, TezosToolkit } from "@taquito/taquito";
 
 CONFIGS().forEach(({ rpc, protocol }) => {
   const Tezos = new TezosToolkit(rpc);
 
   describe(`Test local forger: ${rpc}`, () => {
-
+    const mumbaiAndAlpha = protocol === Protocols.ProtoALpha || protocol == Protocols.PtMumbaii ? it : it.skip;
     // all protocols
     commonCases.forEach(({ name, operation, expected }) => {
       it(`Verify that .forge for local forge will return same result as for network forge for rpc: ${name} (${rpc})`, async done => {
         const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
-
         const result = await localForger.forge(operation);
         const rpcResult = await Tezos.rpc.forgeOperations(operation);
         expect(result).toEqual(rpcResult);
@@ -21,5 +20,17 @@ CONFIGS().forEach(({ rpc, protocol }) => {
         done();
       });
     });
+
+    mumbaiCases.forEach(({ name, operation, expected}) => {
+      mumbaiAndAlpha(`Verify that .forge for local forge smart rollup will return same result as for network forge for rpc: ${name} (${rpc})`, async done => {
+        const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
+        const result = await localForger.forge(operation);
+        const rpcResult = await Tezos.rpc.forgeOperations(operation);
+        expect(result).toEqual(rpcResult);
+        expect(await localForger.parse(result)).toEqual(expected || operation);
+
+        done();
+      });
+    })
   });
 });
