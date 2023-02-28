9e7b9016f374f7c409ff2d7972f3955bf138e302
Davis Sawali <davis.sawali@ecadlabs.com>
2023-02-28 11:11:40 -0800

    add mumbai condition to run test

--- integration-tests/contract-simple-transaction.spec.ts
+++ integration-tests/contract-simple-transaction.spec.ts
@@ -1,16 +1,17 @@
 import { CONFIGS } from './config';
+import { Protocols } from '@taquito/taquito';
 
-CONFIGS().forEach(({ lib, rpc, setup }) => {
+CONFIGS().forEach(({ lib, rpc, setup, protocol }) => {
   const Tezos = lib;
+  const mumbaiAndAlpha = protocol === Protocols.PtMumbaii || protocol === Protocols.ProtoALpha ? test : test.skip;
 
   describe(`Test simple transaction to tezos public key hashes: ${rpc}`, () => {
-
     beforeEach(async (done) => {
       await setup(true);
       done();
     });
 
-    test('should be able to send to a tz4 address', async (done) => {
+    mumbaiAndAlpha('should be able to send to a tz4 address', async (done) => {
       const op = await Tezos.contract.transfer({
         amount: 1,
         to: 'tz4HQ8VeXAyrZMhES1qLMJAc9uAVXjbMpS8u'
