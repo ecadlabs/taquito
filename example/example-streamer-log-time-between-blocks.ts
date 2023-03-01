adff9e779e4bf1499c8a3edaa7ac63bc84c0aab7
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2023-02-28 16:13:02 -0800

    perf: update some examples for ghostnet
    remove file change that will conflict

--- example/example-streamer-log-time-between-blocks.ts
+++ example/example-streamer-log-time-between-blocks.ts
@@ -3,14 +3,14 @@ import { PollingSubscribeProvider, TezosToolkit } from '@taquito/taquito';
 // To run this script, run the following command in the example folder: `npm run example:streamer-block-time`
 // This script polls on the head block at an interval of `pollingIntervalMilliseconds`.
 // When a new block is seen, the time between the new block and the precedent is logged with the block level
-// If a block is missed when polling, 'Missed block: `level`' is logged. 
+// If a block is missed when polling, 'Missed block: `level`' is logged.
 
 const provider = 'https://ghostnet.ecadinfra.com';
 const pollingIntervalMilliseconds = 5000;
 let date: Date;
 let level: number;
 
-function logLevelAndTime(data: any) {
+function logLevelAndTime(data: number) {
   const newDate = new Date();
   const timeBetweenBlocks = date ? (newDate.getTime() - date.getTime()) : 0;
   if (level && data - level > 1) {
