74023c42bd73d43fc810a0e4c0a228087e82a132
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2023-05-03 13:44:23 -0700

    Formatted the fetch adapter

--- packages/taquito-http-utils/src/fetch-adapter.ts
+++ packages/taquito-http-utils/src/fetch-adapter.ts
@@ -14,33 +14,33 @@ const { isUndefined, isStandardBrowserEnv, isFormData } = require('axios/lib/uti
  * - Check if timeout
  */
 export default async function fetchAdapter(config: AxiosRequestConfig): Promise<AxiosResponse> {
-    const request = createRequest(config);
-    const promiseChain = [getResponse(request, config)];
+  const request = createRequest(config);
+  const promiseChain = [getResponse(request, config)];
 
-    if (config.timeout && config.timeout > 0) {
-        promiseChain.push(
-            new Promise((res) => {
-                setTimeout(() => {
-                    const message = config.timeoutErrorMessage
-                        ? config.timeoutErrorMessage
-                        : 'timeout of ' + config.timeout + 'ms exceeded';
-                    res(createError(message, config, 'ECONNABORTED', request));
-                }, config.timeout);
-            })
-        );
+  if (config.timeout && config.timeout > 0) {
+    promiseChain.push(
+      new Promise((res) => {
+        setTimeout(() => {
+          const message = config.timeoutErrorMessage
+            ? config.timeoutErrorMessage
+            : 'timeout of ' + config.timeout + 'ms exceeded';
+          res(createError(message, config, 'ECONNABORTED', request));
+        }, config.timeout);
+      })
+    );
+  }
+
+  const data = await Promise.race(promiseChain);
+  return new Promise((resolve, reject) => {
+    if (data instanceof Error) {
+      reject(data);
+    } else {
+      const c: any = config;
+      'settle' in c && Object.prototype.toString.call(c.settle) === '[object Function]'
+        ? c.settle(resolve, reject, data)
+        : settle(resolve, reject, data);
     }
-
-    const data = await Promise.race(promiseChain);
-    return new Promise((resolve, reject) => {
-        if (data instanceof Error) {
-            reject(data);
-        } else {
-            const c: any = config;
-            ('settle' in c) && Object.prototype.toString.call(c.settle) === '[object Function]'
-                ? c.settle(resolve, reject, data)
-                : settle(resolve, reject, data);
-        }
-    });
+  });
 }
 
 /**
@@ -48,131 +48,131 @@ export default async function fetchAdapter(config: AxiosRequestConfig): Promise<
  * response body based on response's type
  */
 async function getResponse(request: RequestInfo | URL, config: AxiosRequestConfig) {
-    try {
-        const stageOne = await fetch(request as any);
-        
-        let response: {
-            ok: boolean;
-            status: number;
-            statusText: string;
-            headers: Headers;
-            config: AxiosRequestConfig;
-            request: RequestInfo | URL;
-            data?: unknown;
-        } = {
-            ok: stageOne.ok,
-            status: stageOne.status,
-            statusText: stageOne.statusText,
-            headers: new Headers(stageOne.headers), // Make a copy of headers
-            config: config,
-            request,
-          }
-        if (stageOne.status >= 400) {
-          return createError('Response Error', config, 'ERR_NETWORK', request, response);
-        }
+  try {
+    const stageOne = await fetch(request as any);
 
-        response ={
-            ok: stageOne.ok,
-            status: stageOne.status,
-            statusText: stageOne.statusText,
-            headers: new Headers(stageOne.headers), // Make a copy of headers
-            config: config,
-            request,
-        };
-    
-        if (stageOne.status >= 200 && stageOne.status !== 204) {
-            switch (config.responseType) {
-                case 'arraybuffer':
-                    response.data = await stageOne.arrayBuffer();
-                    break;
-                case 'blob':
-                    response.data = await stageOne.blob();
-                    break;
-                case 'json':
-                    response.data = await stageOne.json();
-                    break;
-                // TODO: the next option does not exist in response type
-                // case 'formData':
-                //     response.data = await stageOne.formData();
-                //     break;
-                default:
-                    response.data = await stageOne.text();
-                    break;
-            }
-        }
-    
-        return response;
-    } catch (e) {
-        return createError('Network Error', config, 'ERR_NETWORK', request);
+    let response: {
+      ok: boolean;
+      status: number;
+      statusText: string;
+      headers: Headers;
+      config: AxiosRequestConfig;
+      request: RequestInfo | URL;
+      data?: unknown;
+    } = {
+      ok: stageOne.ok,
+      status: stageOne.status,
+      statusText: stageOne.statusText,
+      headers: new Headers(stageOne.headers), // Make a copy of headers
+      config: config,
+      request,
+    };
+    if (stageOne.status >= 400) {
+      return createError('Response Error', config, 'ERR_NETWORK', request, response);
     }
+
+    response = {
+      ok: stageOne.ok,
+      status: stageOne.status,
+      statusText: stageOne.statusText,
+      headers: new Headers(stageOne.headers), // Make a copy of headers
+      config: config,
+      request,
+    };
+
+    if (stageOne.status >= 200 && stageOne.status !== 204) {
+      switch (config.responseType) {
+        case 'arraybuffer':
+          response.data = await stageOne.arrayBuffer();
+          break;
+        case 'blob':
+          response.data = await stageOne.blob();
+          break;
+        case 'json':
+          response.data = await stageOne.json();
+          break;
+        // TODO: the next option does not exist in response type
+        // case 'formData':
+        //     response.data = await stageOne.formData();
+        //     break;
+        default:
+          response.data = await stageOne.text();
+          break;
+      }
+    }
+
+    return response;
+  } catch (e) {
+    return createError('Network Error', config, 'ERR_NETWORK', request);
+  }
 }
 
 /**
  * This function will create a Request object based on configuration's axios
  */
 function createRequest(config: AxiosRequestConfig) {
-    const headers = new Headers(config.headers as any);
+  const headers = new Headers(config.headers as any);
 
-    // HTTP basic authentication
-    if (config.auth) {
-        const username = config.auth.username || '';
-        const password = config.auth.password ? decodeURI(encodeURIComponent(config.auth.password)) : '';
-        headers.set('Authorization', `Basic ${btoa(username + ':' + password)}`);
-    }
+  // HTTP basic authentication
+  if (config.auth) {
+    const username = config.auth.username || '';
+    const password = config.auth.password
+      ? decodeURI(encodeURIComponent(config.auth.password))
+      : '';
+    headers.set('Authorization', `Basic ${btoa(username + ':' + password)}`);
+  }
 
-    const method = config.method?.toUpperCase();
-    const options: RequestInit = {
-        headers: headers,
-        method,
-    };
-    if (method !== 'GET' && method !== 'HEAD') {
-        options.body = config.data;
+  const method = config.method?.toUpperCase();
+  const options: RequestInit = {
+    headers: headers,
+    method,
+  };
+  if (method !== 'GET' && method !== 'HEAD') {
+    options.body = config.data;
 
-        // In these cases the browser will automatically set the correct Content-Type,
-        // but only if that header hasn't been set yet. So that's why we're deleting it.
-        if (isFormData(options.body) && isStandardBrowserEnv()) {
-            headers.delete('Content-Type');
-        }
+    // In these cases the browser will automatically set the correct Content-Type,
+    // but only if that header hasn't been set yet. So that's why we're deleting it.
+    if (isFormData(options.body) && isStandardBrowserEnv()) {
+      headers.delete('Content-Type');
     }
-    const c = config as any;
-    if ('mode' in c) {
-        options.mode = c.mode as RequestMode;
-    }
-    if ('cache' in c) {
-        options.cache = c.cache as RequestCache;
-    }
-    if ('integrity' in c) {
-        options.integrity = c.integrity as string;
-    }
-    if ('redirect' in c) {
-        options.redirect = c.redirect as RequestRedirect;
-    }
-    if ('referrer' in c) {
-        options.referrer = c.referrer as string;
-    }
-    // This config is similar to XHR’s withCredentials flag, but with three available values instead of two.
-    // So if withCredentials is not set, default value 'same-origin' will be used
-    if (!isUndefined(c.withCredentials)) {
-        options.credentials = c.withCredentials ? 'include' : 'omit';
-    }
+  }
+  const c = config as any;
+  if ('mode' in c) {
+    options.mode = c.mode as RequestMode;
+  }
+  if ('cache' in c) {
+    options.cache = c.cache as RequestCache;
+  }
+  if ('integrity' in c) {
+    options.integrity = c.integrity as string;
+  }
+  if ('redirect' in c) {
+    options.redirect = c.redirect as RequestRedirect;
+  }
+  if ('referrer' in c) {
+    options.referrer = c.referrer as string;
+  }
+  // This config is similar to XHR’s withCredentials flag, but with three available values instead of two.
+  // So if withCredentials is not set, default value 'same-origin' will be used
+  if (!isUndefined(c.withCredentials)) {
+    options.credentials = c.withCredentials ? 'include' : 'omit';
+  }
 
-    const fullPath = buildFullPath(c.baseURL, c.url);
-    const url = buildURL(fullPath, c.params, c.paramsSerializer);
+  const fullPath = buildFullPath(c.baseURL, c.url);
+  const url = buildURL(fullPath, c.params, c.paramsSerializer);
 
-    // Expected browser to throw error if there is any wrong configuration value
-    return new Request(url, options);
+  // Expected browser to throw error if there is any wrong configuration value
+  return new Request(url, options);
 }
-
-
 
 /**
  * Note:
- * 
+ *
  *   From version >= 0.27.0, createError function is replaced by AxiosError class.
  *   So I copy the old createError function here for backward compatible.
- * 
- * 
- * 
+ *
+ *
+ *
  * Create an Error with the specified message, config, error code, request and response.
  *
  * @param {string} message The error message.
@@ -182,23 +182,29 @@ function createRequest(config: AxiosRequestConfig) {
  * @param {Object} [response] The response.
  * @returns {Error} The created error.
  */
-function createError(message: string, config: AxiosRequestConfig, code?: string, request?: RequestInfo | URL, response?: object) {
-    // TODO: this code never runs
-    // if ('AxiosError' in axios && axios.AxiosError && typeof axios.AxiosError === 'function' && isConstructor(axios.AxiosError)) {
-    //     return new axios.AxiosError(message, axios.AxiosError[code], config, request, response);
-    // }
+function createError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string,
+  request?: RequestInfo | URL,
+  response?: object
+) {
+  // TODO: this code never runs
+  // if ('AxiosError' in axios && axios.AxiosError && typeof axios.AxiosError === 'function' && isConstructor(axios.AxiosError)) {
+  //     return new axios.AxiosError(message, axios.AxiosError[code], config, request, response);
+  // }
 
-    const error = new Error(message);
-    return enhanceError(error, config, code, request, response);
+  const error = new Error(message);
+  return enhanceError(error, config, code, request, response);
 }
 
 /**
- * 
+ *
  * Note:
- * 
+ *
  *   This function is for backward compatible.
- * 
- *  
+ *
+ *
  * Update an Error with the specified config, error code, and response.
  *
  * @param {Error} error The error to update.
@@ -208,7 +214,13 @@ function createError(message: string, config: AxiosRequestConfig, code?: string,
  * @param {Object} [response] The response.
  * @returns {Error} The error.
  */
-function enhanceError(error: any, config: AxiosRequestConfig, code?: string, request?: RequestInfo | URL, response?: object) {
+function enhanceError(
+  error: any,
+  config: AxiosRequestConfig,
+  code?: string,
+  request?: RequestInfo | URL,
+  response?: object
+) {
   error.config = config;
   if (code) {
     error.code = code;
@@ -225,16 +237,16 @@ function enhanceError(error: any, config: AxiosRequestConfig, code?: string, req
       name: this.name,
       // Microsoft
       description: 'description' in this ? this.description : undefined,
-      number: 'number' in this ?this.number : undefined,
+      number: 'number' in this ? this.number : undefined,
       // Mozilla
-      fileName: 'fileName' in this ?this.fileName : undefined,
-      lineNumber: 'lineNumber' in this ?this.lineNumber : undefined,
-      columnNumber: 'columnNumber' in this ?this.columnNumber : undefined,
+      fileName: 'fileName' in this ? this.fileName : undefined,
+      lineNumber: 'lineNumber' in this ? this.lineNumber : undefined,
+      columnNumber: 'columnNumber' in this ? this.columnNumber : undefined,
       stack: this.stack,
       // Axios
       config: this.config,
       code: this.code,
-      status: this.response && this.response.status ? this.response.status : null
+      status: this.response && this.response.status ? this.response.status : null,
     };
   };
   return error;
