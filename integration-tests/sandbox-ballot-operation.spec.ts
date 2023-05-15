b86e3a85a4aadbad6db20e10db8dd4088bbaba62
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2023-05-15 14:28:20 -0700

    added repaired test

--- integration-tests/sandbox-ballot-operation.spec.ts
+++ integration-tests/sandbox-ballot-operation.spec.ts
@@ -4,36 +4,34 @@ CONFIGS().forEach(async ({ lib, protocol, setup }) => {
   const Tezos = lib;
 
   describe(`Ballot operation test (${protocol})`, () => {
-    beforeAll(async (done) => {
+    beforeAll(async () => {
       await setup();
-
-      try {
-        const period = await Tezos.rpc.getCurrentPeriod();
-        await sleep((period.remaining + 5)  * 1000);
-
-        const proposalsOp = await Tezos.contract.proposals({
-          proposals: ['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']
-        });
-
-        await proposalsOp.confirmation();
-      } catch(e) {
-        console.log(e);
-      }
-      done();
     });
 
-    // TODO: Fix timing issues in regards to different block times in different sandboxes
-    it.skip('Submit a proposal and inject ballot vote', async (done) => {
+    it('Submit a proposal and inject ballot vote', async (done) => {
       const period = await Tezos.rpc.getCurrentPeriod();
 
       if (period.voting_period.kind === 'proposal') {
-        await sleep((period.remaining + 5) * 1000);
+        const proposalsOp = await Tezos.contract.proposals({
+          proposals: ['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']
+        });
+        await proposalsOp.confirmation();
       }
-      
+
+      let check_period;
+      for (let i = 0; i < 4; i++) {
+        check_period = await Tezos.rpc.getCurrentPeriod();
+        await sleep(10000);
+      }
+
+      const confirm_period = await Tezos.rpc.getCurrentPeriod();
+      expect(confirm_period.voting_period.kind).toBe('exploration');
+
       const op = await Tezos.contract.ballot({
         proposal: 'ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK',
         ballot: 'yay'
       });
+
       await op.confirmation();
 
       expect(op.operationResults).toBeDefined();
@@ -41,8 +39,8 @@ CONFIGS().forEach(async ({ lib, protocol, setup }) => {
       expect(op.operationResults?.ballot).toEqual('yay');
       expect(op.includedInBlock).toBeDefined();
       expect(op.hash).toBeDefined();
-  
+
       done();
     });
-  })
-})
+  });
+});
