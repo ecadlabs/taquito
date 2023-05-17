e746062bc59700102f3a714ce7d31b9c7c811ee4
Davis Sawali <davis.sawali@ecadlabs.com>
2023-05-17 13:34:53 -0700

    updated assertion to expect an error being thrown

--- integration-tests/injecting-multiple-manager-operations-in-a-block.spec.ts
+++ integration-tests/injecting-multiple-manager-operations-in-a-block.spec.ts
@@ -10,15 +10,14 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
     });
 
     it('Verify that doing transfers without awaiting the confirmation after each will fail', async (done) => {
-      try {
+      // Not checking for error message as it is not deterministic
+      expect( async () => {
         const op1 = await Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 1 });
         const op2 = await Tezos.contract.transfer({ to: 'tz1ZfrERcALBwmAqwonRXYVQBDT9BjNjBHJu', amount: 2 });
 
         await op1.confirmation();
         await op2.confirmation();
-      } catch (error: any) {
-        expect(error.message).toContain('mempool already contains a conflicting operation');
-      }
+      }).toThrowError();
       done();
     });
   });
