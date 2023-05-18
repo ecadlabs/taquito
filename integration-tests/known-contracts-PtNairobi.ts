141421fc7401f1cc757ef177c7ba78d64c757d85
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2023-05-18 14:49:58 -0700

    Configure Nairobinet

---
+++ integration-tests/known-contracts-PtNairobi.ts
@@ -0,0 +1,5 @@
+export const knownContractPtNairobi = "KT1XFiUYC36XSeLTanGJwZxqLzsxz9zquLFB";
+export const knownBigMapContractPtNairobi = "KT1KbbvszHoWVSS8Nzh9yLgvRBDkzVjKmCtj";
+export const knownTzip12BigMapOffChainContractPtNairobi = "KT1KKU19PxFbQUT9sBJS8KwYCVaXAzYsTkUK";
+export const knownSaplingContractPtNairobi = "KT1UHkJDY1CWAgYZJR1NkxXv27gsuu7hC77R";
+export const knownOnChainViewContractAddressPtNairobi = "KT1JxWH1vtMiTcvg4AdhTaGmyHt2oBb71tzW";
