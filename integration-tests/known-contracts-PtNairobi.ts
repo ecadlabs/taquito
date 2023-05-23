138f8ea3f9c3083dd74e17d7c5620f4583a5edc2
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2023-05-23 10:07:56 -0700

    Originated known contracts

--- integration-tests/known-contracts-PtNairobi.ts
+++ integration-tests/known-contracts-PtNairobi.ts
@@ -1,5 +1,5 @@
-export const knownContractPtNairobi = "KT1XFiUYC36XSeLTanGJwZxqLzsxz9zquLFB";
-export const knownBigMapContractPtNairobi = "KT1KbbvszHoWVSS8Nzh9yLgvRBDkzVjKmCtj";
-export const knownTzip12BigMapOffChainContractPtNairobi = "KT1KKU19PxFbQUT9sBJS8KwYCVaXAzYsTkUK";
-export const knownSaplingContractPtNairobi = "KT1UHkJDY1CWAgYZJR1NkxXv27gsuu7hC77R";
-export const knownOnChainViewContractAddressPtNairobi = "KT1JxWH1vtMiTcvg4AdhTaGmyHt2oBb71tzW";
+export const knownContractPtNairobi = "KT1GrzF7DSNc7LrLmS7RNaLrBQqyYHyoMzwR";
+export const knownBigMapContractPtNairobi = "KT1Twd6GBBqHEFhzvBDEn4JiUopttq2WjdnF";
+export const knownTzip12BigMapOffChainContractPtNairobi = "KT1WZUqEKZ4TMW75FKpqod4HwB4ts7wbnsFh";
+export const knownSaplingContractPtNairobi = "KT1VNnD8NWx9ep2gxsHbzrmahrWsKpZb3xGY";
+export const knownOnChainViewContractAddressPtNairobi = "KT19eNryXTuVgH6s6cUc1a5LyjSamdBw4JXo";
