1f8056719c4454a92203d0ad0f0efde482b03933
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2023-06-01 15:55:38 -0700

    chore: revert gas limit for reveal to 2200

--- packages/taquito/src/constants.ts
+++ packages/taquito/src/constants.ts
@@ -2,10 +2,16 @@ export enum DEFAULT_GAS_LIMIT {
   DELEGATION = 10600,
   ORIGINATION = 10600,
   TRANSFER = 10600,
-  REVEAL_TZ1 = 166,
-  REVEAL_TZ2 = 152,
-  REVEAL_TZ3 = 1091,
-  REVEAL_TZ4 = 1671,
+  /* This is used for gas_limit. There is no harm in setting a higher limit. 
+   Only if an account has a balance that is very close to the total gas consumption, 
+   then this margin can fail the operation.
+   Another benefit of this higher value is that then Dapps build with Taquito 17 can
+   still work with Mumbainet, as this value is higher than the reveal cost in Mumbai.
+  */
+  REVEAL_TZ1 = 2000,
+  REVEAL_TZ2 = 2000,
+  REVEAL_TZ3 = 2000,
+  REVEAL_TZ4 = 2000,
 }
 export enum DEFAULT_FEE {
   DELEGATION = 1257,
@@ -80,7 +86,7 @@ export enum ChainIds {
 }
 
 export const getRevealGasLimit = (address: string) =>
-  Math.round((getRevealGasLimitInternal(address) * 15) / 10);
+  Math.round((getRevealGasLimitInternal(address) * 11) / 10);
 
 const getRevealGasLimitInternal = (address: string) => {
   switch (address.substring(0, 3)) {
