5bac388a2f54c5eabe0cce78dc3807238f5aa2ad
huianyang <hui-an.yang@ecadlabs.com>
2023-06-06 15:09:02 -0700

    refactor: improve InvalidDerivationTypeError in @taquito-leddger-signer

--- packages/taquito-ledger-signer/src/errors.ts
+++ packages/taquito-ledger-signer/src/errors.ts
@@ -1,4 +1,4 @@
-import { TaquitoError } from '@taquito/core';
+import { ParameterValidationError, TaquitoError } from '@taquito/core';
 
 /**
  *  @category Error
@@ -32,5 +32,17 @@ export class PublicKeyHashRetrievalError extends TaquitoError {
     super();
     this.name = 'PublicKeyHashRetrievalError';
     this.message = 'Unable to retrieve Public Key Hash from Ledger';
+  }
+}
+
+/**
+ *  @category Error
+ *  @description Error indicates an invalid derivation type being passed or used
+ */
+export class InvalidDerivationTypeError extends ParameterValidationError {
+  constructor(public derivationType: string) {
+    super();
+    this.name = 'InvalidDerivationTypeError';
+    this.message = `Invalid derivation type ${derivationType} expecting one of the following: DerivationType.ED25519, DerivationType.SECP256K1, DerivationType.P256 or DerivationType.BIP32_ED25519`;
   }
 }
--- packages/taquito-ledger-signer/src/taquito-ledger-signer.ts
+++ packages/taquito-ledger-signer/src/taquito-ledger-signer.ts
@@ -19,6 +19,7 @@ import {
   PublicKeyHashRetrievalError,
   PublicKeyRetrievalError,
   InvalidLedgerResponseError,
+  InvalidDerivationTypeError,
 } from './errors';
 import { InvalidDerivationPathError, ProhibitedActionError } from '@taquito/core';
 
@@ -33,18 +34,7 @@ export enum DerivationType {
   BIP32_ED25519 = 0x03, // tz1 BIP32
 }
 
-/**
- *  @category Error
- *  @description Error that indicates an invalid derivation type being passed or used
- */
-export class InvalidDerivationTypeError extends Error {
-  public name = 'InvalidDerivationTypeError';
-  constructor(public derivationType: string) {
-    super(
-      `The derivation type ${derivationType} is invalid. The derivation type must be DerivationType.ED25519, DerivationType.SECP256K1, DerivationType.P256 or DerivationType.BIP32_ED25519`
-    );
-  }
-}
+export { InvalidDerivationTypeError } from './errors';
 
 export const HDPathTemplate = (account: number) => {
   return `44'/1729'/${account}'/0'`;
