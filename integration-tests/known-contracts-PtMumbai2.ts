23ba3466fd4526e349988f274e2256af09a2b95c
Daniele Lisi <22307776+danielelisi@users.noreply.github.com>
2023-06-08 14:12:02 +0200

    Both Mumbai and Nairobi integration test configurations should coexist

---
+++ integration-tests/known-contracts-PtMumbai2.ts
@@ -0,0 +1,5 @@
+export const knownContractPtMumbai2 = "KT1XFiUYC36XSeLTanGJwZxqLzsxz9zquLFB";
+export const knownBigMapContractPtMumbai2 = "KT1KbbvszHoWVSS8Nzh9yLgvRBDkzVjKmCtj";
+export const knownTzip12BigMapOffChainContractPtMumbai2 = "KT1KKU19PxFbQUT9sBJS8KwYCVaXAzYsTkUK";
+export const knownSaplingContractPtMumbai2 = "KT1UHkJDY1CWAgYZJR1NkxXv27gsuu7hC77R";
+export const knownOnChainViewContractAddressPtMumbai2 = "KT1JxWH1vtMiTcvg4AdhTaGmyHt2oBb71tzW";
