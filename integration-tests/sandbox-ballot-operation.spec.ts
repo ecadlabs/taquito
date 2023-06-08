24ecbf3020b61b5ada126dac9de2dd47aa08bcfd
huianyang <hui-an.yang@ecadlabs.com>
2023-06-08 15:27:12 -0700

    test: debugging ci ballot test attemp3

--- integration-tests/sandbox-ballot-operation.spec.ts
+++ integration-tests/sandbox-ballot-operation.spec.ts
@@ -5,27 +5,31 @@ import { CONFIGS, sleep } from './config';
 
 CONFIGS().forEach(async ({ lib, rpc, protocol, setup }) => {
   const flextesanet = rpc === 'http://localhost:20000' ? test : test.skip;
+
+  // Our ci flextesa script have 3 bakers Alice, Bob and Charlie (.github/workflows/main.yml)
+  const Alice = lib;
+  const Bob = new TezosToolkit(rpc);
+  const Charlie = new TezosToolkit(rpc);
   let blocksPerVotingPeriod: number;
   let blockTime: number;
   let currentPeriod: VotingPeriodBlockResult;
 
-  // Our ci flextesa script have 3 bakers Alice, Bob and Charlie (.github/workflows/main.yml)
-  const Alice = lib; // Alice's secret key is passed through the command to run test is configured by integration-tests/config.ts
-  const Bob = new TezosToolkit(rpc);
-  Bob.setSignerProvider(new InMemorySigner('edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt'));
-  const Charlie = new TezosToolkit(rpc);
-  Charlie.setSignerProvider(new InMemorySigner('edsk3RgWvbKKA1atEUcaGwivge7QtckHkTL9nQJUXQKY5r8WKp4pF4'));
-
   describe(`Test Proposal and Ballot operation in ${protocol.substring(0, 8)} with flextesa`, () => {
     beforeAll(async (done) => {
       await setup();
+      // configure baker Bob and Charlie
+      Bob.setSignerProvider(new InMemorySigner('edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt'));
+      Charlie.setSignerProvider(new InMemorySigner('edsk3RgWvbKKA1atEUcaGwivge7QtckHkTL9nQJUXQKY5r8WKp4pF4'));
+
+      // get protocol constants
       let constants = await Alice.rpc.getConstants();
       blocksPerVotingPeriod = constants.blocks_per_cycle * constants.cycles_per_voting_period!;
       blockTime = constants.minimal_block_delay!.toNumber();
       currentPeriod = await Alice.rpc.getCurrentPeriod();
-      if (currentPeriod.voting_period.kind !== 'proposal' || currentPeriod.remaining < 3) {
+
+      // if proposal period remaining blocks is less then 3 make the test sleep to get into next voting period
+      if (currentPeriod.voting_period.kind === 'proposal' && currentPeriod.remaining < 3) {
         await sleep(((currentPeriod.remaining + 1) * blockTime) * 1000)
-        console.log('28currentPeriod', await Alice.rpc.getCurrentPeriod())
       }
       done();
     });
@@ -39,12 +43,13 @@ CONFIGS().forEach(async ({ lib, rpc, protocol, setup }) => {
           proposals: ['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']
         });
         await proposalsOp.confirmation();
-        console.log('42currentPeriod', await Alice.rpc.getCurrentPeriod())
         expect(proposalsOp.operationResults).toBeDefined();
         expect(proposalsOp.operationResults?.kind).toEqual('proposals');
+        expect(proposalsOp.operationResults?.source).toEqual(await Alice.signer.publicKeyHash());
         expect(proposalsOp.operationResults?.proposals).toEqual(['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']);
         expect(proposalsOp.includedInBlock).toBeDefined();
         expect(proposalsOp.hash).toBeDefined();
+        console.log(proposalsOp.period);
 
         // injecting 2 more proposals from baker Bob and Charlie to reach above quorum
         const BobOp = await Bob.contract.proposals({
@@ -53,21 +58,24 @@ CONFIGS().forEach(async ({ lib, rpc, protocol, setup }) => {
         const CharlieOp = await Charlie.contract.proposals({
           proposals: ['ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK']
         });
-        const ops = Promise.all([BobOp.confirmation(), CharlieOp.confirmation()])
-        console.log(await ops)
-        console.log('58currentPeriod', await Alice.rpc.getCurrentPeriod())
+        await Promise.all([BobOp.confirmation(), CharlieOp.confirmation()]);
+        const proposals = await Alice.rpc.getProposals()
+        expect(proposals.length).toBe(1);
+        expect(proposals[0][0]).toBe('ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK');
+        expect(proposals[0][1].toNumber).toBe(3);
         done();
       }
     });
 
     flextesanet('Should be able to inject ballot operation in exploration period', async (done) => {
+
       // if it's still proposal period make the test sleep to get into exploration period to inject ballot operation
       currentPeriod = await Alice.rpc.getCurrentPeriod();
-      console.log('66currentPeriod', currentPeriod)
       if (currentPeriod.voting_period.kind === 'proposal') {
         await sleep(((currentPeriod.remaining + 1) * blockTime) * 1000)
-        console.log('69currentPeriod', await Alice.rpc.getCurrentPeriod())
       };
+
+      // double check if it's exploration period so that we can inject ballot operation
       currentPeriod = await Alice.rpc.getCurrentPeriod();
       if (currentPeriod.voting_period.kind === 'exploration') {
         const explorationBallotOp = await Alice.contract.ballot({
@@ -75,13 +83,17 @@ CONFIGS().forEach(async ({ lib, rpc, protocol, setup }) => {
           ballot: 'yay'
         });
         await explorationBallotOp.confirmation();
-        console.log('76currentPeriod', await Alice.rpc.getCurrentPeriod())
         expect(explorationBallotOp.operationResults).toBeDefined();
         expect(explorationBallotOp.operationResults?.kind).toEqual('ballot');
         expect(explorationBallotOp.operationResults?.proposal).toEqual('ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK');
         expect(explorationBallotOp.operationResults?.ballot).toEqual('yay');
         expect(explorationBallotOp.includedInBlock).toBeDefined();
         expect(explorationBallotOp.hash).toBeDefined();
+
+        const ballotList = await Alice.rpc.getBallotList()
+        expect(ballotList.length).toBe(1);
+        expect(ballotList[0].pkh).toBe('ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK');
+        console.log(ballotList)
         done();
       }
     });
