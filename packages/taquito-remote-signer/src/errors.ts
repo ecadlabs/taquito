81e3a749bbc892a650fc3adbe935a5b80c9f07ea
huianyang <hui-an.yang@ecadlabs.com>
2023-06-08 00:48:08 -0700

    refactor: error improvement of taquito/remote-signer
    BREAKING CHANGE: 1. In @taquito/remote-signer KeyNotFoundError renamed to PublicKeyNotFoundError. 2.
    In @taquito/remote-signer PublicKeyMismatch renamed to PublicKeyVerificationError. 3. In
    @taquito/remote-signer SignatureVerificationFailedError renamed to SignatureVerificationError

--- packages/taquito-remote-signer/src/errors.ts
+++ packages/taquito-remote-signer/src/errors.ts
@@ -1,11 +1,14 @@
+import { PermissionDeniedError, TaquitoError } from '@taquito/core';
+
 /**
  *  @category Error
  *  @description Error indicates a failure in grabbing the public key
  */
-export class KeyNotFoundError extends Error {
-  constructor(public message: string, public innerException: any) {
-    super(message);
+export class PublicKeyNotFoundError extends TaquitoError {
+  constructor(public pkh: string, public cause: any) {
+    super();
     this.name = 'KeyNotFoundError';
+    this.message = `Public key not found of this address "${pkh}".`;
   }
 }
 
@@ -13,9 +16,9 @@ export class KeyNotFoundError extends Error {
  *  @category Error
  *  @description Error indicates an unauthorized operation being attempted
  */
-export class OperationNotAuthorizedError extends Error {
-  constructor(public message: string, public innerException: any) {
-    super(message);
+export class OperationNotAuthorizedError extends PermissionDeniedError {
+  constructor(public message: string, public cause: any) {
+    super();
     this.name = 'OperationNotAuthorized';
   }
 }
@@ -24,10 +27,13 @@ export class OperationNotAuthorizedError extends Error {
  *  @category Error
  *  @description Error indicates bad signing data
  */
-export class BadSigningDataError extends Error {
-  constructor(public message: string, public innerException: any, public readonly data: any) {
-    super(message);
+export class BadSigningDataError extends TaquitoError {
+  constructor(public cause: any, public bytes: string, watermark?: Uint8Array) {
+    super();
     this.name = 'BadSigningData';
+    this.message = watermark
+      ? `Invalid signing data with watermark`
+      : `Invalid signing data: "${bytes}"`;
   }
 }
 
@@ -35,14 +41,27 @@ export class BadSigningDataError extends Error {
  *  @category Error
  *  @description Error indicates a mismatch between the initialized and the requested public key
  */
-export class PublicKeyMismatch extends Error {
-  constructor(public requested: string, initialized: string) {
-    super(
-      `Requested public key hash does not match the initialized public key hash: {
-        requested: ${requested},
-        initialized: ${initialized}
-      }`
-    );
-    this.name = 'PublicKeyMismatch';
+export class PublicKeyVerificationError extends TaquitoError {
+  constructor(
+    public requestedPk: string,
+    public requestedPkh: string,
+    public initializedPkh: string
+  ) {
+    super();
+    this.name = 'PublicKeyVerificationFailedError';
+    this.message = `Requested pk "${requestedPk}" has pkh "${requestedPkh}" deesn't match initialized pkh "${initializedPkh}."`;
+  }
+}
+
+/**
+ *  @category Error
+ *  @description Error
+ */
+export class SignatureVerificationError extends TaquitoError {
+  public name = 'SignatureVerificationFailedError';
+  constructor(public bytes: string, public signature: string) {
+    super();
+    this.name = 'SignatureVerificationFailedError';
+    this.message = `Invalid signature of bytes failed verification agaisnt public key.`;
   }
 }
