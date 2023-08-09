c27162d5d97898cf1919cf4cf251516daf7a1971
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2023-08-09 15:10:41 -0700

    chore: separate test into contract and wallet files

--- integration-tests/wallet-failing-noop.spec.ts
+++ integration-tests/wallet-failing-noop.spec.ts
@@ -44,37 +44,5 @@ CONFIGS().forEach(({ rpc, setup}) => {
       expect(verifySignature(signed.bytes, pk!, signed.signature, new Uint8Array([3]))).toBe(true);
       done();
     });
-
-    it('Verify that the contract.failingNoop signs a text on the genesis block', async (done) => {
-      const signed = await Tezos.contract.failingNoop({
-        arbitrary: "48656C6C6F20576F726C64", // Hello World
-        basedOnBlock: 0,
-      });
-      expect(signed).toEqual({
-        bytes: 'df2788eed43ab680c8a2b79969ce4de93b9768cd2786a85ebdfba90ca7612638110000000b48656c6c6f20576f726c64',
-        signature: 'spsig1QVVCiQ6aN2zmut2wKTg4zWLoP9ia4qUY2hBo21odA7P25gqfieFWJMyntaJWmyrd6v3mgjKF5n4d2wcaB3LxkLmd1MoJQ',
-        signedContent: {
-          branch: await Tezos.rpc.getBlockHash({block: 'genesis'}),
-          contents: [{
-            kind: OpKind.FAILING_NOOP,
-            arbitrary: '48656C6C6F20576F726C64'
-          }]
-        }
-      });
-      const pk = await Tezos.wallet.getPublicKey();
-      expect(verifySignature(signed.bytes, pk!, signed.signature, new Uint8Array([3]))).toBe(true);
-      done();
-    });
-
-    it('Verify that the contract.failingNoop signs a text', async (done) => {
-      const signed = await Tezos.contract.failingNoop({
-        arbitrary: "48656C6C6F20576F726C64", // Hello World
-        basedOnBlock: 'head',
-      });
-      const pk = await Tezos.wallet.getPublicKey();
-      expect(verifySignature(signed.bytes, pk!, signed.signature, new Uint8Array([3]))).toBe(true);
-      done();
-    });
-
   });
 })
