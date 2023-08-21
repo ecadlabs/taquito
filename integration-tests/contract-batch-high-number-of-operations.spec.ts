6571fc09e73d463cc4c6dcf3bf9d52930db9db45
huianyang <hui-an.yang@ecadlabs.com>
2023-08-21 12:15:57 -0700

    test: add try-catch to high-number-of-operation-spec.ts

--- integration-tests/contract-batch-high-number-of-operations.spec.ts
+++ integration-tests/contract-batch-high-number-of-operations.spec.ts
@@ -4,37 +4,37 @@ import { InMemorySigner } from '@taquito/signer';
 const crypto = require('crypto');
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
-    const Tezos = lib;
+  const Tezos = lib;
 
-    describe(`Test contract.batch containing a high number of operations through contract api using: ${rpc}`, () => {
-        beforeEach(async (done) => {
-            await setup(true);
-            done();
-        });
-
-        it('Verify contract.batch with 150 operations', async (done) => {
-            const dests: { key: string, pkh: string }[] = [];
-            const batchSize = 150;
-
-            for (let i = 0; i < batchSize; i++) {
-                const keyBytes = Buffer.alloc(32);
-                crypto.randomFillSync(keyBytes)
-
-                const key = b58cencode(new Uint8Array(keyBytes), prefix[Prefix.SPSK]);
-                const pkh = await new InMemorySigner(key).publicKeyHash();
-                dests.push({ key, pkh });
-            }
-
-            const batch = Tezos.contract.batch()
-            dests.forEach(({ pkh }) => {
-                batch.withTransfer({ to: pkh, amount: 0.001});
-            })
-
-            const op = await batch.send();
-            await op.confirmation(1, 700);
-
-            expect(op.status).toEqual('applied');
-            done();
-        });
+  describe(`Test contract.batch containing a high number of operations through contract api using: ${rpc}`, () => {
+    beforeEach(async (done) => {
+      await setup(true);
+      done();
     });
+
+    it('Verify contract.batch with 150 operations', async (done) => {
+      const dests: { key: string, pkh: string }[] = [];
+      const batchSize = 150;
+
+      for (let i = 0; i < batchSize; i++) {
+        const keyBytes = Buffer.alloc(32);
+        crypto.randomFillSync(keyBytes)
+
+        const key = b58cencode(new Uint8Array(keyBytes), prefix[Prefix.SPSK]);
+        const pkh = await new InMemorySigner(key).publicKeyHash();
+        dests.push({ key, pkh });
+      }
+
+      const batch = Tezos.contract.batch()
+      dests.forEach(({ pkh }) => {
+        batch.withTransfer({ to: pkh, amount: 0.001 });
+      })
+      try {
+        const op = await batch.send();
+        await op.confirmation(1, 300);
+        expect(op.status).toEqual('applied');
+      } catch (e) { console.log(JSON.stringify(e)) }
+      done();
+    });
+  });
 });
