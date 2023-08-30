29c2effd59ecc02a2a6661dc5341593afab7d8c6
arvidj <arvid.jakobsson@nomadic-labs.com>
2023-08-30 19:44:25 +0200

    integration tests: don't hardcode testnet constant values (#2164)
    * integration-tests: refactor knownContracts configuration
    
    * integration-tests: refactor config.ts
    
    * integration-tests: add [NetworkType] configuration parameter
    
    This enables the distinction between 'real' test networks, such as
    Jakartanet, and locally spawned sandbox networks.
    
    * integration-tests: only run [rpc-get-protocol-constants] in testnet

--- integration-tests/known-contracts-PtGhostnet.ts
+++ integration-tests/known-contracts-PtGhostnet.ts
@@ -1,5 +1,8 @@
-export const knownContractPtGhostnet = "KT1HwcBvLjyQZi6CapBfCt5xiXSW5KcaJ4A5";
-export const knownBigMapContractPtGhostnet = "KT1Dig4sQxbZaZjo9X18etvpEWN1eGKxTVk2";
-export const knownTzip12BigMapOffChainContractPtGhostnet = "KT1NMtSQq484bDYSFvNrBjfkGtpug2Fm1rrr";
-export const knownSaplingContractPtGhostnet = "KT1ToBD7bovonshNrxs3i4KMFuZ8PE2LUmQf";
-export const knownOnChainViewContractAddressPtGhostnet = "KT1R5spjSabhD9q8cwZfCvssuvUoHmEd6KNv";
+import { KnownContracts } from './known-contracts';
+export const knownContractsPtGhostnet: KnownContracts = {
+  contract: "KT1HwcBvLjyQZi6CapBfCt5xiXSW5KcaJ4A5",
+  bigMapContract: "KT1Dig4sQxbZaZjo9X18etvpEWN1eGKxTVk2",
+  tzip12BigMapOffChainContract: "KT1NMtSQq484bDYSFvNrBjfkGtpug2Fm1rrr",
+  saplingContract: "KT1ToBD7bovonshNrxs3i4KMFuZ8PE2LUmQf",
+  onChainViewContractAddress: "KT1R5spjSabhD9q8cwZfCvssuvUoHmEd6KNv",
+}
--- integration-tests/known-contracts-PtNairobi.ts
+++ integration-tests/known-contracts-PtNairobi.ts
@@ -1,5 +1,8 @@
-export const knownContractPtNairobi = "KT1GrzF7DSNc7LrLmS7RNaLrBQqyYHyoMzwR";
-export const knownBigMapContractPtNairobi = "KT1Twd6GBBqHEFhzvBDEn4JiUopttq2WjdnF";
-export const knownTzip12BigMapOffChainContractPtNairobi = "KT1WZUqEKZ4TMW75FKpqod4HwB4ts7wbnsFh";
-export const knownSaplingContractPtNairobi = "KT1VNnD8NWx9ep2gxsHbzrmahrWsKpZb3xGY";
-export const knownOnChainViewContractAddressPtNairobi = "KT19eNryXTuVgH6s6cUc1a5LyjSamdBw4JXo";
+import { KnownContracts } from './known-contracts';
+export const knownContractsPtNairobi: KnownContracts = {
+  contract: "KT1GrzF7DSNc7LrLmS7RNaLrBQqyYHyoMzwR",
+  bigMapContract: "KT1Twd6GBBqHEFhzvBDEn4JiUopttq2WjdnF",
+  tzip12BigMapOffChainContract: "KT1WZUqEKZ4TMW75FKpqod4HwB4ts7wbnsFh",
+  saplingContract: "KT1VNnD8NWx9ep2gxsHbzrmahrWsKpZb3xGY",
+  onChainViewContractAddress: "KT19eNryXTuVgH6s6cUc1a5LyjSamdBw4JXo"
+};
