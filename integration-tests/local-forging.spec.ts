2bc19bc8eefacd0bb387ab9f2c3a21f4e70c8208
hui-an-yang <106410553+hui-an-yang@users.noreply.github.com>
2023-09-06 12:10:00 -0700

    Mondaynet update 0905 (#2564)
    * test: changes for alpha protocol 1
    
    * test: changes for alpha protocol 2
    
    * test: updated mondaynet-2023-07-17 known contract
    
    * test: updated modanynet changes of protocol constants
    
    * test: updated modanynet changes of protocol constants 2023-07-25
    
    * test: updated mondaynet changes of protocol constants 2023-08-02
    
    * test: updated mondaynet known-contracts
    
    * test: updated mondaynet changes of protocol constsants 2023-08-22
    
    * revert: revert protocol change that belongs to protocol P
    
    * test: updated mondaynet known-contracts 0826
    
    * revert: integration-tests/known-contracts-PtNairobi.ts back
    
    * test: updated mondaynet known-contracts 0826
    
    * test: mondaynet update of week 0828
    
    * test: mondaynet update of week 0905
    
    * revert: add back failing_noop test case
    
    * fix: complete defaultRpc url of ghostnet
    
    * test: added back networktype
    
    * test: updating estimation assertion

--- integration-tests/local-forging.spec.ts
+++ integration-tests/local-forging.spec.ts
@@ -1,13 +1,26 @@
 import { CONFIGS } from "./config";
-import { commonCases } from './data/allTestsCases';
+import { commonCases, nairobiCases } from './data/allTestsCases';
 import { LocalForger, ProtocolsHash } from '@taquito/local-forging'
-import { TezosToolkit } from "@taquito/taquito";
+import { Protocols, TezosToolkit } from "@taquito/taquito";
 
 CONFIGS().forEach(({ rpc, protocol }) => {
   const Tezos = new TezosToolkit(rpc);
+  const nairobinet = protocol === Protocols.PtNairobi ? it : it.skip;
 
   describe(`Test local forger: ${rpc}`, () => {
     // all protocols
+    nairobiCases.forEach(({ name, operation, expected }) => {
+      nairobinet(`Verify that .forge for local forge will return same result as for network forge for rpc: ${name} (${rpc})`, async done => {
+        const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
+        const result = await localForger.forge(operation);
+        const rpcResult = await Tezos.rpc.forgeOperations(operation);
+        expect(result).toEqual(rpcResult);
+        expect(await localForger.parse(result)).toEqual(expected || operation);
+
+        done();
+      });
+    });
+
     commonCases.forEach(({ name, operation, expected }) => {
       it(`Verify that .forge for local forge will return same result as for network forge for rpc: ${name} (${rpc})`, async done => {
         const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
