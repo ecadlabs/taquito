59def8f78c0423b93552b8d8cea35ebfefd6c2d1
hui-an-yang <106410553+hui-an-yang@users.noreply.github.com>
2023-09-18 16:19:18 -0700

    2646 remove set deposits limit op reference (#2655)
    * refactor: removed set_deposit_limit in local-forger.forge() for oxford
    
    re #2646
    
    * test: updated local-forging test to be compatible both nairobi and oxford
    
    * chore: syncing the fix of known-contract oxfordnet bug

--- integration-tests/local-forging.spec.ts
+++ integration-tests/local-forging.spec.ts
@@ -1,12 +1,24 @@
 import { CONFIGS } from "./config";
-import { commonCases } from './data/allTestsCases';
+import { commonCases, nairobiCases } from './data/allTestsCases';
 import { LocalForger, ProtocolsHash } from '@taquito/local-forging'
-import { TezosToolkit } from "@taquito/taquito";
+import { Protocols, TezosToolkit } from "@taquito/taquito";
 
 CONFIGS().forEach(({ rpc, protocol }) => {
   const Tezos = new TezosToolkit(rpc);
+  const nairobinet = protocol === Protocols.PtNairobi ? it : it.skip;
 
   describe(`Test local forger: ${rpc}`, () => {
+    nairobiCases.forEach(({ name, operation, expected }) => {
+      // Oxford has removed to forge set and unset deposit limit ops in rpcForger.
+      // We removed them in localForger .forge, but keep the .parse logic for now.
+      nairobinet(`Verify that .parse for local forge will return same operation that rpc forge for rpc: ${name} (${rpc})`, async done => {
+        const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
+        const rpcResult = await Tezos.rpc.forgeOperations(operation);
+        expect(await localForger.parse(rpcResult)).toEqual(expected || operation);
+
+        done();
+      });
+    });
     // all protocols
     commonCases.forEach(({ name, operation, expected }) => {
       it(`Verify that .forge for local forge will return same result as for network forge for rpc: ${name} (${rpc})`, async done => {
