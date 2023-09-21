cdb31bf028f7df9e1966d2913766656f839371b7
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2023-09-21 12:23:09 -0700

    fix: test staking in Oxford only

--- integration-tests/contract-staking.spec.ts
+++ integration-tests/contract-staking.spec.ts
@@ -1,14 +1,15 @@
+import { Protocols } from "@taquito/taquito";
 import { CONFIGS, isSandbox } from "./config";
 
-CONFIGS().forEach(({ lib, rpc, setup }) => {
+CONFIGS().forEach(({ lib, rpc, setup, protocol }) => {
     const Tezos = lib;
     describe(`Test staking through contract API using: ${rpc}`, () => {
-      const flextesaNet = isSandbox({rpc}) ? test : test.skip;
+      const flextesaOxford = isSandbox({rpc}) && protocol === Protocols.Proxford ? test : test.skip;
       beforeEach(async (done) => {
           await setup(true)
           done()
         });
-        flextesaNet('Should be able to stake', async (done) => {
+        flextesaOxford('Should be able to stake', async (done) => {
           const op = await Tezos.contract.stake({
             amount: 0.1,
           });
@@ -16,7 +17,7 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
           expect(op.hash).toBeDefined();
           done();
         });
-        flextesaNet('Should be able to unstake', async (done) => {
+        flextesaOxford('Should be able to unstake', async (done) => {
           const op = await Tezos.contract.unstake({
             amount: 0.1,
           });
@@ -24,7 +25,7 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
           expect(op.hash).toBeDefined();
           done();
         });
-        flextesaNet('Should be able to finalizeUnstake', async (done) => {
+        flextesaOxford('Should be able to finalizeUnstake', async (done) => {
           const op = await Tezos.contract.finalizeUnstake({});
           await op.confirmation()
           expect(op.hash).toBeDefined();
