125377051fa1352d3f78a9073f00bbf9307f5096
hui-an-yang <106410553+hui-an-yang@users.noreply.github.com>
2023-09-27 12:11:23 -0700

    test: added timelock in localForger testcase (#2670)
    re #2653

--- integration-tests/local-forging.spec.ts
+++ integration-tests/local-forging.spec.ts
@@ -1,11 +1,12 @@
 import { CONFIGS } from "./config";
-import { commonCases, nairobiCases } from './data/allTestsCases';
+import { commonCases, nairobiCases, oxfordCases } from './data/allTestsCases';
 import { LocalForger, ProtocolsHash } from '@taquito/local-forging'
 import { Protocols, TezosToolkit } from "@taquito/taquito";
 
 CONFIGS().forEach(({ rpc, protocol }) => {
   const Tezos = new TezosToolkit(rpc);
   const nairobinet = protocol === Protocols.PtNairobi ? it : it.skip;
+  const oxfordnet = protocol === Protocols.ProxfordS ? it : it.skip;
 
   describe(`Test local forger: ${rpc}`, () => {
     nairobiCases.forEach(({ name, operation, expected }) => {
@@ -15,6 +16,17 @@ CONFIGS().forEach(({ rpc, protocol }) => {
         const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
         const rpcResult = await Tezos.rpc.forgeOperations(operation);
         expect(await localForger.parse(rpcResult)).toEqual(expected || operation);
+
+        done();
+      });
+    });
+    oxfordCases.forEach(({ name, operation, expected }) => {
+      oxfordnet(`Verify that .forge for local forge will return same result as for network forge for rpc: ${name} (${rpc})`, async done => {
+        const localForger = new LocalForger(protocol as unknown as ProtocolsHash);
+        const result = await localForger.forge(operation);
+        const rpcResult = await Tezos.rpc.forgeOperations(operation);
+        expect(result).toEqual(rpcResult);
+        expect(await localForger.parse(result)).toEqual(expected || operation);
 
         done();
       });
