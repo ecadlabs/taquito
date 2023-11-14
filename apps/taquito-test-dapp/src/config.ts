4f44dd73b7659554c167acb80f0c20f222f893a5
AlirezaHaghshenas <alirezahaghshenas@gmail.com>
2023-11-14 14:32:18 -0800

    Subscribe to all events in the test dApp (#2713)
    * feat: subscribed to all events in the test dApp
    
    * chore: slightly improve apparance of event logs
    
    * chore: minor improvements to appearance
    
    * chore: clean unnecessary comments

--- apps/taquito-test-dapp/src/config.ts
+++ apps/taquito-test-dapp/src/config.ts
@@ -1,13 +1,30 @@
 import { NetworkType } from "@airgap/beacon-sdk";
 
-export const rpcUrl = {
-  mumbainet: "https://mumbainet.ecadinfra.com/",
-  nairobinet: "https://nairobinet.ecadinfra.com/",
-  ghostnet: "https://ghostnet.ecadinfra.com/",
-  ithacanet: "https://ithacanet.ecadinfra.com/",
-  mainnet: "https://mainnet.ecadinfra.com", //"https://mainnet-tezos.giganode.io"
-  custom: "https://ghostnet.ecadinfra.com/"
+export type SupportedNetworks = NetworkType.NAIROBINET | NetworkType.GHOSTNET | NetworkType.MAINNET | NetworkType.CUSTOM;
+
+const rpcUrls: Record<SupportedNetworks, string> = {
+  [NetworkType.NAIROBINET]: "https://nairobinet.ecadinfra.com/",
+  [NetworkType.GHOSTNET]: "https://ghostnet.ecadinfra.com/",
+  [NetworkType.MAINNET]: "https://mainnet.ecadinfra.com", //"https://mainnet-tezos.giganode.io"
+  [NetworkType.CUSTOM]: "https://ghostnet.ecadinfra.com/",
 };
+
+export const getRpcUrl = (networkType: SupportedNetworks): string => {
+  return rpcUrls[networkType];
+}
+
+export const getTzKtUrl = (networkType: SupportedNetworks): string | undefined => {
+  switch (networkType) {
+    case NetworkType.NAIROBINET:
+      return "https://nairobinet.tzkt.io";
+    case NetworkType.GHOSTNET:
+      return "https://ghostnet.tzkt.io";
+    case NetworkType.MAINNET:
+      return "https://tzkt.io";
+    case NetworkType.CUSTOM:
+      return undefined;
+  }
+}
 
 export const defaultMatrixNode = "beacon-node-1.sky.papers.tech";
 
