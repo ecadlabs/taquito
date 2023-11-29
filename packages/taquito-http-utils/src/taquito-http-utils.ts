882a5659870df1db0b9f0b9c13ce642b39cb3bd7
Davis Sawali <davis.sawali@ecadlabs.com>
2023-11-29 12:29:41 -0800

    Changed axios and axios fetch adapter usage into node-fetch  (#2736)
    * Changed axios and axios fetch adapter usage into node-fetch implementation
    
    * removed axios reference in the sapling package and replaced it with the fetch counterpart
    
    * updated documentation on cancellable http backend class
    
    * changed sequence of destructured param
    
    * removed references to node-fetch
    
    * updated http-utils to conditionally use node-fetch depending on env
    
    * updated condition for browser

--- packages/taquito-http-utils/src/taquito-http-utils.ts
+++ packages/taquito-http-utils/src/taquito-http-utils.ts
@@ -3,23 +3,20 @@
  * @module @taquito/http-utils
  */
 
-import fetchAdapter from './fetch-adapter';
-import { STATUS_CODE } from './status_code';
-import axios from 'axios';
-import { HttpRequestFailed, HttpResponseError } from './errors';
-
+let fetch = globalThis?.fetch;
+// Will only use browser fetch if we are in a browser environment,
+// default to the more stable node-fetch otherwise
 const isNode = typeof process !== 'undefined' && !!process?.versions?.node;
+if (isNode) {
+  fetch = require('node-fetch');
+}
 
-const adapter = isNode ? undefined : fetchAdapter;
+import { STATUS_CODE } from './status_code';
+import { HttpRequestFailed, HttpResponseError, HttpTimeoutError } from './errors';
 
 export * from './status_code';
 export { VERSION } from './version';
-export { HttpRequestFailed, HttpResponseError } from './errors';
-
-enum ResponseType {
-  TEXT = 'text',
-  JSON = 'json',
-}
+export { HttpRequestFailed, HttpResponseError, HttpTimeoutError } from './errors';
 
 type ObjectType = Record<string, any>;
 
@@ -74,60 +71,63 @@ export class HttpBackend {
   /**
    *
    * @param options contains options to be passed for the HTTP request (url, method and timeout)
-   * @throws {@link HttpRequestFailed} | {@link HttpResponseError}
+   * @throws {@link HttpRequestFailed} | {@link HttpResponseError} | {@link HttpTimeoutError}
    */
   async createRequest<T>(
     { url, method, timeout = this.timeout, query, headers = {}, json = true }: HttpRequestOptions,
     data?: object | string
   ) {
+    // Serializes query params
     const urlWithQuery = url + this.serialize(query);
-    let resType: ResponseType;
-    let transformResponse = undefined;
 
+    // Adds default header entry if there aren't any Content-Type header
     if (!headers['Content-Type']) {
       headers['Content-Type'] = 'application/json';
     }
 
-    if (!json) {
-      resType = ResponseType.TEXT;
-      transformResponse = [<Type>(v: Type) => v];
-    } else {
-      resType = ResponseType.JSON;
-    }
+    // Creates a new AbortController instance to handle timeouts
+    const controller = new AbortController();
+    const t = setTimeout(() => controller.abort(), timeout);
 
     try {
-      const response = await axios.request<T>({
-        url: urlWithQuery,
-        method: method ?? 'GET',
-        headers: headers,
-        responseType: resType,
-        transformResponse,
-        timeout: timeout,
-        data: data,
-        adapter,
+      const response = await fetch(urlWithQuery, {
+        method,
+        headers,
+        body: JSON.stringify(data),
+        signal: controller.signal,
       });
 
-      return response.data;
-    } catch (err: any) {
-      if ((axios.isAxiosError(err) && err.response) || (!isNode && err.response)) {
-        let errorData;
+      if (typeof response === 'undefined') {
+        throw new Error('Response is undefined');
+      }
 
-        if (typeof err.response.data === 'object') {
-          errorData = JSON.stringify(err.response.data);
-        } else {
-          errorData = err.response.data;
-        }
-
+      // Handle responses with status code >= 400
+      if (response.status >= 400) {
+        const errorData = await response.text();
         throw new HttpResponseError(
-          `Http error response: (${err.response.status}) ${errorData}`,
-          err.response.status as STATUS_CODE,
-          err.response.statusText,
+          `Http error response: (${response.status}) ${errorData}`,
+          response.status as STATUS_CODE,
+          response.statusText,
           errorData,
           urlWithQuery
         );
-      } else {
-        throw new HttpRequestFailed(String(method), urlWithQuery, err);
       }
+
+      if (json) {
+        return response.json() as T;
+      } else {
+        return response.text() as unknown as T;
+      }
+    } catch (e: unknown) {
+      if (e instanceof Error && e.name === 'AbortError') {
+        throw new HttpTimeoutError(timeout, urlWithQuery);
+      } else if (e instanceof HttpResponseError) {
+        throw e;
+      } else {
+        throw new HttpRequestFailed(String(method), urlWithQuery, e as Error);
+      }
+    } finally {
+      clearTimeout(t);
     }
   }
 }
