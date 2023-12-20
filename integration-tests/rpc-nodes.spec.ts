7568dd40972994b916a3626ea1c155b35ab29834
huianyang <hui-an.yang@ecadlabs.com>
2023-12-20 08:51:25 -0800

    test: fix integration test of pendingOperations

--- integration-tests/rpc-nodes.spec.ts
+++ integration-tests/rpc-nodes.spec.ts
@@ -1,10 +1,11 @@
-import { CONFIGS } from './config';
-import { DefaultContractType } from "@taquito/taquito";
-import { RpcClientCache, RpcClient, RPCRunViewParam, RPCRunScriptViewParam, PendingOperations, PvmKind } from '@taquito/rpc';
+import { CONFIGS, NetworkType } from './config';
+import { DefaultContractType, Protocols } from "@taquito/taquito";
+import { RpcClientCache, RpcClient, RPCRunViewParam, RPCRunScriptViewParam, PendingOperationsV1, PendingOperationsV2, PvmKind } from '@taquito/rpc';
 import { encodeExpr } from '@taquito/utils';
 import { Schema } from '@taquito/michelson-encoder';
 import { tokenBigmapCode, tokenBigmapStorage } from './data/token_bigmap';
 import { ticketCode, ticketStorage } from './data/code_with_ticket';
+import { ProtoGreaterOrEqual } from '@taquito/michel-codec';
 
 CONFIGS().forEach(
   ({
@@ -20,6 +21,7 @@ CONFIGS().forEach(
   }) => {
     const Tezos = lib;
     const unrestrictedRPCNode = rpc.endsWith("ecadinfra.com") ? test.skip : test;
+    const oxfordAndAlpha = ProtoGreaterOrEqual(protocol, Protocols.ProxfordY) ? test : test.skip
 
     let ticketContract: DefaultContractType;
 
@@ -445,10 +447,20 @@ CONFIGS().forEach(
           expect(ticketBalances[0].amount).toBeDefined();
         });
 
-        it('Verify that rpcClient.getPendingOperations will retrieve the pending operations in mempool', async () => {
-          const pendingOperations: PendingOperations = await rpcClient.getPendingOperations();
+        it('Verify that rpcClient.getPendingOperations v1 will retrieve the pending operations in mempool with property applied', async () => {
+          const pendingOperations = await rpcClient.getPendingOperations({ version: '1'}) as PendingOperationsV1;
           expect(pendingOperations).toBeDefined();
           expect(pendingOperations.applied).toBeInstanceOf(Array);
+          expect(pendingOperations.refused).toBeInstanceOf(Array);
+          expect(pendingOperations.outdated).toBeInstanceOf(Array);
+          expect(pendingOperations.branch_delayed).toBeInstanceOf(Array);
+          expect(pendingOperations.branch_refused).toBeInstanceOf(Array);
+        });
+
+        oxfordAndAlpha('Verify that rpcClient.getPendingOperations v2 will retrieve the pending operations in mempool with property validated', async () => {
+          const pendingOperations = await rpcClient.getPendingOperations({ version: '2' }) as PendingOperationsV2;
+          expect(pendingOperations).toBeDefined();
+          expect(pendingOperations.validated).toBeInstanceOf(Array);
           expect(pendingOperations.refused).toBeInstanceOf(Array);
           expect(pendingOperations.outdated).toBeInstanceOf(Array);
           expect(pendingOperations.branch_delayed).toBeInstanceOf(Array);
