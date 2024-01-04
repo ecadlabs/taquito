c7fb6e7aa6d9eca65b6c24a8492ba83a0762d85c
Davis Sawali <davis.sawali@ecadlabs.com>
2024-01-04 12:28:31 -0800

    chore(releng) bump version to 17.5.1

--- packages/taquito-michelson-encoder/src/version.ts
+++ packages/taquito-michelson-encoder/src/version.ts
@@ -1,5 +1,5 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 export const VERSION = {
-  commitHash: 'ff6c3b619e314a09bb0aa65d711ef9eaf81a0ee9',
-  version: '17.5.0',
+  commitHash: '89ba984a102a13cef7ad7cca6a42df85dfb63b80',
+  version: '17.5.1',
 };
