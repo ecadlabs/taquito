139330bbf14149ae22bf9b3859a7f7ee1b415a23
Davis Sawali <davis.sawali@ecadlabs.com>
2024-04-05 13:08:40 -0700

    added timelock package and integration test (#2857)
    * added timelock package and integration test
    
    * added unit tests for timelock
    
    * updated package.json
    
    * updated exports and integration test
    
    * added documentation for timelock
    
    * added package level readme content
    
    * added interface file, updated docs, re-worked exports
    
    * fixed build, adjusted import
    
    * test: added open-chest test and replaced hardcoded chest and chestKey
    
    * test: removed unnecessary console.logs
    
    * updated package lock
    
    ---------
    
    Co-authored-by: huianyang <hui-an.yang@ecadlabs.com>

---
+++ packages/taquito-timelock/rollup.config.ts
@@ -0,0 +1,26 @@
+import camelCase from 'lodash.camelcase';
+import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
+
+const pkg = require('./package.json');
+
+const libraryName = 'taquito-timelock';
+
+export default {
+  input: `src/taquito-timelock.ts`,
+  output: [
+    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
+  ],
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  external: [],
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    // Allow json resolution
+    json(),
+    // Compile TypeScript files
+    typescript({ tsconfig: './tsconfig.prod.json', useTsconfigDeclarationDir: true }),
+  ],
+};
