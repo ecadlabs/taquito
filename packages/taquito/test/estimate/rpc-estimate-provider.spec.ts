d95d865d97ba27e962802f0ebc0526a6595d77e9
Davis Sawali <davis.sawali@ecadlabs.com>
2024-04-23 18:11:36 -0700

    added staking support in contract API, with unit and integration tests (#2930)
    * added staking support in contract API, with unit and integration tests
    
    * updated ux to not require destination (for stake/unstake) and not require a param for finalize unstake
    
    * updated error message
    
    * addressed PR comments
    
    * remove wrong check
    
    * removed undefined check for a more general falsy check

--- packages/taquito/test/estimate/rpc-estimate-provider.spec.ts
+++ packages/taquito/test/estimate/rpc-estimate-provider.spec.ts
@@ -22,6 +22,9 @@ import {
   smartRollupAddMessagesNoReveal,
   smartRollupOriginateWithReveal,
   smartRollupExecuteOutboxMessageNoReveal,
+  stakeNoReveal,
+  unstakeNoReveal,
+  finalizeUnstakeNoReveal,
 } from '../contract/helper';
 import { OpKind, PvmKind } from '@taquito/rpc';
 import { TransferTicketParams } from '../../src/operations/types';
@@ -175,6 +178,36 @@ describe('RPCEstimateProvider test signer', () => {
         init: '{}',
       });
       expect(estimate.gasLimit).toEqual(1100);
+    });
+  });
+
+  describe('staking', () => {
+    it('should return estimates for stake pseudo-operation', async () => {
+      mockRpcClient.simulateOperation.mockResolvedValue(stakeNoReveal);
+
+      const estimate = await estimateProvider.stake({
+        amount: 2,
+      });
+
+      expect(estimate.gasLimit).toEqual(3730);
+    });
+
+    it('should return estimates for unstake pseudo-operation', async () => {
+      mockRpcClient.simulateOperation.mockResolvedValue(unstakeNoReveal);
+
+      const estimate = await estimateProvider.unstake({
+        amount: 2,
+      });
+
+      expect(estimate.gasLimit).toEqual(4350);
+    });
+
+    it('should return estimates for finalize_unstake pseudo-operation', async () => {
+      mockRpcClient.simulateOperation.mockResolvedValue(finalizeUnstakeNoReveal);
+
+      const estimate = await estimateProvider.finalizeUnstake({});
+
+      expect(estimate.gasLimit).toEqual(1629);
     });
   });
 
