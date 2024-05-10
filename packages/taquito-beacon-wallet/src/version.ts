5b9e3029baaef495f4ae8377dfb82dcd225c26f9
Davis Sawali <davis.sawali@ecadlabs.com>
2024-05-10 12:04:52 -0700

    chore(releng) bump version to 19.2.1-beta.2

--- packages/taquito-beacon-wallet/src/version.ts
+++ packages/taquito-beacon-wallet/src/version.ts
@@ -1,5 +1,5 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 export const VERSION = {
-  commitHash: '53a72356967af6908595cc3cdd3289183c4b2489',
-  version: '19.2.1-beta.1',
+  commitHash: '06c78f51d36ac42cd51e4fc747ebceb5df3cd19b',
+  version: '19.2.1-beta.2',
 };
