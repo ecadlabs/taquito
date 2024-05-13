ac5cb5bcb6d98e36ba3d1024baf600900e7ec357
Davis Sawali <davis.sawali@ecadlabs.com>
2024-05-13 13:05:13 -0700

    updated taquito-beacon-wallet to use eventsub

--- packages/taquito-beacon-wallet/src/taquito-beacon-wallet.ts
+++ packages/taquito-beacon-wallet/src/taquito-beacon-wallet.ts
@@ -10,6 +10,8 @@ import {
   PermissionScope,
   getDAppClientInstance,
   SigningType,
+  AccountInfo,
+  BeaconEvent,
 } from '@airgap/beacon-dapp';
 import { BeaconWalletNotInitialized, MissingRequiredScopes } from './errors';
 import toBuffer from 'typedarray-to-buffer';
@@ -32,9 +34,15 @@ export { BeaconWalletNotInitialized, MissingRequiredScopes } from './errors';
 
 export class BeaconWallet implements WalletProvider {
   public client: DAppClient;
+  public account: AccountInfo | undefined;
 
   constructor(options: DAppClientOptions) {
     this.client = getDAppClientInstance(options);
+
+    this.client.subscribeToEvent(BeaconEvent.ACTIVE_ACCOUNT_SET, async (data) => {
+      console.log(`${BeaconEvent.ACTIVE_ACCOUNT_SET} triggered: `, data);
+      this.account = data;
+    });
   }
 
   private validateRequiredScopesOrFail(
@@ -59,19 +67,19 @@ export class BeaconWallet implements WalletProvider {
   }
 
   async getPKH() {
-    const account = await this.client.getActiveAccount();
-    if (!account) {
+    // const account = await this.client.getActiveAccount();
+    if (!this.account) {
       throw new BeaconWalletNotInitialized();
     }
-    return account.address;
+    return this.account.address;
   }
 
   async getPK() {
-    const account = await this.client.getActiveAccount();
-    if (!account) {
+    // const account = await this.client.getActiveAccount();
+    if (!this.account) {
       throw new BeaconWalletNotInitialized();
     }
-    return account.publicKey ?? '';
+    return this.account.publicKey ?? '';
   }
 
   async mapTransferParamsToWalletParams(params: () => Promise<WalletTransferParams>) {
@@ -167,11 +175,11 @@ export class BeaconWallet implements WalletProvider {
   }
 
   async sendOperations(params: any[]) {
-    const account = await this.client.getActiveAccount();
-    if (!account) {
+    // const account = await this.client.getActiveAccount();
+    if (!this.account) {
       throw new BeaconWalletNotInitialized();
     }
-    const permissions = account.scopes;
+    const permissions = this.account.scopes;
     this.validateRequiredScopesOrFail(permissions, [PermissionScope.OPERATION_REQUEST]);
 
     const { transactionHash } = await this.client.requestOperation({ operationDetails: params });
