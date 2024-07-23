518caecc943917aa4cbabb859f144585fab65760
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2024-07-23 16:25:15 -0700

    test: disconnect BeaconWallet in tests, change jest setup

--- packages/taquito-beacon-wallet/test/taquito-beacon-wallet.spec.ts
+++ packages/taquito-beacon-wallet/test/taquito-beacon-wallet.spec.ts
@@ -12,7 +12,9 @@ global.window = { addEventListener: jest.fn() } as any;
 
 describe('Beacon Wallet tests', () => {
   it('Verify that BeaconWallet is instantiable', () => {
-    expect(new BeaconWallet({ name: 'testWallet' })).toBeInstanceOf(BeaconWallet);
+    const wallet = new BeaconWallet({ name: 'testWallet' });
+    expect(wallet).toBeInstanceOf(BeaconWallet);
+    wallet.disconnect();
   });
 
   it('Verify BeaconWallet not initialized error', () => {
@@ -24,11 +26,13 @@ describe('Beacon Wallet tests', () => {
   });
 
   it('Verify that permissions must be called before getPKH', async () => {
+    const wallet = new BeaconWallet({ name: 'testWallet' });
     try {
-      const wallet = new BeaconWallet({ name: 'testWallet' });
       await wallet.getPKH();
     } catch (error: any) {
       expect(error.message).toContain('BeaconWallet needs to be initialized');
+    } finally {
+      wallet.disconnect();
     }
   });
 
@@ -36,15 +40,18 @@ describe('Beacon Wallet tests', () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     expect(typeof (await wallet.client.beaconId)).toEqual('string');
     expect(await wallet.client.beaconId).toBeDefined;
+    wallet.disconnect();
   });
 
   it(`Verify that an error is thrown if BeaconWallet is initialized with an empty object`, async () => {
+    const wallet = new BeaconWallet({} as any);
     try {
-      const wallet = new BeaconWallet({} as any);
       expect(wallet).toBeDefined();
     } catch (e) {
       console.log('hello there', e);
       expect((e as any).message).toEqual('Name not set');
+    } finally {
+      wallet.disconnect();
     }
   });
 
@@ -52,24 +59,28 @@ describe('Beacon Wallet tests', () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const formattedParam = await wallet.formatParameters({ fee: 10 });
     expect(formattedParam.fee).toEqual('10');
+    wallet.disconnect();
   });
 
   it(`Verify formatParameters for storageLimit`, async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const formattedParam = await wallet.formatParameters({ storageLimit: 2000 });
     expect(formattedParam.storageLimit).toEqual('2000');
+    wallet.disconnect();
   });
 
   it(`Verify formatParameters for gasLimit`, async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const formattedParam = await wallet.formatParameters({ gasLimit: 40 });
     expect(formattedParam.gasLimit).toEqual('40');
+    wallet.disconnect();
   });
 
   it(`Verify removeDefaultParameters for fees`, async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const formattedParam = await wallet.removeDefaultParams({ fee: 10 }, { fee: 30 });
     expect(formattedParam.fee).toEqual(30);
+    wallet.disconnect();
   });
 
   it(`Verify removeDefaultParameters for storageLimit`, async () => {
@@ -79,42 +90,49 @@ describe('Beacon Wallet tests', () => {
       { storageLimit: 165 }
     );
     expect(formattedParam.storageLimit).toEqual(165);
+    wallet.disconnect();
   });
 
   it(`Verify removeDefaultParameters for gas limit`, async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const formattedParam = await wallet.removeDefaultParams({ gasLimit: 40 }, { gasLimit: 80 });
     expect(formattedParam.gasLimit).toEqual(80);
+    wallet.disconnect();
   });
 
   it('Verify getSigningType returns correct signing type for undefined', async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const signingType = wallet['getSigningType'](undefined);
     expect(signingType).toBe(SigningType.RAW);
+    wallet.disconnect();
   });
 
   it('Verify getSigningType returns correct signing type for an empty array', async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const signingType = wallet['getSigningType'](new Uint8Array([]));
     expect(signingType).toBe(SigningType.RAW);
+    wallet.disconnect();
   });
 
   it('Verify getSigningType returns correct signing type for 3', async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const signingType = wallet['getSigningType'](new Uint8Array([3]));
     expect(signingType).toBe(SigningType.OPERATION);
+    wallet.disconnect();
   });
 
   it('Verify getSigningType returns correct signing type for 5', async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     const signingType = wallet['getSigningType'](new Uint8Array([5]));
     expect(signingType).toBe(SigningType.MICHELINE);
+    wallet.disconnect();
   });
 
   it('Verify getSigningType throws for invalid inputs', async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     expect(() => wallet['getSigningType'](new Uint8Array([5, 3]))).toThrow();
     expect(() => wallet['getSigningType'](new Uint8Array([7]))).toThrow();
+    wallet.disconnect();
   });
 
   it('Verify sign throws for Micheline', async () => {
@@ -122,10 +140,12 @@ describe('Beacon Wallet tests', () => {
     expect(
       async () => await wallet.sign('48656C6C6F20576F726C64', new Uint8Array([5]))
     ).rejects.toThrow();
+    wallet.disconnect();
   });
 
   it('Verify sign throws for Raw', async () => {
     const wallet = new BeaconWallet({ name: 'Test', storage: new LocalStorage() });
     expect(async () => await wallet.sign('48656C6C6F20576F726C64')).rejects.toThrow();
+    wallet.disconnect();
   });
 });
