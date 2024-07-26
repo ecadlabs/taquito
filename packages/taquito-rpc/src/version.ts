668e57eda3f7408503646f564346704d0f979327
Davis Sawali <davis.sawali@ecadlabs.com>
2024-07-26 11:23:36 -0700

    chore(releng) bump version to 20.0.2-beta.0

--- packages/taquito-rpc/src/version.ts
+++ packages/taquito-rpc/src/version.ts
@@ -1,5 +1,5 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 export const VERSION = {
-  commitHash: 'd44ee8a26e6924e12a8cae2f9c5b717fc65af72c',
-  version: '20.0.1',
+  commitHash: '8c7737cb6ad36ea87f5f8833e012a9d472832790',
+  version: '20.0.2-beta.0',
 };
