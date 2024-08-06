81f55ffb8f272e048bcad17a0c6c8eedc18630cb
huianyang <hui-an.yang@ecadlabs.com>
2024-08-06 11:57:27 -0400

    test: betanet test run

---
+++ integration-tests/known-contracts-PtA4NFGxa.ts
@@ -0,0 +1,8 @@
+import { KnownContracts } from './known-contracts';
+export const knownContractsPtA4NFGxa: KnownContracts = {
+  contract: "KT1SoqbFs4w9rwxh4TjPDuERpnqygA6grcDa",
+  bigMapContract: "KT1FunayaTPV6DMQtFeu7MERjSuoNEeurm9N",
+  tzip12BigMapOffChainContract: "KT1EbNPgyxc3sGTUJZtbscjpprsGemVUCo2c",
+  saplingContract: "KT1JBbu1MKyMLRQGYBZLJLEmfPYSxb6czCMW",
+  onChainViewContractAddress: "KT1BuRRcXXPN3Pb45nPFZJ2snxkuxpCmtQ56",
+};
