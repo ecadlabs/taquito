943d5fd11790da35f4a3e6ecfcaae3c4f0399a97
Davis Sawali <davis.sawali@ecadlabs.com>
2024-08-26 13:12:01 -0700

    added mock for beacon wallet tests

---
+++ packages/taquito-beacon-wallet/test/__mocks__/broadcast-channel.ts
@@ -0,0 +1,47 @@
+interface MessageListener {
+    (message: any): void;
+  }
+  
+  class BroadcastChannel {
+    name: string;
+    listeners: MessageListener[];
+  
+    constructor(name: string) {
+      this.name = name;
+      this.listeners = [];
+    }
+  
+    postMessage(message: any) {
+      // Mock implementation of postMessage
+      this.listeners.forEach((listener) => listener(message));
+    }
+  
+    addEventListener(event: string, listener: MessageListener) {
+      if (event === 'message') {
+        this.listeners.push(listener);
+      }
+    }
+  
+    removeEventListener(event: string, listener: MessageListener) {
+      if (event === 'message') {
+        this.listeners = this.listeners.filter((l) => l !== listener);
+      }
+    }
+  
+    close() {
+      // Mock implementation of close
+      this.listeners = [];
+    }
+  }
+  
+  function createLeaderElection(_channel: BroadcastChannel) {
+    // Mock implementation of createLeaderElection
+    return {
+      awaitLeadership: jest.fn(),
+      hasLeader: jest.fn().mockReturnValue(true),
+      isLeader: jest.fn().mockReturnValue(true),
+      die: jest.fn(),
+    };
+  }
+  
+  export { BroadcastChannel, createLeaderElection };
