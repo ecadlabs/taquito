f0f9e4d22a374c72913abbb1991b0e5b34f4c978
Davis Sawali <davis.sawali@ecadlabs.com>
2024-08-27 10:18:23 -0700

    chore(releng) bump version to 20.0.2-beta.2

--- packages/taquito-tzip16/src/version.ts
+++ packages/taquito-tzip16/src/version.ts
@@ -1,5 +1,6 @@
+
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 export const VERSION = {
-  commitHash: 'd44ee8a26e6924e12a8cae2f9c5b717fc65af72c',
-  version: '20.0.1',
+    "commitHash": "943d5fd11790da35f4a3e6ecfcaae3c4f0399a97",
+    "version": "20.0.2-beta.2"
 };
