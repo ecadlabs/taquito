35c67ee75cb0426edecb5e008b35091bfad6ff0a
huianyang <hui-an.yang@ecadlabs.com>
2025-01-09 11:40:37 -0800

    test: added quebecnet ticketContract

--- integration-tests/known-contracts-PsQuebecn.ts
+++ integration-tests/known-contracts-PsQuebecn.ts
@@ -5,4 +5,5 @@ export const knownContractsPsQuebecn: KnownContracts = {
   tzip12BigMapOffChainContract: "KT1HW4jLAy8q52cXSsdKRXpDrgbobK6GCHfE",
   saplingContract: "KT1H5U6DYcmBnxeywD1Usi74agwxNX63pXUK",
   onChainViewContractAddress: "KT1NMQN7dEtALemqWJooQvvKgAHa4ZtuoFE4",
+  ticketContract: "KT1Q7wR3G9m5LWYmkHMw5j7hggCrhFdU69Db",
 };
