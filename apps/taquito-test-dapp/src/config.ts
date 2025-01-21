b91e4846fd9ca54efa0f2a59ac217f2ad842114b
huianyang <hui-an.yang@ecadlabs.com>
2025-01-20 16:37:10 -0800

    ci: removed most paris tests and reference after mainnet protocol migration

--- apps/taquito-test-dapp/src/config.ts
+++ apps/taquito-test-dapp/src/config.ts
@@ -1,12 +1,11 @@
 import { NetworkType as NetworkTypeBeacon } from "@airgap/beacon-sdk";
 import { NetworkType as NetworkTypeWc } from "@taquito/wallet-connect";
 
-export type SupportedNetworks = NetworkTypeBeacon.MAINNET | NetworkTypeBeacon.GHOSTNET | NetworkTypeBeacon.PARISNET | NetworkTypeBeacon.QUEBECNET | NetworkTypeWc.MAINNET | NetworkTypeWc.GHOSTNET | NetworkTypeWc.PARISNET | NetworkTypeWc.QUEBECNET | NetworkTypeBeacon.CUSTOM;
+export type SupportedNetworks = NetworkTypeBeacon.MAINNET | NetworkTypeBeacon.GHOSTNET  | NetworkTypeBeacon.QUEBECNET | NetworkTypeWc.MAINNET | NetworkTypeWc.GHOSTNET | NetworkTypeWc.QUEBECNET | NetworkTypeBeacon.CUSTOM;
 
 const rpcUrls: Record<SupportedNetworks, string> = {
   [NetworkTypeBeacon.MAINNET]: "https://mainnet.tezos.ecadinfra.com",
   [NetworkTypeBeacon.GHOSTNET]: "https://ghostnet.tezos.ecadinfra.com/",
-  [NetworkTypeBeacon.PARISNET]: "https://rpc.pariscnet.teztnets.com/",
   [NetworkTypeBeacon.QUEBECNET]: "https://rpc.quebecnet.teztnets.com",
   [NetworkTypeBeacon.CUSTOM]: "https://ghostnet.tezos.ecadinfra.com/",
 };
@@ -23,9 +22,6 @@ export const getTzKtUrl = (networkType: SupportedNetworks): string | undefined =
     case NetworkTypeBeacon.GHOSTNET:
     case NetworkTypeWc.GHOSTNET:
       return "https://ghostnet.tzkt.io";
-    case NetworkTypeBeacon.PARISNET:
-    case NetworkTypeWc.PARISNET:
-      return "https://parisnet.tzkt.io";
     case NetworkTypeBeacon.QUEBECNET:
     case NetworkTypeWc.QUEBECNET:
         return "https://quebecnet.tzkt.io";
@@ -43,6 +39,5 @@ export const contractAddress = {
   mainnet: "KT1ShtH2zCrKMuWGRejEd6RAcnePwxBQeMAN",
   ghostnet: "KT1QKmcNBcfzVTXG2kBcE6XqXtEuYYUzMcT5",
   oxfordnet: "KT1GYx1KDhMQt2GJEztRh8EyYxJUPM6fnAMM",
-  parisnet: "KT1E43cQefjM8fq7B5pEJFJoGbRmuNibDoBC",
   quebecnet: "KT1JZ3H8zMn6GXoftLpRzGUwRD4fP7mmxKqW"
 };
