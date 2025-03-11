30b04a1a961638b1dff65cee481ffb854bab4051
Léo Pradel <pradel.leo@gmail.com>
2025-03-11 11:19:46 +0100

    feat: replace bip39 with @scure/bip39

--- integration-tests/__tests__/sapling/sapling-transactions-proof-using-proving-key.spec.ts
+++ integration-tests/__tests__/sapling/sapling-transactions-proof-using-proving-key.spec.ts
@@ -1,15 +1,24 @@
-import { ContractAbstraction, ContractProvider, RpcReadAdapter } from '@taquito/taquito';
+import {
+  ContractAbstraction,
+  ContractProvider,
+  RpcReadAdapter,
+} from '@taquito/taquito';
 import { CONFIGS } from '../../config';
-import { InMemorySpendingKey, SaplingToolkit, InMemoryProvingKey } from '@taquito/sapling';
+import {
+  InMemorySpendingKey,
+  SaplingToolkit,
+  InMemoryProvingKey,
+} from '@taquito/sapling';
 import BigNumber from 'bignumber.js';
 import { singleSaplingStateContractJProtocol } from '../../data/single_sapling_state_contract_jakarta_michelson';
-import * as bip39 from 'bip39';
+import * as bip39 from '@scure/bip39';
+import { wordlist } from '@scure/bip39/wordlists/english';
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
   const Tezos = lib;
   let saplingContract: ContractAbstraction<ContractProvider>;
   let bobInmemorySpendingKey: InMemorySpendingKey;
-  let bobPaymentAddress: string
+  let bobPaymentAddress: string;
   let aliceInMemorySpendingKey: InMemorySpendingKey;
   let aliceInMemoryProvingKey: InMemoryProvingKey;
   let alicePaymentAddress: string;
@@ -17,99 +26,119 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
   const memoSize = 8;
 
   describe(`Test producing proofs with a proving key rather than a spending key: ${rpc}`, () => {
-
     beforeAll(async () => {
       await setup();
 
       // Deploy the sapling contract
       const saplingContractOrigination = await Tezos.contract.originate({
         code: singleSaplingStateContractJProtocol(),
-        init: '{}'
+        init: '{}',
       });
       await saplingContractOrigination.confirmation();
       saplingContract = await saplingContractOrigination.contract();
 
       // Generate a spending key and an InMemorySpendingKey instance for Bob using a mnemonic
-      const mnemonic: string = bip39.generateMnemonic();
+      const mnemonic: string = bip39.generateMnemonic(wordlist);
       bobInmemorySpendingKey = await InMemorySpendingKey.fromMnemonic(mnemonic);
 
       // Instantiate an InMemorySpendingKey from a spending key for Alice
-      aliceInMemorySpendingKey = new InMemorySpendingKey('sask27SLmU9herddHz4qFJBLMjWYMbJF8RtS579w9ej9mfCYK7VUdyCJPHK8AzW9zMsopGZEkYeNjAY7Zz1bkM7CGu8eKLzrjBLTMC5wWJDhxiK91ahA29rhDRsHdJDV2u2jFwb2MNUix8JW7sAkAqYVaJpCehTBPgRQ1KqKwqqUaNmuD8kazd4Q8MCWmgbWs21Yuomdqyi9FLigjRp7oY4m5adaVU19Nj1AHvsMY2tePeU2L')
-      aliceInMemoryProvingKey = new InMemoryProvingKey('44259fd700dc80120d3c9ca65d698f6064043b048b079caa4f198aed962717403f80bf8cb9a8da8deb290913e9302be00c56f4565d917a6170be1880f42bb709');
-
+      aliceInMemorySpendingKey = new InMemorySpendingKey(
+        'sask27SLmU9herddHz4qFJBLMjWYMbJF8RtS579w9ej9mfCYK7VUdyCJPHK8AzW9zMsopGZEkYeNjAY7Zz1bkM7CGu8eKLzrjBLTMC5wWJDhxiK91ahA29rhDRsHdJDV2u2jFwb2MNUix8JW7sAkAqYVaJpCehTBPgRQ1KqKwqqUaNmuD8kazd4Q8MCWmgbWs21Yuomdqyi9FLigjRp7oY4m5adaVU19Nj1AHvsMY2tePeU2L'
+      );
+      aliceInMemoryProvingKey = new InMemoryProvingKey(
+        '44259fd700dc80120d3c9ca65d698f6064043b048b079caa4f198aed962717403f80bf8cb9a8da8deb290913e9302be00c56f4565d917a6170be1880f42bb709'
+      );
     });
 
     it('Verify that Alice can shield tokens', async () => {
-
       const amountToAlice = 3;
       const aliceSaplingToolkit = new SaplingToolkit(
-        { saplingSigner: aliceInMemorySpendingKey, saplingProver: aliceInMemoryProvingKey },
+        {
+          saplingSigner: aliceInMemorySpendingKey,
+          saplingProver: aliceInMemoryProvingKey,
+        },
         { contractAddress: saplingContract.address, memoSize },
         new RpcReadAdapter(Tezos.rpc)
       );
-      const aliceInMemoryViewingKey = await aliceInMemorySpendingKey.getSaplingViewingKeyProvider();
+      const aliceInMemoryViewingKey =
+        await aliceInMemorySpendingKey.getSaplingViewingKeyProvider();
       // Fetch a payment address (zet) for Alice
-      alicePaymentAddress = (await aliceInMemoryViewingKey.getAddress()).address;
+      alicePaymentAddress = (await aliceInMemoryViewingKey.getAddress())
+        .address;
 
-      const shieldedTx = await aliceSaplingToolkit.prepareShieldedTransaction([{
-        to: alicePaymentAddress,
-        amount: amountToAlice,
-        memo: 'First Tx'
-      }])
+      const shieldedTx = await aliceSaplingToolkit.prepareShieldedTransaction([
+        {
+          to: alicePaymentAddress,
+          amount: amountToAlice,
+          memo: 'First Tx',
+        },
+      ]);
 
       // Inject the sapling transaction using the ContractAbstraction by calling the default entrypoint
       // The amount MUST be specified in the send method in order to transfer the 3 tez to the shielded pool
-      const op = await saplingContract.methods.default([shieldedTx]).send({ amount: amountToAlice });
+      const op = await saplingContract.methods
+        .default([shieldedTx])
+        .send({ amount: amountToAlice });
       await op.confirmation();
 
       expect(op.status).toEqual('applied');
       expect(op.hash).toBeDefined();
       expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
-
     });
 
     it("Verify that Alice's balance in the sapling pool updated after the shielded tx", async () => {
       const aliceSaplingToolkit = new SaplingToolkit(
-        { saplingSigner: aliceInMemorySpendingKey, saplingProver: aliceInMemoryProvingKey },
+        {
+          saplingSigner: aliceInMemorySpendingKey,
+          saplingProver: aliceInMemoryProvingKey,
+        },
         { contractAddress: saplingContract.address, memoSize },
         new RpcReadAdapter(Tezos.rpc)
       );
-      const aliceTxViewer = await aliceSaplingToolkit.getSaplingTransactionViewer();
+      const aliceTxViewer =
+        await aliceSaplingToolkit.getSaplingTransactionViewer();
       const aliceBalance = await aliceTxViewer.getBalance();
 
       // The returned balance is in MUTEZ
       expect(aliceBalance).toEqual(new BigNumber(3000000));
 
-      const inputsAlice = await aliceTxViewer.getIncomingAndOutgoingTransactions();
+      const inputsAlice =
+        await aliceTxViewer.getIncomingAndOutgoingTransactions();
       expect(inputsAlice).toEqual({
         incoming: [
           {
             value: new BigNumber(3000000),
             memo: 'First Tx',
             paymentAddress: alicePaymentAddress,
-            isSpent: false
-          }
+            isSpent: false,
+          },
         ],
-        outgoing: []
-      })
+        outgoing: [],
+      });
     });
 
     it('Verify that Alice can do a shielded transaction to Bob', async () => {
       const amountToBob = 2;
       // Bob needs to give a payment address (zet) to Alice
-      const bobInMemoryViewingKey = await bobInmemorySpendingKey.getSaplingViewingKeyProvider();
+      const bobInMemoryViewingKey =
+        await bobInmemorySpendingKey.getSaplingViewingKeyProvider();
       bobPaymentAddress = (await bobInMemoryViewingKey.getAddress()).address;
 
       const aliceSaplingToolkit = new SaplingToolkit(
-        { saplingSigner: aliceInMemorySpendingKey, saplingProver: aliceInMemoryProvingKey },
+        {
+          saplingSigner: aliceInMemorySpendingKey,
+          saplingProver: aliceInMemoryProvingKey,
+        },
         { contractAddress: saplingContract.address, memoSize },
         new RpcReadAdapter(Tezos.rpc)
       );
-      const tx = await aliceSaplingToolkit.prepareSaplingTransaction([{
-        to: bobPaymentAddress,
-        amount: amountToBob,
-        memo: 'A gift'
-      }])
+      const tx = await aliceSaplingToolkit.prepareSaplingTransaction([
+        {
+          to: bobPaymentAddress,
+          amount: amountToBob,
+          memo: 'A gift',
+        },
+      ]);
 
       // Inject the sapling transaction using the ContractAbstraction by calling the default entrypoint
       const op = await saplingContract.methods.default([tx]).send();
@@ -118,53 +147,63 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
       expect(op.status).toEqual('applied');
       expect(op.hash).toBeDefined();
       expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
-
     });
 
     it("Verify that Alice's balance in the sapling pool updated after the sapling tx", async () => {
       const aliceSaplingToolkit = new SaplingToolkit(
-        { saplingSigner: aliceInMemorySpendingKey, saplingProver: aliceInMemoryProvingKey },
+        {
+          saplingSigner: aliceInMemorySpendingKey,
+          saplingProver: aliceInMemoryProvingKey,
+        },
         { contractAddress: saplingContract.address, memoSize },
         new RpcReadAdapter(Tezos.rpc)
       );
-      const aliceTxViewer = await aliceSaplingToolkit.getSaplingTransactionViewer();
+      const aliceTxViewer =
+        await aliceSaplingToolkit.getSaplingTransactionViewer();
       const aliceBalance = await aliceTxViewer.getBalance();
 
       // The returned balance is in MUTEZ
       expect(aliceBalance).toEqual(new BigNumber(1000000));
 
-      const inputsAlice = await aliceTxViewer.getIncomingAndOutgoingTransactions();
+      const inputsAlice =
+        await aliceTxViewer.getIncomingAndOutgoingTransactions();
       expect(inputsAlice).toEqual({
         incoming: [
           {
             value: new BigNumber(3000000),
             memo: 'First Tx',
             paymentAddress: alicePaymentAddress,
-            isSpent: true
+            isSpent: true,
           },
-          { // This input is a payback for when Alice sent 2 tz to bob (3tz - 2tz = 1tz).
+          {
+            // This input is a payback for when Alice sent 2 tz to bob (3tz - 2tz = 1tz).
             // Alice consumed the 3tz input and received 1tz back.
             value: new BigNumber(1000000),
             memo: '',
             paymentAddress: alicePaymentAddress,
-            isSpent: false
-          }
+            isSpent: false,
+          },
         ],
         outgoing: [
           {
             value: new BigNumber(2000000),
             memo: 'A gift',
-            paymentAddress: bobPaymentAddress
+            paymentAddress: bobPaymentAddress,
           },
-          { // Here Alice sent 1tz back to her (matching the payback input above).
+          {
+            // Here Alice sent 1tz back to her (matching the payback input above).
             value: new BigNumber(1000000),
             memo: '',
-            paymentAddress: alicePaymentAddress
-          }
-        ]
-      })
+            paymentAddress: alicePaymentAddress,
+          },
+        ],
+      });
 
-      const bobSaplingToolkit = new SaplingToolkit({ saplingSigner: bobInmemorySpendingKey }, { contractAddress: saplingContract.address, memoSize }, new RpcReadAdapter(Tezos.rpc))
+      const bobSaplingToolkit = new SaplingToolkit(
+        { saplingSigner: bobInmemorySpendingKey },
+        { contractAddress: saplingContract.address, memoSize },
+        new RpcReadAdapter(Tezos.rpc)
+      );
       const bobTxViewer = await bobSaplingToolkit.getSaplingTransactionViewer();
       const bobBalance = await bobTxViewer.getBalance();
 
@@ -178,27 +217,30 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
             value: new BigNumber(2000000),
             memo: 'A gift',
             paymentAddress: bobPaymentAddress,
-            isSpent: false
-          }
+            isSpent: false,
+          },
         ],
-        outgoing: []
-      })
+        outgoing: [],
+      });
     });
 
     it('Verify that Alice can unshield tokens', async () => {
-
       const amount = 1;
       const aliceSaplingToolkit = new SaplingToolkit(
-        { saplingSigner: aliceInMemorySpendingKey, saplingProver: aliceInMemoryProvingKey },
+        {
+          saplingSigner: aliceInMemorySpendingKey,
+          saplingProver: aliceInMemoryProvingKey,
+        },
         { contractAddress: saplingContract.address, memoSize },
         new RpcReadAdapter(Tezos.rpc)
       );
       const tezosInitialBalance = await Tezos.tz.getBalance(tezosAddress);
 
-      const unshieldedTx = await aliceSaplingToolkit.prepareUnshieldedTransaction({
-        to: tezosAddress,
-        amount
-      })
+      const unshieldedTx =
+        await aliceSaplingToolkit.prepareUnshieldedTransaction({
+          to: tezosAddress,
+          amount,
+        });
 
       // Inject the sapling transaction using the ContractAbstraction by calling the default entrypoint
       const op = await saplingContract.methods.default([unshieldedTx]).send();
@@ -209,63 +251,67 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
       expect(op.includedInBlock).toBeLessThan(Number.POSITIVE_INFINITY);
 
       const tezosUpdatedBalance = await Tezos.tz.getBalance(tezosAddress);
-      expect(tezosUpdatedBalance).toEqual(tezosInitialBalance.plus(new BigNumber(1000000)));
-
+      expect(tezosUpdatedBalance).toEqual(
+        tezosInitialBalance.plus(new BigNumber(1000000))
+      );
     });
 
     it("Verify that Alice's balance in the sapling pool updated after the unshielded tx", async () => {
       const aliceSaplingToolkit = new SaplingToolkit(
-        { saplingSigner: aliceInMemorySpendingKey, saplingProver: aliceInMemoryProvingKey },
+        {
+          saplingSigner: aliceInMemorySpendingKey,
+          saplingProver: aliceInMemoryProvingKey,
+        },
         { contractAddress: saplingContract.address, memoSize },
         new RpcReadAdapter(Tezos.rpc)
       );
-      const aliceTxViewer = await aliceSaplingToolkit.getSaplingTransactionViewer();
+      const aliceTxViewer =
+        await aliceSaplingToolkit.getSaplingTransactionViewer();
       const aliceBalance = await aliceTxViewer.getBalance();
 
       expect(aliceBalance).toEqual(new BigNumber(0));
 
-      const inputsAlice = await aliceTxViewer.getIncomingAndOutgoingTransactions();
+      const inputsAlice =
+        await aliceTxViewer.getIncomingAndOutgoingTransactions();
       expect(inputsAlice).toEqual({
         incoming: [
           {
             value: new BigNumber(3000000),
             memo: 'First Tx',
             paymentAddress: alicePaymentAddress,
-            isSpent: true
+            isSpent: true,
           },
           {
             value: new BigNumber(1000000),
             memo: '',
             paymentAddress: alicePaymentAddress,
-            isSpent: true
+            isSpent: true,
           },
           {
             value: new BigNumber(0),
             memo: '',
             paymentAddress: alicePaymentAddress,
-            isSpent: false
-          }
+            isSpent: false,
+          },
         ],
         outgoing: [
           {
             value: new BigNumber(2000000),
             memo: 'A gift',
-            paymentAddress: bobPaymentAddress
+            paymentAddress: bobPaymentAddress,
           },
           {
             value: new BigNumber(1000000),
             memo: '',
-            paymentAddress: alicePaymentAddress
+            paymentAddress: alicePaymentAddress,
           },
           {
             value: new BigNumber(0),
             memo: '',
-            paymentAddress: alicePaymentAddress
-          }
-        ]
-      })
-
+            paymentAddress: alicePaymentAddress,
+          },
+        ],
+      });
     });
-
   });
 });
--- integration-tests/__tests__/signer-mnemonic.spec.ts
+++ integration-tests/__tests__/signer-mnemonic.spec.ts
@@ -1,20 +1,19 @@
 import { InMemorySigner } from '@taquito/signer';
 import { TezosToolkit } from '@taquito/taquito';
 import { CONFIGS } from '../config';
-import * as Bip39 from 'bip39';
+import * as bip39 from '@scure/bip39';
+import { wordlist } from '@scure/bip39/wordlists/english';
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
   let Funder: TezosToolkit;
   describe(`Create signer instances with fromMnemonic: ${rpc}`, () => {
     let mnemonic: string;
-    let Tez1: TezosToolkit,
-      Tez2: TezosToolkit,
-      Tez3: TezosToolkit;
+    let Tez1: TezosToolkit, Tez2: TezosToolkit, Tez3: TezosToolkit;
 
     let funderPKH: string;
 
     beforeAll(async () => {
-      mnemonic = Bip39.generateMnemonic();
+      mnemonic = bip39.generateMnemonic(wordlist);
       Funder = lib;
       await setup();
 
@@ -30,28 +29,44 @@ CONFIGS().forEach(({ lib, rpc, setup }) => {
         Tez1.setSignerProvider(signer1);
         const tez1Pkh = await Tez1.signer.publicKeyHash();
 
-
         Tez2 = new TezosToolkit(rpc);
-        const signer2 = InMemorySigner.fromMnemonic({ mnemonic, curve: 'secp256k1' });
+        const signer2 = InMemorySigner.fromMnemonic({
+          mnemonic,
+          curve: 'secp256k1',
+        });
         Tez2.setSignerProvider(signer2);
         const tez2Pkh = await Tez2.signer.publicKeyHash();
 
         Tez3 = new TezosToolkit(rpc);
-        const signer3 = InMemorySigner.fromMnemonic({ mnemonic, curve: 'p256' });
+        const signer3 = InMemorySigner.fromMnemonic({
+          mnemonic,
+          curve: 'p256',
+        });
         Tez3.setSignerProvider(signer3);
         const tez3Pkh = await Tez3.signer.publicKeyHash();
 
         // Fund all the signer accounts
-        const send1 = await Funder.contract.transfer({ to: tez1Pkh, amount: 2 });
+        const send1 = await Funder.contract.transfer({
+          to: tez1Pkh,
+          amount: 2,
+        });
         await send1.confirmation();
 
-        const send2 = await Funder.contract.transfer({ to: tez2Pkh, amount: 2 });
+        const send2 = await Funder.contract.transfer({
+          to: tez2Pkh,
+          amount: 2,
+        });
         await send2.confirmation();
 
-        const send3 = await Funder.contract.transfer({ to: tez3Pkh, amount: 2 });
+        const send3 = await Funder.contract.transfer({
+          to: tez3Pkh,
+          amount: 2,
+        });
         await send3.confirmation();
       } catch (e) {
-        console.log(`Error when trying to fund account: \n ${JSON.stringify(e)}`)
+        console.log(
+          `Error when trying to fund account: \n ${JSON.stringify(e)}`
+        );
       }
     });
 
