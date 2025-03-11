31f26cff9921f6464cc4642f13410c0884b1eb3a
huianyang <hui-an.yang@ecadlabs.com>
2025-03-11 16:37:03 -0700

    chore: removed deprecated hardcoded constants DEFAULT_STORAGE_LIMIT,DEFAULT_FEE,DEFAULT_GAS_LIMIT
    BREAKING CHANGE: removed deprecated hardcoded constants
    DEFAULT_STORAGE_LIMIT,DEFAULT_FEE,DEFAULT_GAS_LIMIT

--- packages/taquito/src/constants.ts
+++ packages/taquito/src/constants.ts
@@ -1,52 +1,22 @@
-/**
- * @deprecated default reveal gasLimit please use getRevealGasLimit(address) instead, removing hardcoded gasLimit of delegation, origination and transfer
- */
-export const DEFAULT_GAS_LIMIT = {
-  DELEGATION: 10600,
-  ORIGINATION: 10600,
-  TRANSFER: 10600,
-  REVEAL_TZ1: 1000,
-  REVEAL_TZ2: 1000,
-  REVEAL_TZ3: 2000,
-  REVEAL_TZ4: 2000,
-};
-/**
- * @deprecated default reveal fee please use getRevealFee(address) instead, removing hardcoded fee of delegation, origination and transfer
- */
-export const DEFAULT_FEE = {
-  DELEGATION: 1257,
-  ORIGINATION: 10000,
-  TRANSFER: 10000,
-  REVEAL: 374,
-};
-/**
- * @deprecated default reveal storageLimit please use REVEAL_STORAGE_LIMIT instead, removing hardcoded storageLimit of delegation, origination and transfer
- */
-export const DEFAULT_STORAGE_LIMIT = {
-  DELEGATION: 0,
-  ORIGINATION: 257,
-  TRANSFER: 257,
-  REVEAL: 0,
-};
-// value is based on octez-client reveal operation gasLimit of each address type
+// value is based on octez-client reveal operation gasLimit of each address type in Rio Protocol
 const REVEAL_GAS_LIMIT = {
   TZ1: 169,
   TZ2: 155,
   TZ3: 445,
   TZ4: 1674,
 };
-// value is based on octez-client reveal operation fee of each address type
+// value is based on octez-client reveal operation fee of each address type in Rio Protocol
 const REVEAL_FEE = {
   TZ1: 276,
   TZ2: 276,
   TZ3: 305,
   TZ4: 477,
 };
-// value is based on octez-client reveal operation storageLimit of all address type
+// value is based on octez-client reveal operation storageLimit of all address type in Rio Protocol
 export const REVEAL_STORAGE_LIMIT = 0;
-// protocol constants
+// protocol constants in Rio Protocol
 export const ORIGINATION_SIZE = 257;
-// protocol constants
+// protocol constants in Rio Protocol
 export const COST_PER_BYTE = 250;
 
 export enum Protocols {
