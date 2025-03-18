f89c633fdd598af1c22a91a7fdbf06a9494dd7ec
huianyang <hui-an.yang@ecadlabs.com>
2025-03-18 14:10:21 -0700

    chore(releng) bump version to 21.0.4-beta.1

--- packages/taquito-beacon-wallet/src/version.ts
+++ packages/taquito-beacon-wallet/src/version.ts
@@ -1,6 +1,6 @@
 
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 export const VERSION = {
-    "commitHash": "37c8a05360ab00dc3b5d1fbc691d0ac571051a32",
-    "version": "21.0.3"
+    "commitHash": "fc1deedd999e20a066dafd5f5a1e0a6e0186fec4",
+    "version": "21.0.4-beta.1"
 };
