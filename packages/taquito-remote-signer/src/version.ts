1469d6f0d55134a4b853598a2eec48e6f71b3da3
huianyang <hui-an.yang@ecadlabs.com>
2025-08-28 16:13:29 -0700

    chore(releng) bump version to 23.0.0-beta.1

--- packages/taquito-remote-signer/src/version.ts
+++ packages/taquito-remote-signer/src/version.ts
@@ -1,6 +1,6 @@
 
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 export const VERSION = {
-    "commitHash": "7af2138a9e5c5b230c4b4c726f35c2f2e67b721c",
-    "version": "23.0.0-beta.0"
+    "commitHash": "10b3de10de15ae68d47b1fca922d3129d2f79641",
+    "version": "23.0.0-beta.1"
 };
