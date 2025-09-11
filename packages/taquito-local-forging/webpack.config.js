3c4305115059083a9f534ba0c468ec2127f37e1e
huianyang <hui-an.yang@ecadlabs.com>
2025-09-11 07:28:46 -0700

    chore: added fast-text-encoding plugin for TextEncoder of local-forging webpack bundle

--- packages/taquito-local-forging/webpack.config.js
+++ packages/taquito-local-forging/webpack.config.js
@@ -3,34 +3,38 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: {
-        "taquito_local_forging": ['./src/taquito-local-forging.ts']
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        modules: ['node_modules'],
-        fallback: { 
-            "stream": require.resolve("stream-browserify") 
-        }
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        library: ['[name]'],
-        libraryTarget: "var"
-    },
-    plugins: [
-        new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'] })
+  entry: {
+    "taquito_local_forging": ['./src/taquito-local-forging.ts']
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
     ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: ['node_modules'],
+    fallback: {
+      "stream": require.resolve("stream-browserify")
+    }
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    library: ['[name]'],
+    libraryTarget: "var"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      TextEncoder: ['fast-text-encoding', 'TextEncoder'],
+      TextDecoder: ['fast-text-encoding', 'TextDecoder'],
+    })
+  ]
 };
