c7129ccd13dfc6d6d4e2b1ab16d887d8f0758a05
Maxwell Ward <max.jp.ward@gmail.com>
2025-10-31 12:33:13 -0700

    use preset setup and tezos lib

--- integration-tests/__tests__/wallet/register-global-constant.spec.ts
+++ integration-tests/__tests__/wallet/register-global-constant.spec.ts
@@ -3,22 +3,15 @@ import { CONFIGS } from "../../config";
 const crypto = require('crypto');
 
 CONFIGS().forEach(({ lib, rpc, setup }) => {
-  let signerAlice = new InMemorySigner('edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq');
+  const Tezos = lib;
 
   describe(`Test wallet registerGlobalConstant using: ${rpc}`, () => {
-    const Tezos = lib
+    
     const randomAnnots = () => crypto.randomBytes(16).toString('hex');
     let annots = randomAnnots();
 
-    beforeAll(async () => {
-      setup(true)
-      if (rpc.includes('seoul')) {
-        Tezos.setProvider({ signer: signerAlice, rpc: 'https://rpc.tzkt.io/seoulnet' })
-      } else if (rpc.includes('ghost')) {
-        Tezos.setProvider({ signer: signerAlice, rpc: 'https://rpc.tzkt.io/ghostnet' })
-      } else if (rpc.includes('shadow')) {
-        Tezos.setProvider({ signer: signerAlice, rpc: 'https://rpc.tzkt.io/shadownet' })
-      }
+    beforeEach(async () => {
+      await setup(true);
     });
     
     test('Should register a global constant', async () => {
