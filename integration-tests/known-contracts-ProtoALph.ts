08ab934a014ce2124d67d180ebb0fadd9b960796
huianyang <hui-an.yang@ecadlabs.com>
2025-11-05 14:56:31 -0800

    test: deployed weeklynet contracts and config cleanup

--- integration-tests/known-contracts-ProtoALph.ts
+++ integration-tests/known-contracts-ProtoALph.ts
@@ -1,9 +1,9 @@
 import { KnownContracts } from './known-contracts';
 export const knownContractsProtoALph: KnownContracts = {
-  contract: "KT1Pn9woy3PJhEk6Mi2YzkEaFk8qg8DmALco",
-  bigMapContract: "KT1En2tm3BmRRfsNdA15RZw18JHH2KXvsvXQ",
-  tzip12BigMapOffChainContract: "KT1Mzm5phDddzLDpyPVnqbv4Nze9ErSDYN4u",
-  saplingContract: "KT1VmGY5qqRG6bjhLZNBWFVZrffgkKDF7JzS",
-  onChainViewContractAddress: "KT1NaFv8D9sGfAmwnCPBMQJgeN5zK3dbHd45",
-  ticketContract: "KT19naSoCwpdE1vZz1Yjx5wYKaHSsSyLft9K",
+  contract: "KT1WESJQMqveCj43gnmjzGHrd4GpgKtf5mb9",
+  bigMapContract: "KT1XZnAUbLb2jnFU3mr4rWmvrUz2q9BPSej6",
+  tzip12BigMapOffChainContract: "KT1M2gwXenFmxL485jgn2ddFqvXvAekmarXJ",
+  saplingContract: "KT1L5WbqcPMGieADujAjANHK5it1to6PGBDP",
+  onChainViewContractAddress: "KT1RDkZdgHjcVAUfGoRG76iKzuaCdTUEZwCX",
+  ticketContract: "KT1ANWxTGE3Wu1zdwQBn4brFy3ELY4YL1WXt",
 };
