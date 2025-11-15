ce99107cff306ad3236b0e5505a6c30eccafa3b8
Maxwell Ward <max.jp.ward@gmail.com>
2025-11-14 21:00:14 -0800

    refactor taquito signer to resolve circular dependency errors, global definition errors, and bundler issues

--- packages/taquito-signer/rollup.config.ts
+++ packages/taquito-signer/rollup.config.ts
@@ -9,11 +9,47 @@ const libraryName = 'taquito-signer';
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { 
+      file: pkg.main, 
+      name: camelCase(libraryName), 
+      format: 'umd', 
+      sourcemap: true, 
+      globals: { 
+        "@stablelib/blake2b": "blake2b",
+        "@stablelib/ed25519": "ed25519$1",
+        "@taquito/utils": "utils",
+        "@taquito/core": "core",
+        "elliptic": "elliptic",
+        "elliptic/lib/elliptic/ec/key": "KeyPair",
+        "@stablelib/hmac": "hmac",
+        "@stablelib/sha512": "sha512",
+        "bn.js": "BN",
+        "@noble/curves/bls12-381": "bls12381",
+        "@stablelib/nacl": "nacl",
+        "pbkdf2": "pbkdf2",
+        "bip39": "Bip39",
+        "typedarray-to-buffer": "toBuffer"
+      } 
+    },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['typedarray-to-buffer'],
+  external: [
+    '@stablelib/blake2b',
+    '@stablelib/ed25519',
+    '@taquito/utils',
+    '@taquito/core',
+    'elliptic',
+    'elliptic/lib/elliptic/ec/key',
+    '@stablelib/hmac',
+    '@stablelib/sha512',
+    'bn.js',
+    '@noble/curves/bls12-381',
+    '@stablelib/nacl',
+    'pbkdf2',
+    'bip39',
+    'typedarray-to-buffer'
+  ],
   watch: {
     include: 'src/**',
   },
