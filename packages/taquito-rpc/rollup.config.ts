7cb0dab4305db7b4106fd55b737212486fe525da
Maxwell Ward <max.jp.ward@gmail.com>
2025-11-14 21:16:22 -0800

    fix rollup warnings for taquito rpc

--- packages/taquito-rpc/rollup.config.ts
+++ packages/taquito-rpc/rollup.config.ts
@@ -9,11 +9,27 @@ const libraryName = 'taquito-rpc';
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { 
+      file: pkg.main, 
+      name: camelCase(libraryName), 
+      format: 'umd', 
+      sourcemap: true, 
+      globals: { 
+        '@taquito/http-utils': 'httpUtils',
+        'bignumber.js': 'BigNumber',
+        '@taquito/utils': 'utils',
+        '@taquito/core': 'core',
+      } 
+    },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: [
+  'bignumber.js',
+  '@taquito/core',
+  '@taquito/utils',
+  '@taquito/http-utils',
+  ],
   watch: {
     include: 'src/**',
   },
