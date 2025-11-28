4e5267ff624d75e6f9875e91a5d3d4c2b7f01d3c
huianyang <hui-an.yang@ecadlabs.com>
2025-11-27 16:33:00 -0800

    test: updated weeklynet known contracts and originate-known-contracts scripts

--- integration-tests/known-contracts-ProtoALph.ts
+++
@@ -1,9 +0,0 @@
-import { KnownContracts } from './known-contracts';
-export const knownContractsProtoALph: KnownContracts = {
-  contract: "KT1WESJQMqveCj43gnmjzGHrd4GpgKtf5mb9",
-  bigMapContract: "KT1XZnAUbLb2jnFU3mr4rWmvrUz2q9BPSej6",
-  tzip12BigMapOffChainContract: "KT1M2gwXenFmxL485jgn2ddFqvXvAekmarXJ",
-  saplingContract: "KT1L5WbqcPMGieADujAjANHK5it1to6PGBDP",
-  onChainViewContractAddress: "KT1RDkZdgHjcVAUfGoRG76iKzuaCdTUEZwCX",
-  ticketContract: "KT1ANWxTGE3Wu1zdwQBn4brFy3ELY4YL1WXt",
-};
---
+++ integration-tests/known-contracts-weeklynet.ts
@@ -0,0 +1,9 @@
+import { KnownContracts } from './known-contracts';
+export const knownContractsWeeklynet: KnownContracts = {
+  contract: "KT1D5pGwghoHUyn53dECyBRwQCn6gAeXTMLT",
+  bigMapContract: "KT1UbX5ztZwVu6GMs5pKnhZpPdbur9FMMeBs",
+  tzip12BigMapOffChainContract: "KT1TwgGi2Megn3pATMquWnZ64K97UYwjP9as",
+  saplingContract: "KT19KXavkhfiJknDdJ9hiD6vAiQmM4HfoT1w",
+  onChainViewContractAddress: "KT1GbVpdnz9NX3XDNKAjsaGsNkNgUWSJn1VM",
+  ticketContract: "KT1N66b4srKtWfseoRjq5RNbf6bx1FSe4A8j",
+};
