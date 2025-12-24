6bae95b881b20a62ff2373621b82fa2e876a0587
hui-an-yang <106410553+hui-an-yang@users.noreply.github.com>
2025-12-24 15:49:38 -0800

    Dependencies 24 (#3266)
    * [Snyk] Upgrade @ledgerhq/hw-transport-node-hid from 6.29.5 to 6.29.14 (#3250)
    
    * fix: upgrade @ledgerhq/hw-transport-node-hid from 6.29.5 to 6.29.14
    
    Snyk has created this PR to upgrade @ledgerhq/hw-transport-node-hid from 6.29.5 to 6.29.14.
    
    See this package in npm:
    @ledgerhq/hw-transport-node-hid
    
    See this project in Snyk:
    https://app.snyk.io/org/roxane/project/005b4718-7e8c-498c-b817-b6640d54a5ed?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * update package lock
    
    * use native fetch instead of node-fetch
    
    * update package lock
    
    * update package lock
    
    * add whatwg-url
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Maxwell Ward <max.jp.ward@gmail.com>
    Co-authored-by: huianyang <hui-an.yang@ecadlabs.com>
    
    * [Snyk] Upgrade @ledgerhq/hw-transport-node-hid-noevents from 6.30.5 to 6.30.14 (#3251)
    
    * fix: upgrade @ledgerhq/hw-transport-node-hid-noevents from 6.30.5 to 6.30.14
    
    Snyk has created this PR to upgrade @ledgerhq/hw-transport-node-hid-noevents from 6.30.5 to 6.30.14.
    
    See this package in npm:
    @ledgerhq/hw-transport-node-hid-noevents
    
    See this project in Snyk:
    https://app.snyk.io/org/roxane/project/005b4718-7e8c-498c-b817-b6640d54a5ed?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * update package lock
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Maxwell Ward <max.jp.ward@gmail.com>
    Co-authored-by: huianyang <hui-an.yang@ecadlabs.com>
    
    * [Snyk] Upgrade @ledgerhq/devices from 8.6.1 to 8.7.0 (#3252)
    
    * fix: upgrade @ledgerhq/devices from 8.6.1 to 8.7.0
    
    Snyk has created this PR to upgrade @ledgerhq/devices from 8.6.1 to 8.7.0.
    
    See this package in npm:
    @ledgerhq/devices
    
    See this project in Snyk:
    https://app.snyk.io/org/roxane/project/005b4718-7e8c-498c-b817-b6640d54a5ed?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * update package lock
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Maxwell Ward <max.jp.ward@gmail.com>
    Co-authored-by: huianyang <hui-an.yang@ecadlabs.com>
    
    * [Snyk] Upgrade @ledgerhq/hw-transport from 6.31.12 to 6.31.13 (#3253)
    
    * fix: upgrade @ledgerhq/hw-transport from 6.31.12 to 6.31.13
    
    Snyk has created this PR to upgrade @ledgerhq/hw-transport from 6.31.12 to 6.31.13.
    
    See this package in npm:
    @ledgerhq/hw-transport
    
    See this project in Snyk:
    https://app.snyk.io/org/roxane/project/005b4718-7e8c-498c-b817-b6640d54a5ed?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * update package lock
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Maxwell Ward <max.jp.ward@gmail.com>
    Co-authored-by: huianyang <hui-an.yang@ecadlabs.com>
    
    * fix: upgrade @cloudflare/workers-types from 4.20251126.0 to 4.20251128.0 (#3262)
    
    Snyk has created this PR to upgrade @cloudflare/workers-types from 4.20251126.0 to 4.20251128.0.
    
    See this package in npm:
    @cloudflare/workers-types
    
    See this project in Snyk:
    https://app.snyk.io/org/roxane/project/ad4c74dc-f97a-4a6b-8c68-4c305b03e490?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: huianyang <hui-an.yang@ecadlabs.com>
    
    ---------
    
    Co-authored-by: Roxane Létourneau <65877347+roxaneletourneau@users.noreply.github.com>
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Maxwell Ward <max.jp.ward@gmail.com>
    Co-authored-by: Michael Kernaghan <michaelkernaghan@ecadlabs.com>

--- packages/taquito-sapling/fetch-sapling-params.js
+++ packages/taquito-sapling/fetch-sapling-params.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs")
-const fetch = require("node-fetch");
 
 const ZCASH_DOWNLOAD_URL = 'https://download.z.cash/downloads';
 const ZCASH_SPEND_PARAMS_FILE_NAME = 'sapling-spend.params';
