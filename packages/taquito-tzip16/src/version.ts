2833c5196c80c3474d111d87bb87d8b604f1aab2
Maxwell Ward <max.jp.ward@gmail.com>
2026-01-14 18:06:09 -0800

    revert: remove version.ts formatting changes

--- packages/taquito-tzip16/src/version.ts
+++ packages/taquito-tzip16/src/version.ts
@@ -1,5 +1,6 @@
+
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT!
 export const VERSION = {
-  commitHash: 'ff502b6457efd5502635c8d6fe796fe156ef82b7',
-  version: '24.0.1',
+    "commitHash": "ff502b6457efd5502635c8d6fe796fe156ef82b7",
+    "version": "24.0.1"
 };
