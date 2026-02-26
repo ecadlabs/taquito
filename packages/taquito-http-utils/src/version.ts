84f5fd6af5da6c24a6f0e7acd179152a84c6b990
huianyang <hui-an.yang@ecadlabs.com>
2026-02-26 00:11:23 -0800

    chore(releng) bump version to 24.1.0-beta.0

--- packages/taquito-http-utils/src/version.ts
+++ packages/taquito-http-utils/src/version.ts
@@ -1,6 +1,6 @@
 
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT!
 export const VERSION = {
-    "commitHash": "f798e97ba998acc2cc5310278d812448591bd312",
-    "version": "24.0.2"
+    "commitHash": "9e6e5fe757d7fe8afc63e1829d3a7fefb3020bae",
+    "version": "24.1.0-beta.0"
 };
