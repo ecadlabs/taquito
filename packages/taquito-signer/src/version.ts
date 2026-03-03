c4577abf1c335b62171a6b40029a223ee1b2eefb
huianyang <hui-an.yang@ecadlabs.com>
2026-03-02 23:32:23 -0800

    chore(releng) bump version to 24.2.0-beta.0

--- packages/taquito-signer/src/version.ts
+++ packages/taquito-signer/src/version.ts
@@ -1,6 +1,6 @@
 
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT!
 export const VERSION = {
-    "commitHash": "a702f648a1d2aa5948bd26d29dffe1ea14387234",
-    "version": "24.1.0"
+    "commitHash": "809a0e8196889f48bd41c8fb70b429a3e4e0f9d1",
+    "version": "24.2.0-beta.0"
 };
