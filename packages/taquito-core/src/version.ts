cdad2446f320801a0e4da6b936865968d2a188b1
hui-an-yang <106410553+hui-an-yang@users.noreply.github.com>
2026-03-04 15:17:56 -0800

    24.2.0 staging (#3335)
    * chore(releng) bump version to 24.2.0-beta.1
    
    * chore: satisfy lerna
    
    * chore(releng) bump version to 24.2.0

--- packages/taquito-core/src/version.ts
+++ packages/taquito-core/src/version.ts
@@ -1,6 +1,6 @@
 
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT!
 export const VERSION = {
-    "commitHash": "809a0e8196889f48bd41c8fb70b429a3e4e0f9d1",
-    "version": "24.2.0-beta.0"
+    "commitHash": "105a7b15cfb862a0732c204e0e9741098d697775",
+    "version": "24.2.0"
 };
