d3255f4b10a1ea65272579bc407ef384eaa463fa
Jev Björsell <jev@ecadlabs.com>
2026-04-09 15:15:06 -0700

    chore: tighten dependency declarations

--- packages/taquito-sapling/rollup.config.ts
+++ packages/taquito-sapling/rollup.config.ts
@@ -1,6 +1,6 @@
 import camelCase from 'lodash.camelcase';
 import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
